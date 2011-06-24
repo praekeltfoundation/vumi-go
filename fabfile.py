31e46b8eec66cfb2a2dc5c3297d0402331d8daf2
Simon de Haan <simon@praekeltfoundation.org>
2011-06-24 16:01:39 +0200

    removing link to ssh key

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,5 @@
 from fabric.api import *
 
-env.key_filename = '/Users/sdehaan/.ssh/praekelt_foundation_eu.pem'
 env.hosts = ['ubuntu@vumi.praekeltfoundation.org']
 env.path = '/var/praekelt/vumi/ux'
 
