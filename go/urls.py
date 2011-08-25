24a24c7b7be63969a163ef7909e5fe75d899ad91
Simon de Haan <simon@praekeltfoundation.org>
2011-08-25 21:54:33 +0200

    home page for empty conversation list

--- go/urls.py
+++ go/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # django default auth 
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^', include('go.base.urls', namespace="go")),
     url(r'^todo/.*$', 'go.base.views.todo', name='todo'),
 )
