6856d6927a1c413d3770cf2823d715b0d53c8731
Simon de Haan <simon@praekeltfoundation.org>
2011-08-25 20:40:19 +0200

    run-tests.sh for jenkins

---
+++ run-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+virtualenv --no-site-packages ve && \
+source ve/bin/activate && \
+    pip install -r requirements.pip && \
+    find ./go -name '*.pyc' -delete && \
+    python go/manage.py test --with-coverage --cover-package=go --with-xunit && \
+    coverage xml --include="go/*" && \
+    (pyflakes go/ > pyflakes.log || true) && \
+    (pep8 go/ > pep8.log || true) && \
+deactivate
