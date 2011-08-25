175f708ac7c9711eb14448df749fe0705ad1d9f3
Simon de Haan <simon@praekeltfoundation.org>
2011-08-25 21:34:36 +0200

    login working

--- go/base/urls.py
+++ go/base/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 from go.base import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home),
+    url(r'^$', views.home, name="home"),
 )
