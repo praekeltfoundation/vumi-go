efa50160e696fa5ba4a2b922305590e7345f4015
Simon de Haan <simon@praekeltfoundation.org>
2011-08-25 21:02:52 +0200

    base login & logout

--- go/settings.py
+++ go/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django_nose',
+    'base',
 )
 
 # A sample logging configuration. The only tangible logging
--- go/urls.py
+++ go/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'go.views.home', name='home'),
-    # url(r'^go/', include('go.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # django admin site
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    # django default auth 
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^', include('go.base.urls', namespace="go")),
 )
