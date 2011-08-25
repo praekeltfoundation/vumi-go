2a6ac7e633ddcdb6080b066a2c4eae91034840d7
Simon de Haan <simon@praekeltfoundation.org>
2011-08-25 21:02:52 +0200

    base login & logout

---
+++ go/base/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import patterns, include, url
+from go.base import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.home),
+)
--- go/base/views.py
+++ go/base/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response
+
+@login_required
+def home(request):
+    return render_to_response('home.html', {
+    }, context_instance=RequestContext(request))
