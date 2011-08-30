f243c4de96a42506d59aea48772f131ed9ae01fd
Simon de Haan <simon@praekeltfoundation.org>
2011-08-30 15:02:28 +0200

    group creation

--- go/contacts/urls.py
+++ go/contacts/urls.py
@@ -4,11 +4,6 @@ from go.contacts import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^groups/$', views.groups, name='groups'),
+    url(r'^groups/(?P<group_pk>\d+)/$', views.group, name='group'),
     url(r'^people/$', views.people, name='people'),
-    # url(r'^new/', views.new, name='new'),
-    # url(r'^(?P<conversation_pk>\d+)/people/', views.participants,
-    #     name='participants'),
-    # url(r'^(?P<conversation_pk>\d+)/send/', views.send, name='send'),
-    # url(r'^(?P<conversation_pk>\d+)/start/', views.start, name='start'),
-    # url(r'^(?P<conversation_pk>\d+)/', views.show, name='show'),
 )
--- go/contacts/views.py
+++ go/contacts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from go.contacts import forms
+from go.contacts.models import ContactGroup
 
 
 @login_required
@@ -11,14 +13,32 @@ def index(request):
 
 @login_required
 def groups(request):
+    if request.POST:
+        new_contact_group_form = forms.NewContactGroupForm(request.POST)
+        if new_contact_group_form.is_valid():
+            group = new_contact_group_form.save(commit=False)
+            group.user = request.user
+            group.save()
+            return redirect(reverse('contacts:group', kwargs={
+                'group_pk': group.pk}))
+    else:
+        new_contact_group_form = forms.NewContactGroupForm()
+    
     groups = request.user.contactgroup_set.all()
     paginator = Paginator(groups, 5)
     page = paginator.page(request.GET.get('p', 1))
     return render(request, 'groups.html', {
         'paginator': paginator,
         'page': page,
+        'new_contact_group_form': new_contact_group_form,
     })
 
+@login_required
+def group(request, group_pk):
+    group = get_object_or_404(ContactGroup, pk=group_pk, user=request.user)
+    return render(request, 'group.html', {
+        'group': group,
+    })
 
 @login_required
 def people(request):
