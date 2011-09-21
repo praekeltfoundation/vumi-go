0619ec622359dd57ae40d7250480f93365b1b76a
Simon de Haan <simon@praekeltfoundation.org>
2011-09-21 15:35:12 +0200

    editing contacts

--- go/contacts/forms.py
+++ go/contacts/forms.py
@@ -1,9 +1,36 @@
 from django import forms
 from go.contacts import models
 
+widebox = forms.TextInput(attrs={'class': 'txtbox widebox'})
+wideselect = forms.SelectMultiple(attrs={'class': 'mult-select'})
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
+        fields = (
+            'name',
+            'surname',
+            'email_address',
+            'msisdn',
+            'dob',
+            'groups',
+            'twitter_handle',
+            'facebook_id',
+            'bbm_pin',
+            'gtalk_id',
+        )
+        widgets = {
+            'name': widebox,
+            'surname': widebox,
+            'email_address': widebox,
+            'msisdn': widebox,
+            'dob': widebox,
+            'twitter_handle': widebox,
+            'facebook_id': widebox,
+            'bbm_pin': widebox,
+            'gtalk_id': widebox,
+            'groups': wideselect,
+        }
 
 class NewContactGroupForm(forms.ModelForm):
     class Meta:
