349e9b73c17910ec3b1a6ae609e5423aeef06b87
Simon de Haan <simon@praekeltfoundation.org>
2011-09-22 14:24:44 +0200

    uploading works across the board

--- go/base/context_processors.py
+++ go/base/context_processors.py
@@ -1,6 +1,30 @@
+from go.contacts import forms
+
+
 def user_profile(request):
     if request.user.is_anonymous():
         return {}
     return {
         'user_profile': request.user.get_profile()
     }
+
+def standard_forms(request):
+    if request.user.is_anonymous():
+        return {}
+    else:
+        upload_contacts_form = forms.UploadContactsForm()
+        new_contact_group_form = forms.NewContactGroupForm()
+
+        user = request.user
+        queryset = user.contactgroup_set.all()
+
+        select_contact_group_form = forms.SelectContactGroupForm()
+        select_contact_group_form['contact_group'].queryset = queryset
+
+        return {
+            'upload_contacts_form': upload_contacts_form,
+            'new_contact_group_form': new_contact_group_form,
+            'select_contact_group_form': select_contact_group_form,
+        }
+
+    
