a8179b68d53458ec9d61bb6a1cf4eddb0d57cd57
Simon Cross <hodgestar@gmail.com>
2012-01-19 16:01:46 +0200

    More Vumi API skeleton.

---
+++ go/vumitools/api_worker.py
@@ -0,0 +1,10 @@
+# -*- test-case-name: go.vumitools.tests.test_api_worker -*-
+# -*- coding: utf-8 -*-
+
+"""Vumi application worker for the vumitools API."""
+
+from vumi.application import ApplicationWorker
+
+
+class VumiApiWorker(ApplicationWorker):
+    pass
