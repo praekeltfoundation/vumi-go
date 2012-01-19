23bc776f75bbcae4d4fe570f31e31603db35fab0
Simon Cross <hodgestar@gmail.com>
2012-01-19 17:31:09 +0200

    Start filling out API and API worker.

--- go/vumitools/api.py
+++ go/vumitools/api.py
@@ -5,4 +5,43 @@
 
 
 class VumiApi(object):
-    pass
+
+    def batch_start(self):
+        """Start a message batch.
+
+        :rtype:
+            Returns the batch_id of the new batch.
+        """
+        # create batch id in data store
+        raise NotImplementedError
+
+    def batch_send(self, batch_id, msg, addresses):
+        """Send a batch of text message to a list of addresses.
+
+        Use multiple calls to :meth:`batch_send` if you have *lots* of
+        addresses and don't want to pass them all in one API
+        call. Messages passed to multiple calls to :meth:`batch_send`
+        do not have to be the same.
+
+        :type batch_id:
+        :param batch_id:
+            batch to append the messages too
+        :type msg: unicode
+        :param msg:
+            text to send
+        :type addresses:
+        :param msg:
+            list of addresses to send messages to
+        """
+        # run celery task that sends messages to Vumi API worker
+        raise NotImplementedError
+
+    def batch_status(batch_id):
+        """Check the status of a batch of messages.
+
+        :type batch_id:
+        :param batch_id:
+            batch to check the status of
+        """
+        # retrieve information about batch from data store
+        raise NotImplementedError
