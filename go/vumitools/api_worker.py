7f64bbf4a04762047e9b4379e5365f5048f15202
Simon Cross <hodgestar@gmail.com>
2012-01-23 13:12:33 +0200

    Add consumer connected to vumi api command queue.

--- go/vumitools/api_worker.py
+++ go/vumitools/api_worker.py
@@ -3,22 +3,48 @@
 
 """Vumi application worker for the vumitools API."""
 
+from twisted.internet.defer import inlineCallbacks
+
 from vumi.application import ApplicationWorker
+
+from go.vumitools.api import VumiApiCommand
 
 
 class VumiApiWorker(ApplicationWorker):
+    """An application worker that provides a RabbitMQ and message-store
+    based API for outside applications to interact with.
+
+    Configuration parameters:
+
+    :type api_routing: dict
+    :param api_routing:
+        Dictionary describing where to consume API commands.
+    """
 
     # TODO: Vumi application worker will need to grow
     #       support for sending messages that are not
     #       replies
 
     def validate_config(self):
-        pass
+        self.api_routing_config = VumiApiCommand.default_routing_config()
+        self.api_routing_config.update(self.config.get('api_routing', {}))
+        self.api_consumer = None
 
+    @inlineCallbacks
     def setup_application(self):
-        pass
+        self.api_consumer = yield self.consume(
+            self.api_routing_config['routing_key'],
+            self.consume_api_command,
+            exchange_name=self.api_routing_config['exchange'],
+            exchange_type=self.api_routing_config['exchange_type'],
+            message_class=VumiApiCommand)
 
     def teardown_application(self):
+        if self.api_consumer is not None:
+            yield self.api_consumer.stop()
+            self.api_consumer = None
+
+    def consume_api_command(self, cmd):
         pass
 
     def consume_ack(self, event):
--- go/vumitools/tests/test_api_worker.py
+++ go/vumitools/tests/test_api_worker.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 
 """Tests for go.vumitools.api_worker."""
+
+from twisted.internet.defer import inlineCallbacks
+
+from vumi.application.tests.test_base import ApplicationTestCase
+
+from go.vumitools.api_worker import VumiApiWorker
+
+
+class TestVumiApiWorker(ApplicationTestCase):
+
+    application_class = VumiApiWorker
+
+    @inlineCallbacks
+    def setUp(self):
+        super(TestVumiApiWorker, self).setUp()
+        self.api = yield self.get_application({})
+
+    def test_send(self):
+        pass
