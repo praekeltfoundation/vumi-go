bfaa8bd56373f5ddb9aedb2973a4055aa8a07048
Simon Cross <hodgestar@gmail.com>
2012-02-01 15:59:21 +0200

    Sort out message routing.

--- go/vumitools/tests/test_api_celery.py
+++ go/vumitools/tests/test_api_celery.py
@@ -5,41 +5,29 @@
 from twisted.trial.unittest import TestCase
 
 from go.vumitools.api import VumiApiCommand
-from go.vumitools.tests.utils import setup_celery_for_tests, restore_celery
+from go.vumitools.tests.utils import CeleryTestMixIn
 
 
-class TestTasks(TestCase):
+class TestTasks(TestCase, CeleryTestMixIn):
 
     def setUp(self):
-        self._celery_setup = setup_celery_for_tests()
+        self.setup_celery_for_tests()
         from go.vumitools import api_celery
         self.api = api_celery
-        self.exchange_config = {}
+        self.exchange_config = VumiApiCommand.default_routing_config()
 
     def tearDown(self):
-        restore_celery(*self._celery_setup)
-
-    def _fetch(self, consumer):
-        msgs = []
-        while True:
-            msg = consumer.fetch()
-            if msg is not None:
-                msgs.append(msg.payload)
-            else:
-                break
-        return msgs
+        self.restore_celery()
 
     def test_batch_send_task(self):
-        consumer = self.api.batch_send_task.get_consumer(
-            self.exchange_config)
+        consumer = self.get_consumer(**self.exchange_config)
         result = self.api.batch_send_task.delay("b123", "hello world",
                                                 ["+1234", "+5678"],
                                                 self.exchange_config)
         self.assertTrue(result.successful())
         self.assertEqual(result.result, None)
 
-        [cmd1, cmd2] = [VumiApiCommand(**payload) for payload
-                        in self._fetch(consumer)]
+        [cmd1, cmd2] = self.fetch_cmds(consumer)
 
         self.assertEqual(cmd1,
                          VumiApiCommand.send("b123", "hello world", "+1234"))
