a6fd807b49661472244a5bd1f6839b42a2c24c1f
Simon Cross <hodgestar@gmail.com>
2012-02-03 16:19:14 +0200

    Import API classes into go.vumitools.__init__ for convenience.

---
+++ go/vumitools/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Vumi Go vumitools api."""
+
+from go.vumitools.api import VumiApi
+from go.vumitools.api_worker import VumiApiWorker
+
+__all__ = ['VumiApi', 'VumiApiWorker']
