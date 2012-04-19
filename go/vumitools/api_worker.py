ca40d22c2a2659d3278d59b75ab11cf9cbe901b8
Simon de Haan <simon@praekeltfoundation.org>
2012-04-19 23:30:47 +0200

    trial go/vumitools pass

--- go/vumitools/api_worker.py
+++ go/vumitools/api_worker.py
@@ -6,10 +6,113 @@
 from twisted.internet.defer import inlineCallbacks
 
 from vumi.application import ApplicationWorker, MessageStore
+from vumi.dispatchers.base import BaseDispatchRouter
+from vumi.middleware.tagger import TaggingMiddleware
 from vumi.message import TransportUserMessage
 from vumi import log
 
 from go.vumitools.api import VumiApiCommand, get_redis
+
+
+class CommandDispatcher(ApplicationWorker):
+    """
+    An application worker that forwards commands arriving on the Vumi Api
+    queue to the relevant applications. It does this by using the commands
+    worker_name parameter to construct the routing key.
+
+    Configuration parameters:
+
+    :type api_routing: dict
+    :param api_routing:
+        Dictionary describing where to consume API commands.
+    :type worker_names: list
+    :param worker_names:
+        A list of known worker names that we can forward
+        VumiApiCommands to.
+    """
+
+    def validate_config(self):
+        self.api_routing_config = VumiApiCommand.default_routing_config()
+        self.api_routing_config.update(self.config.get('api_routing', {}))
+        self.api_consumer = None
+        self.worker_names = self.config.get('worker_names', [])
+
+    @inlineCallbacks
+    def setup_application(self):
+        self.api_consumer = yield self.consume(
+            self.api_routing_config['routing_key'],
+            self.consume_api_command,
+            exchange_name=self.api_routing_config['exchange'],
+            exchange_type=self.api_routing_config['exchange_type'],
+            message_class=VumiApiCommand)
+
+        self.worker_publishers = {}
+        for worker_name in self.worker_names:
+            worker_publisher = yield self.publish_to('%s.control' % (
+                worker_name,))
+            self.worker_publishers[worker_name] = worker_publisher
+
+
+    @inlineCallbacks
+    def teardown_application(self):
+        if self.api_consumer:
+            yield self.api_consumer.stop()
+            self.api_consumer = None
+
+    @inlineCallbacks
+    def consume_api_command(self, cmd):
+        worker_name = cmd.get('worker_name')
+        publisher = self.worker_publishers.get(worker_name)
+        if publisher:
+            yield publisher.publish_message(cmd)
+        else:
+            log.error('No worker publisher available for %s' % (worker_name,))
+
+
+class GoApplicationRouter(BaseDispatchRouter):
+    """
+    Router for a dispatcher that routes messages
+    based on their tags.
+
+    """
+    def setup_routing(self):
+        self.conversation_mappings = self.config['conversation_mappings']
+
+    def get_conversation_type_for_tag(self, tag):
+        raise NotImplementedError()
+
+    def find_application_for_msg(self, msg):
+        tag = TaggingMiddleware.map_msg_to_tag(msg)
+        conv_info = self.get_conversation_for_tag(tag)
+        conv_metadata = msg['helper_metadata'].setdefault('conversations', {})
+        conv_metadata.update(conv_info)
+        conv_type = conv_metadata.get('conversation_type')
+        return self.conversation_mappings.get(conv_type)
+
+
+    @inlineCallbacks
+    def dispatch_inbound_message(self, msg):
+        application = self.find_application_for_msg(msg)
+        if application:
+            publisher = self.dispatcher.exposed_publisher[application]
+            yield publisher.publish_message(msg)
+        else:
+            log.error('No application setup for type: %s' % (
+                        msg,))
+
+    def dispatch_inbound_event(self, msg):
+        application = self.find_application_for_msg(msg)
+        if application:
+            publisher = self.dispatcher.exposed_event_publisher[application]
+            yield publisher.publish_message(msg)
+        else:
+            log.error('No application setup for type: %s' % (
+                        msg,))
+
+    def dispatch_outbound_message(self, msg):
+        name = msg['transport_name']
+        name = self.config.get('transport_mappings', {}).get(name, name)
+        self.dispatcher.transport_publisher[name].publish_message(msg)
 
 
 class VumiApiWorker(ApplicationWorker):
@@ -32,6 +135,7 @@ class VumiApiWorker(ApplicationWorker):
         # api worker
         self.api_routing_config = VumiApiCommand.default_routing_config()
         self.api_routing_config.update(self.config.get('api_routing', {}))
+        print 'api_routing', self.api_routing_config
         self.api_consumer = None
         self.applications = self.config.get('applications', {})
 
