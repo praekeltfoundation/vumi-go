5870aaad425b37e6262cef0ea13eac1269050a0b
Ruan Zeelie <ruan@unomena.com>
2012-04-22 17:55:36 +0200

    Reskin Complete

--- go/base/static/js/script.js
+++ go/base/static/js/script.js
@@ -1,18 +1,28 @@
 /* Author: Praekelt */
 
-$("#ctime").timePicker({step:10, startTime:"00:00", endTime:"23:50", show24Hours: true,separator: ':',});
-$('#cmessage').keyup(function(){
-	    	$('#charcount').text($('#cmessage').val().length);
-			if($('#cmessage').val().length>140){$('#valid-twitter').hide()}else{$('#valid-twitter').show()}
-			if($('#cmessage').val().length>160){$('#valid-sms').hide()}else{$('#valid-sms').show()}
-			$('span.preview-message').text($('#cmessage').val());
+$("#timepicker_1").timePicker({step:10, startTime:"00:00", endTime:"23:50", show24Hours: true,separator: ':',});
+$('#conv-message').keyup(function(){
+	    	$('#charcount').text($('#conv-message').val().length);
+			if($('#conv-message').val().length>140){$('#valid-twitter').hide()}else{$('#valid-twitter').show()}
+			if($('#conv-message').val().length>160){$('#valid-sms').hide()}else{$('#valid-sms').show()}
+			$('span.preview-message').text($('#conv-message').val());
 			
 });
 
 $('.typeahead').typeahead();
-$(function() { $( "#cdate" ).datepicker({ minDate: 0, dateFormat: 'dd MM yy' }); });
+
+$(function() { $( "#datepicker" ).datepicker({ minDate: 0, dateFormat: 'dd MM yy' }); });
 $(function() { $( "#dob" ).datepicker({ minDate: 0, dateFormat: 'dd MM yy' }); });
 
 $('#surveyUSSD').click(function() {
 	if($('#surveyUSSD').attr('checked')) { $('#surveyUSSDNum').show(); } else { $('#surveyUSSDNum').hide(); }
+});
+
+$('#search-filter-input').focus(function() {
+	$('input#search-filter-input').val('');
+});
+
+$('#convtype').change(function() {
+	var newAction = $('#convtype').val();
+	$('#newConversationType').attr('action', newAction);
 });
