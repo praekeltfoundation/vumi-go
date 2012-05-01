8dd209440d9266fc4338c4658e0e1c5e2ceb0ad7
Simon de Haan <simon@praekeltfoundation.org>
2012-05-02 01:02:58 +0200

    account pages

--- go/account/forms.py
+++ go/account/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 
 
+class AccountForm(forms.Form):
+    name = forms.CharField(required=True)
+    surname = forms.CharField(required=True)
+    email_address = forms.EmailField(required=True, widget=forms.TextInput(
+        attrs={'class': 'input-xlarge required email', 'autocomplete': 'off'}))
+    password = forms.CharField(
+        label='Type in a password if you want a new one.',
+        widget=forms.PasswordInput(attrs={
+            'class': 'input-xlarge',
+            'autocomplete': 'off',
+        }),
+        required=False)
+
+
 class EmailForm(forms.Form):
     subject = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
