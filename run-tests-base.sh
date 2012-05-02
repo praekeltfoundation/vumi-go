49c6fc7c68ae01b4e930cba210fdad14874ceffc
Jeremy Thurgood <jeremy@praekelt.com>
2012-05-02 19:22:07 +0200

    Remove debug echo.

--- run-tests-base.sh
+++ run-tests-base.sh
@@ -7,8 +7,6 @@ find ./go -name '*.pyc' -delete
 # These are expected to be Twisted tests that trial should run.
 vumi_tests="go/vumitools `find ./go -name 'test*vumi_app*.py'`"
 
-echo $COVERAGE_COMMAND
-
 eval $COVERAGE_COMMAND `which trial` ${vumi_tests}
 r1=$?
 eval $COVERAGE_COMMAND `which django-admin.py` test --settings=go.testsettings
