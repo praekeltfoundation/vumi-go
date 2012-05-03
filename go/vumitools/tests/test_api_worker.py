d865160cd95c6f9eeb6eb5d925aa4ca59d547850
Simon de Haan <simon@praekeltfoundation.org>
2012-05-03 22:20:00 +0200

    failing test on conversation retrieval from a batch

--- go/vumitools/tests/test_api_worker.py
+++ go/vumitools/tests/test_api_worker.py
@@ -9,9 +9,12 @@ from vumi.dispatchers.tests.test_base import DispatcherTestCase
 from vumi.dispatchers.base import BaseDispatchWorker
 from vumi.middleware.tagger import TaggingMiddleware
 from vumi.tests.utils import FakeRedis, LogCatcher
+from vumi.application.tagpool import TagpoolManager
 
 from go.vumitools.api_worker import CommandDispatcher, GoApplicationRouter
 from go.vumitools.api import VumiApiCommand
+from go.vumitools.account import AccountStore
+from go.vumitools.conversation import ConversationStore
 
 
 class CommandDispatcherTestCase(ApplicationTestCase):
@@ -82,19 +85,44 @@ class GoApplicationRouterTestCase(DispatcherTestCase):
                 'type_2': 'app_2',
             }
         })
+        self.r_server = FakeRedis()
+        self.tpm = TagpoolManager(self.r_server, 'testpool')
+        self.tpm.declare_tags([('xmpp', 'test1@xmpp.org')])
+
+        # get the router to test
         self.router = self.dispatcher._router
+        self.manager = self.router.manager
+        # monkey patch the redis server
+        self.message_store = self.router.message_store
+        self.message_store.r_server = self.r_server
+
+        self.account_store = self.router.account_store
+
+        self.account = yield self.account_store.new_user(u'user')
+        self.conversation_store = ConversationStore.from_user_account(
+                                                                self.account)
+        self.conversation = yield self.conversation_store.new_conversation(
+            u'bulk_message', u'subject', u'message')
 
     @inlineCallbacks
     def tearDown(self):
-        yield self.router.manager.purge_all()
+        yield self.manager.purge_all()
         yield super(GoApplicationRouterTestCase, self).tearDown()
 
     @inlineCallbacks
     def test_tag_retrieval_and_dispatching(self):
         msg = self.mkmsg_in(transport_type='xmpp',
-                                transport_name='xmpp_transport')
-        TaggingMiddleware.add_tag_to_msg(msg, ('xmpp', 'test1@xmpp.org'))
-        yield self.dispatch(msg, self.transport_name)
+                                transport_name=self.transport_name)
+
+        tag = self.tpm.acquire_tag('xmpp')
+        batch_id = yield self.message_store.batch_start([tag],
+            user_account=unicode(self.account.key))
+        self.conversation.batches.add_key(batch_id)
+
+        TaggingMiddleware.add_tag_to_msg(msg, tag)
+        with LogCatcher() as log:
+            yield self.dispatch(msg, self.transport_name)
+            self.assertEqual(log.errors, [])
         [dispatched] = self.get_dispatched_messages('app_1',
                                                     direction='inbound')
         conv_metadata = dispatched['helper_metadata']['conversations']
