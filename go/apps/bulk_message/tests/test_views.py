9a471d5c7b09cd304b0383e46e4909edee646050
Simon de Haan <simon@praekeltfoundation.org>
2012-05-25 23:18:35 +0200

    pyflakes fixes

--- go/apps/bulk_message/tests/test_views.py
+++ go/apps/bulk_message/tests/test_views.py
@@ -2,8 +2,6 @@ from django.test.client import Client
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-from go.vumitools.contact import ContactStore
-from go.vumitools.conversation import ConversationStore
 from go.vumitools.tests.utils import VumiApiCommand
 from go.apps.tests.base import DjangoGoApplicationTestCase
 from go.base.utils import vumi_api_for_user
