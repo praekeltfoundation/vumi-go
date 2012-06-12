9be5680e4f10f4d935f73a4c502e5b6d81f0bc8b
Simon de Haan <simon@praekeltfoundation.org>
2012-06-12 14:35:27 +0200

    deleting of groups now works

---
+++ go/contacts/tasks.py
@@ -0,0 +1,16 @@
+from celery.task import task
+
+from django.conf import settings
+
+from go.vumitools.api import VumiUserApi
+
+
+@task
+def delete_group(account_key, group_key):
+    api = VumiUserApi(account_key, settings.VUMI_API_CONFIG)
+    contact_store = api.contact_store
+    group = contact_store.get_group(group_key)
+    for contact in group.backlinks.contacts():
+        contact.groups.remove(group)
+        contact.save()
+    group.delete()
