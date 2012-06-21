cd4785c5befe70769df6abe5a738a6d355189fb4
dmaclay <dmaclay@gmail.com>
2012-06-21 15:28:34 +0200

    django files

---
+++ go/apps/opt_out/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, url
+from go.apps.opt_opt import views
+
+urlpatterns = patterns('',
+    url(r'^new/$', views.new, name='new'),
+    url(r'^(?P<conversation_key>\w+)/$', views.show, name='show'),
+    url(r'^(?P<conversation_key>\w+)/start/$', views.start, name='start'),
+    url(r'^(?P<conversation_key>\w+)/end/$', views.end, name='end'),
+)
