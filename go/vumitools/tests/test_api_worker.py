a1cc00168f48ade1603574261d242d8b849e7f12
Jeremy Thurgood <jeremy@praekelt.com>
2012-06-23 13:21:49 +0200

    Replace lookup middlewares with smarter metadata handler.

--- go/vumitools/tests/test_api_worker.py
+++ go/vumitools/tests/test_api_worker.py
@@ -2,7 +2,8 @@
 
 """Tests for go.vumitools.api_worker."""
 
-from twisted.internet.defer import inlineCallbacks
+from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 from vumi.application.tests.test_base import ApplicationTestCase
 from vumi.dispatchers.tests.test_base import DispatcherTestCase
@@ -11,8 +12,9 @@ from vumi.middleware.tagger import TaggingMiddleware
 from vumi.persist.txriak_manager import TxRiakManager
 from vumi.persist.message_store import MessageStore
 from vumi.tests.utils import FakeRedis, LogCatcher
+from vumi.message import TransportUserMessage
 
-from go.vumitools.api_worker import CommandDispatcher
+from go.vumitools.api_worker import CommandDispatcher, GoMessageMetadata
 from go.vumitools.api import VumiApiCommand
 from go.vumitools.conversation import ConversationStore
 from go.vumitools.account import AccountStore
@@ -63,6 +65,155 @@ class CommandDispatcherTestCase(ApplicationTestCase):
                                 error['message'][0])
 
 
+class GoMessageMetadataTestCase(TestCase):
+
+    @inlineCallbacks
+    def setUp(self):
+        self.mdb_prefix = 'test_message_store'
+        self.default_config = {
+            'message_store': {
+                'store_prefix': self.mdb_prefix,
+            }
+        }
+        self.r_server = FakeRedis()
+
+        self.manager = TxRiakManager.from_config({
+                'bucket_prefix': self.mdb_prefix})
+        self.account_store = AccountStore(self.manager)
+        self.message_store = MessageStore(self.manager, self.r_server,
+                                            self.mdb_prefix)
+
+        self.account = yield self.account_store.new_user(u'user')
+        self.conversation_store = ConversationStore.from_user_account(
+                                                                self.account)
+        self.tag = ('xmpp', 'test1@xmpp.org')
+
+    @inlineCallbacks
+    def tearDown(self):
+        yield self.manager.purge_all()
+
+    @inlineCallbacks
+    def create_conversation(self, conversation_type=u'bulk_message',
+        subject=u'subject', message=u'message'):
+        conversation = yield self.conversation_store.new_conversation(
+            conversation_type, subject, message)
+        returnValue(conversation)
+
+    @inlineCallbacks
+    def tag_conversation(self, conversation, tag):
+        batch_id = yield self.message_store.batch_start([tag],
+                            user_account=unicode(self.account.key))
+        conversation.batches.add_key(batch_id)
+        conversation.save()
+        returnValue(batch_id)
+
+    def mk_msg(self, to_addr, from_addr):
+        return TransportUserMessage(to_addr=to_addr, from_addr=from_addr,
+                                   transport_name="dummy_endpoint",
+                                   transport_type="dummy_transport_type")
+
+    def mk_md(self, message):
+        return GoMessageMetadata(self.message_store, message)
+
+    @inlineCallbacks
+    def test_account_key_lookup(self):
+        conversation = yield self.create_conversation()
+        batch_key = yield self.tag_conversation(conversation, self.tag)
+        msg = self.mk_msg('to@domain.org', 'from@domain.org')
+        TaggingMiddleware.add_tag_to_msg(msg, self.tag)
+
+        self.assertEqual(msg['helper_metadata'],
+                         {'tag': {'tag': list(self.tag)}})
+
+        md = self.mk_md(msg)
+        # The metadata wrapper creates the 'go' metadata
+        self.assertEqual(msg['helper_metadata']['go'], {})
+
+        account_key = yield md.get_account_key()
+        self.assertEqual(account_key, self.account.key)
+        self.assertEqual(msg['helper_metadata']['go'], {
+                'batch_key': batch_key,
+                'user_account': account_key,
+                })
+
+    @inlineCallbacks
+    def test_batch_lookup(self):
+        conversation = yield self.create_conversation()
+        batch_key = yield self.tag_conversation(conversation, self.tag)
+        msg = self.mk_msg('to@domain.org', 'from@domain.org')
+        TaggingMiddleware.add_tag_to_msg(msg, self.tag)
+
+        self.assertEqual(msg['helper_metadata'],
+                         {'tag': {'tag': list(self.tag)}})
+
+        md = self.mk_md(msg)
+        # The metadata wrapper creates the 'go' metadata
+        self.assertEqual(msg['helper_metadata']['go'], {})
+
+        msg_batch_key = yield md.get_batch_key()
+        self.assertEqual(batch_key, msg_batch_key)
+        self.assertEqual(msg['helper_metadata']['go'],
+                         {'batch_key': batch_key})
+
+    @inlineCallbacks
+    def test_conversation_lookup(self):
+        conversation = yield self.create_conversation()
+        batch_key = yield self.tag_conversation(conversation, self.tag)
+        msg = self.mk_msg('to@domain.org', 'from@domain.org')
+        TaggingMiddleware.add_tag_to_msg(msg, self.tag)
+
+        self.assertEqual(msg['helper_metadata'],
+                         {'tag': {'tag': list(self.tag)}})
+
+        md = self.mk_md(msg)
+        # The metadata wrapper creates the 'go' metadata
+        self.assertEqual(msg['helper_metadata']['go'], {})
+
+        conv_key, conv_type = yield md.get_conversation_info()
+        self.assertEqual(conv_key, conversation.key)
+        self.assertEqual(conv_type, conversation.conversation_type)
+        self.assertEqual(msg['helper_metadata']['go'], {
+                'batch_key': batch_key,
+                'user_account': self.account.key,
+                'conversation_key': conv_key,
+                'conversation_type': conv_type,
+                })
+
+    @inlineCallbacks
+    def test_rewrap(self):
+        conversation = yield self.create_conversation()
+        batch_key = yield self.tag_conversation(conversation, self.tag)
+        msg = self.mk_msg('to@domain.org', 'from@domain.org')
+        TaggingMiddleware.add_tag_to_msg(msg, self.tag)
+
+        self.assertEqual(msg['helper_metadata'],
+                         {'tag': {'tag': list(self.tag)}})
+
+        md = self.mk_md(msg)
+        # The metadata wrapper creates the 'go' metadata
+        self.assertEqual(msg['helper_metadata']['go'], {})
+
+        msg_batch_key = yield md.get_batch_key()
+        self.assertEqual(batch_key, msg_batch_key)
+        self.assertEqual(msg['helper_metadata']['go'],
+                         {'batch_key': batch_key})
+
+        # We create a new wrapper around the same message object and make sure
+        # the cached message store objects are still there in the new one.
+        new_md = self.mk_md(msg)
+        self.assertNotEqual(md, new_md)
+        self.assertEqual(md._store_objects, new_md._store_objects)
+        self.assertEqual(md._go_metadata, new_md._go_metadata)
+
+        # We create a new wrapper around the a copy of the message object and
+        # make sure the message store object cache is empty, but the metadata
+        # remains.
+        other_md = self.mk_md(msg.copy())
+        self.assertNotEqual(md, other_md)
+        self.assertEqual({}, other_md._store_objects)
+        self.assertEqual(md._go_metadata, other_md._go_metadata)
+
+
 class GoApplicationRouterTestCase(DispatcherTestCase):
 
     dispatcher_class = BaseDispatchWorker
@@ -74,11 +225,6 @@ class GoApplicationRouterTestCase(DispatcherTestCase):
         yield super(GoApplicationRouterTestCase, self).setUp()
         self.r_server = FakeRedis()
         self.mdb_prefix = 'test_message_store'
-        self.message_store_config = {
-            'message_store': {
-                'store_prefix': self.mdb_prefix,
-            }
-        }
         self.dispatcher = yield self.get_dispatcher({
             'router_class': 'go.vumitools.api_worker.GoApplicationRouter',
             'redis_clr': lambda: self.r_server,
@@ -97,21 +243,15 @@ class GoApplicationRouterTestCase(DispatcherTestCase):
                 'survey': 'app_2',
             },
             'middleware': [
-                {'account_mw':
-                    'go.vumitools.middleware.LookupAccountMiddleware'},
-                {'batch_mw':
-                    'go.vumitools.middleware.LookupBatchMiddleware'},
-                {'conversation_mw':
-                    'go.vumitools.middleware.LookupConversationMiddleware'},
                 {'optout_mw':
                     'go.vumitools.middleware.OptOutMiddleware'},
             ],
-            'account_mw': self.message_store_config,
-            'batch_mw': self.message_store_config,
-            'conversation_mw': self.message_store_config,
             'optout_mw': {
                 'optout_keywords': ['stop']
-            }
+            },
+            'message_store': {
+                'store_prefix': self.mdb_prefix,
+            },
         })
 
         # get the router to test
@@ -141,7 +281,7 @@ class GoApplicationRouterTestCase(DispatcherTestCase):
         batch_id = yield self.message_store.batch_start([tag],
             user_account=unicode(self.account.key))
         self.conversation.batches.add_key(batch_id)
-        self.conversation.save()
+        yield self.conversation.save()
 
         TaggingMiddleware.add_tag_to_msg(msg, tag)
         with LogCatcher() as log:
@@ -149,11 +289,11 @@ class GoApplicationRouterTestCase(DispatcherTestCase):
             self.assertEqual(log.errors, [])
         [dispatched] = self.get_dispatched_messages('app_1',
                                                     direction='inbound')
-        conv_metadata = dispatched['helper_metadata']['conversations']
-        self.assertEqual(conv_metadata, {
-            'conversation_key': self.conversation.key,
-            'conversation_type': self.conversation.conversation_type,
-        })
+        go_metadata = dispatched['helper_metadata']['go']
+        self.assertEqual(go_metadata['conversation_type'],
+                         self.conversation.conversation_type)
+        self.assertEqual(go_metadata['conversation_key'],
+                         self.conversation.key)
 
     @inlineCallbacks
     def test_no_tag(self):
