81e79ed669300b2b674e97af09828b31f5645433
dmaclay <dmaclay@gmail.com>
2012-07-06 17:58:50 +0200

    keep link to dispatcher

--- go/vumitools/handler.py
+++ go/vumitools/handler.py
@@ -1,9 +1,12 @@
 
 
 from vumi import log
+from go.vumitools.api import VumiApiCommand
+
 
 class EventHandler(object):
-    def __init__(self, config):
+    def __init__(self, dispatcher, config):
+        self.dispatcher = dispatcher
         self.config = config
 
     def setup_handler(self):
@@ -11,5 +14,27 @@ class EventHandler(object):
 
     def handle_event(self, event, handler_config):
         raise NotImplementedError()
+
+
+class LoggingHandler(EventHandler):
+
+    def handle_event(self, event, handler_config):
+        log.info("LoggingHandler handling event: %s with config: %s" % (
+            event, handler_config))
+
+class SendMessageCommandHandler(EventHandler):
+
+    def handle_event(self, event, handler_config):
+        log.info(
+            "SendMessageCommandHandler handling event: %s with config: %s" % (
+            event, handler_config))
+        sm_cmd = VumiApiCommand.command(
+                handler_config['worker_name'],
+                "send_message",
+                send_message = event.payload['content'],
+                conversation_key=handler_config['conversation.key'],
+                )
+        #print sm_cmd
+        self.dispatcher.api_command_publisher.publish_message(sm_cmd)
 
 
