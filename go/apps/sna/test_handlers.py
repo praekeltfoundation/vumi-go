649702e817b1705d3e7e51395e3df06cfd72f793
Simon de Haan <simon@praekeltfoundation.org>
2012-08-15 00:44:22 +0200

    sna & afrop tests pass, now with event setup

--- go/apps/sna/test_handlers.py
+++ go/apps/sna/test_handlers.py
@@ -1,65 +1,10 @@
-from go.vumitools.api_worker import EventDispatcher
-from go.vumitools.tests.utils import AppWorkerTestCase
 from go.vumitools.opt_out import OptOutStore
-from go.vumitools.api import VumiApiEvent, VumiUserApi
+from go.vumitools.tests.test_handler import EventHandlerTestCase
 
 from twisted.internet.defer import inlineCallbacks
-from twisted.internet.base import DelayedCall
-# DelayedCall.debug = True
 
 from vxpolls.manager import PollManager
 
-
-class EventHandlerTestCase(AppWorkerTestCase):
-
-    application_class = EventDispatcher
-    handlers = None
-
-    @inlineCallbacks
-    def setUp(self):
-        yield super(EventHandlerTestCase, self).setUp()
-
-        app_config = {
-            'event_handlers': {},
-        }
-        for name, handler_class, config in self.handlers:
-            app_config['event_handlers'][name] = handler_class
-            app_config[name] = config
-
-        self.event_dispatcher = yield self.get_application(app_config)
-        self.vumi_api = self.event_dispatcher.vumi_api
-        self.account = yield self.vumi_api.account_store.new_user(u'acct')
-        self.user_api = VumiUserApi(self.vumi_api, self.account.key)
-        self.conversation = yield self.user_api.new_conversation(
-            u'survey', u'subject', u'message',
-            delivery_tag_pool=u'pool',
-            delivery_class=u'sms')
-
-    @inlineCallbacks
-    def tearDown(self):
-        yield super(EventHandlerTestCase, self).tearDown()
-        yield self.event_dispatcher.teardown_application()
-
-    def publish_event(self, event):
-        return self.dispatch(event, rkey='vumi.event')
-
-    def mkevent(self, event_type, content, conv_key=None,
-                account_key=None):
-        return VumiApiEvent.event(
-            account_key or self.account.key,
-            conv_key or self.conversation.key,
-            event_type, content)
-
-    def track_event(self, account_key, conversation_key, event_type,
-                        handler_name, handler_config={}):
-        handler_configs = self.event_dispatcher.account_handler_configs
-        account_handlers = handler_configs.setdefault(account_key, [])
-
-        account_handlers.append([
-            [conversation_key, event_type], [
-                [handler_name, handler_config]
-            ]
-        ])
 
 class USSDOptOutHandlerTestCase(EventHandlerTestCase):
 
