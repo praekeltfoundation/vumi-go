f589d2ad07de89e2d9b74d8c27f548e8006ddd89
Jeremy Thurgood <jeremy@praekelt.com>
2012-09-10 16:16:01 +0200

    Prototype unified app views.

--- go/apps/bulk_message/urls.py
+++ go/apps/bulk_message/urls.py
@@ -1,10 +1,4 @@
-from django.conf.urls.defaults import patterns, url
-from go.apps.bulk_message import views
+from django.conf.urls.defaults import patterns
+from go.apps.bulk_message.views import BulkSendConversationViews
 
-urlpatterns = patterns('',
-    url(r'^new/', views.new, name='new'),
-    url(r'^(?P<conversation_key>\w+)/people/', views.people, name='people'),
-    url(r'^(?P<conversation_key>\w+)/send/', views.send, name='send'),
-    url(r'^(?P<conversation_key>\w+)/end/', views.end, name='end'),
-    url(r'^(?P<conversation_key>\w+)/', views.show, name='show'),
-)
+urlpatterns = patterns('', *BulkSendConversationViews().urls())
