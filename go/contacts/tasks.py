777a5fc0b8ea9034f22c9088b71ff0f35ca35c33
Simon de Haan <simon@praekeltfoundation.org>
2012-09-11 11:14:04 +0200

    offloading CSV processing to Celery

--- go/contacts/tasks.py
+++ go/contacts/tasks.py
@@ -1,8 +1,11 @@
+import os.path
+
 from celery.task import task
 
 from django.conf import settings
 
 from go.vumitools.api import VumiUserApi
+from go.contacts.utils import parse_contacts_csv_file
 
 
 @task(ignore_result=True)
@@ -20,3 +23,26 @@ def delete_group(account_key, group_key):
         contact.groups.remove(group)
         contact.save()
     group.delete()
+
+@task(ignore_result=True)
+def import_csv_file(account_key, group_key, csv_path, field_names, has_header):
+    print 'importing csv file'
+    api = VumiUserApi.from_config(account_key, settings.VUMI_API_CONFIG)
+    contact_store = api.contact_store
+    group = contact_store.get_group(group_key)
+
+    # open in Universal mode to allow us to read files with Windows,
+    # MacOS9 & Unix line-endings
+    full_path = os.path.join(settings.MEDIA_ROOT, csv_path)
+
+    with open(full_path, 'rU') as csv_file:
+        for csv_data in parse_contacts_csv_file(csv_file, field_names,
+                                            has_header):
+            [count, contact_dictionary] = csv_data
+
+            # Make sure we set this group they're being uploaded in to
+            groups = contact_dictionary.setdefault('groups', [])
+            groups.append(group.key)
+
+            contact_store.new_contact(**contact_dictionary)
+    print 'done'
