a9152d3b5aac45a902ccb413e5e988db16762e2a
Jeremy Thurgood <jeremy@praekelt.com>
2012-09-12 16:23:39 +0200

    Better URL pattern generation.

--- go/apps/bulk_message/urls.py
+++ go/apps/bulk_message/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls.defaults import patterns
 from go.apps.bulk_message.views import BulkSendConversationViews
 
-urlpatterns = patterns('', *BulkSendConversationViews().urls())
+urlpatterns = BulkSendConversationViews().get_urlpatterns()
