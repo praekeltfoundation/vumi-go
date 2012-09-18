8e1d50fd8e40df1ebfd6fb3cb1763d69fb015c3f
Simon de Haan <simon@praekeltfoundation.org>
2012-09-18 15:47:45 +0200

    bootstrapping files

--- go/account/forms.py
+++ go/account/forms.py
@@ -1,22 +1,23 @@
 from django import forms
 from registration.forms import RegistrationFormUniqueEmail
 
+from bootstrap.forms import BootstrapMixin, BootstrapForm
 
-class AccountForm(forms.Form):
+
+class AccountForm(BootstrapForm):
     name = forms.CharField(required=True)
     surname = forms.CharField(required=True)
     email_address = forms.EmailField(required=True, widget=forms.TextInput(
-        attrs={'class': 'input-xlarge required email', 'autocomplete': 'off'}))
+        attrs={'autocomplete': 'off'}))
     password = forms.CharField(
         label='Type in a password if you want a new one.',
         widget=forms.PasswordInput(attrs={
-            'class': 'input-xlarge',
             'autocomplete': 'off',
         }),
         required=False)
 
 
-class RegistrationForm(RegistrationFormUniqueEmail):
+class RegistrationForm(BootstrapMixin, RegistrationFormUniqueEmail):
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         del self.fields['username']
@@ -27,6 +28,6 @@ class RegistrationForm(RegistrationFormUniqueEmail):
         return self.cleaned_data
 
 
-class EmailForm(forms.Form):
+class EmailForm(BootstrapForm):
     subject = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
