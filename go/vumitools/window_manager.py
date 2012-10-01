51d15a95d5f2146b0bbdbece9990df02da79d67f
Simon de Haan <simon@praekeltfoundation.org>
2012-10-01 15:03:38 +0200

    moar tests

--- go/vumitools/window_manager.py
+++ go/vumitools/window_manager.py
@@ -1,7 +1,6 @@
 # -*- test-case-name: go.vumitools.tests.test_window_manager -*-
 import json
 import uuid
-import time
 
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.task import LoopingCall
@@ -17,16 +16,24 @@ class WindowManager(object):
     FLIGHT_STATS_KEY = 'flightstats'
 
     def __init__(self, redis, name='window_manager', window_size=100,
-        flight_lifetime=10, max_flight_retries=10):
+        flight_lifetime=10, max_flight_retries=10, gc_interval=10):
         self.name = name
         self.window_size = window_size
-        self.flight_lifetime = 10
+        self.flight_lifetime = flight_lifetime
         self.max_flight_retries = max_flight_retries
         self.redis = redis.sub_manager(self.name)
         self.gc = LoopingCall(self.clear_or_retry_flight_keys)
+        self.gc.start(gc_interval)
+
+    def stop(self):
+        if self.gc.running:
+            self.gc.stop()
 
     def get_windows(self):
         return self.redis.zrange(self.window_key(), 0, -1)
+
+    def window_exists(self, window_id):
+        return self.redis.zscore(self.window_key(), window_id)
 
     def window_key(self, *keys):
         return ':'.join([self.WINDOW_KEY] + map(str, keys))
@@ -37,11 +44,16 @@ class WindowManager(object):
     def stats_key(self, *keys):
         return self.window_key(self.FLIGHT_STATS_KEY, *keys)
 
+    def get_clocktime(self):
+        return self.gc.clock.seconds()
+
     @inlineCallbacks
     def create(self, window_id):
-        if (yield self.redis.zscore(self.WINDOW_KEY, window_id)):
+        if (yield self.window_exists(window_id)):
             raise WindowException('Window already exists: %s' % (window_id,))
-        yield self.redis.zadd(self.WINDOW_KEY, **{window_id: time.time()})
+        yield self.redis.zadd(self.WINDOW_KEY, **{
+            window_id: self.get_clocktime(),
+            })
 
     @inlineCallbacks
     def remove_window(self, window_id):
@@ -56,12 +68,13 @@ class WindowManager(object):
         yield self.redis.lpush(self.window_key(window_id), key)
         yield self.redis.set(self.window_key(window_id, key),
             json.dumps(data))
+        returnValue(key)
 
     @inlineCallbacks
     def next(self, window_id):
 
         window_key = self.window_key(window_id)
-        flight_key = self.flight_key(window_id)
+        inflight_key = self.flight_key(window_id)
 
         waiting_list = yield self.count_waiting(window_id)
         if waiting_list == 0:
@@ -71,17 +84,25 @@ class WindowManager(object):
         room_available = self.window_size - flight_size
 
         if room_available:
-            next_key = yield self.redis.rpoplpush(window_key, flight_key)
-            yield self.set_timestamp(window_id, flight_key)
-            yield self.increment_tries(window_id, flight_key)
+            next_key = yield self.redis.rpoplpush(window_key, inflight_key)
+            yield self.set_timestamp(window_id, next_key)
+            yield self.increment_tries(window_id, next_key)
             returnValue(next_key)
         else:
             returnValue(None)
 
     def set_timestamp(self, window_id, flight_key):
         return self.redis.zadd(self.stats_key(window_id), **{
-                flight_key: time.time()
+                flight_key: self.get_clocktime(),
         })
+
+    def clear_timestamp(self, window_id, flight_key):
+        return self.redis.zrem(self.stats_key(window_id), flight_key)
+
+    @inlineCallbacks
+    def get_tries(self, window_id, flight_key):
+        tries = yield self.redis.get(self.stats_key(window_id, flight_key))
+        returnValue(int(tries or 0))
 
     def increment_tries(self, window_id, flight_key):
         return self.redis.incr(self.stats_key(window_id, flight_key))
@@ -94,21 +115,24 @@ class WindowManager(object):
         flight_key = self.flight_key(window_id)
         return self.redis.llen(flight_key)
 
+    def get_expired_flight_keys(self, window_id):
+        return self.redis.zrangebyscore(self.stats_key(window_id),
+            '-inf', self.get_clocktime() - self.flight_lifetime)
+
     @inlineCallbacks
     def clear_or_retry_flight_keys(self):
         windows = yield self.get_windows()
         for window_id in windows:
-            expired_keys = yield self.redis.zcardbyscore(
-                self.stats_key(window_id), '-inf',
-                time.time() - self.flight_lifetime)
+            expired_keys = yield self.get_expired_flight_keys(window_id)
             for key in expired_keys:
                 tries = yield self.increment_tries(window_id, key)
                 window_key = self.window_key(window_id)
-                flight_key = self.flight_key(window_id)
+                inflight_key = self.flight_key(window_id)
                 if tries < self.max_flight_retries:
-                    yield self.redis.rpoplpush(flight_key, window_key)
+                    yield self.redis.rpoplpush(inflight_key, window_key)
+                    yield self.clear_timestamp(window_id, key)
                 else:
-                    yield self.redis.remove(window_id, key)
+                    yield self.remove(window_id, key)
 
     @inlineCallbacks
     def get(self, window_id, key):
@@ -119,3 +143,4 @@ class WindowManager(object):
     def remove(self, window_id, key):
         yield self.redis.lrem(self.flight_key(window_id), key, 1)
         yield self.redis.delete(self.window_key(window_id, key))
+        yield self.redis.delete(self.stats_key(window_id, key))
