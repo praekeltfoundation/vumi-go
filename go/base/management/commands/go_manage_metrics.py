eb3bed22a180a642b0e7afe0f2da9c35d454b7b7
Jeremy Thurgood <jeremy@praekelt.com>
2012-10-17 15:27:54 +0200

    Management command to enable/disable metrics collection for an account.

---
+++ go/base/management/commands/go_manage_metrics.py
@@ -0,0 +1,62 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+from go.base.utils import vumi_api_for_user
+
+
+class Command(BaseCommand):
+    help = "Enable or disable metrics for a Vumi Go user"
+
+    LOCAL_OPTIONS = [
+        make_option('--email-address',
+            dest='email-address',
+            help='Email address for the Vumi Go user'),
+        make_option('--enable',
+            dest='enable',
+            action='store_true',
+            default=False,
+            help='Give access to this application'),
+        make_option('--disable',
+            dest='disable',
+            action='store_true',
+            default=False,
+            help='Revoke access to this application'),
+    ]
+    option_list = BaseCommand.option_list + tuple(LOCAL_OPTIONS)
+
+    def handle(self, *args, **options):
+        options = options.copy()
+        for opt in self.LOCAL_OPTIONS:
+            if options.get(opt.dest) is None:
+                value = raw_input("%s: " % (opt.help,))
+                if value:
+                    options[opt.dest] = value
+                else:
+                    raise CommandError('Please provide %s:' % (opt.dest,))
+
+        self.handle_validated(*args, **options)
+
+    def handle_validated(self, *args, **options):
+        try:
+            email_address = options['email-address']
+            enable = options['enable']
+            disable = options['disable']
+
+            if enable and disable:
+                raise CommandError('Please provide specify --enable or '
+                    '--disable, not both.')
+
+            user = User.objects.get(username=email_address)
+            user_api = vumi_api_for_user(user)
+            user_account_key = user_api.user_account_key
+            redis = user_api.api.redis
+
+            if enable:
+                redis.sadd('metrics_accounts', user_account_key)
+            elif disable:
+                redis.srem('metrics_accounts', user_account_key)
+
+        except User.DoesNotExist, e:
+            raise CommandError(e)
