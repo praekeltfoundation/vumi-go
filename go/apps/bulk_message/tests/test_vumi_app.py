92199984b08ba5b3d82ba93aa77360f9887ac8de
Simon de Haan <simon@praekeltfoundation.org>
2012-10-30 18:29:09 +0200

    cleanup of tests and better tests for nack/ack handling

--- go/apps/bulk_message/tests/test_vumi_app.py
+++ go/apps/bulk_message/tests/test_vumi_app.py
@@ -2,7 +2,7 @@
 
 """Tests for go.vumitools.bulk_send_application"""
 
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.task import Clock
 
 from vumi.message import TransportUserMessage
@@ -33,6 +33,7 @@ class TestBulkMessageApplication(AppWorkerTestCase):
 
         # Create a test user account
         self.user_account = yield self.mk_user(self.vumi_api, u'testuser')
+        self.message_store = self.vumi_api.mdb
         self.user_api = VumiUserApi(self.vumi_api, self.user_account.key)
 
         yield self.user_api.api.declare_tags([("pool", "tag1"),
@@ -45,40 +46,34 @@ class TestBulkMessageApplication(AppWorkerTestCase):
         return self.vumi_api.mdb.add_outbound_message(self.mkmsg_out(**kw))
 
     @inlineCallbacks
-    def test_start(self):
+    def setup_conversation(self, contact_count=2, group_name=u'test group',
+        conversation_type=u'bulk_message', subject=u'subject',
+        message=u'message', delivery_tag_pool=u'pool', delivery_class=u'sms',
+        from_addr=u'+27831234567{0}'):
         user_api = self.user_api
-        group = yield user_api.contact_store.new_group(u'test group')
-        contact1 = yield user_api.contact_store.new_contact(
-            name=u'First', surname=u'Contact', msisdn=u'27831234567',
-            groups=[group])
-        contact2 = yield user_api.contact_store.new_contact(
-            name=u'Second', surname=u'Contact', msisdn=u'27831234568',
-            groups=[group])
+        group = yield user_api.contact_store.new_group(group_name)
+
+        for i in range(contact_count):
+            yield user_api.contact_store.new_contact(
+                name=u'First', surname=u'Surname %s' % (i,),
+                msisdn=from_addr.format(i), groups=[group])
+
         conversation = yield user_api.new_conversation(
-            u'bulk_message', u'Subject', u'Message', delivery_tag_pool=u"pool",
-            delivery_class=u'sms')
+            conversation_type, subject, message,
+            delivery_tag_pool=delivery_tag_pool, delivery_class=delivery_class)
         conversation.add_group(group)
         yield conversation.save()
         conversation = user_api.wrap_conversation(conversation)
+        returnValue(conversation)
 
+    @inlineCallbacks
+    def test_start(self):
+
+        conversation = yield self.setup_conversation()
         yield self.start_conversation(conversation)
-
-        # batch_id
-        [batch_id] = conversation.batches.keys()
-
-        # check commands made it through to the dispatcher and the vumi_app
-        [disp_cmd] = self.get_dispatcher_commands()
-        self.assertEqual(disp_cmd['command'], 'start')
-        [bulk_cmd] = self.get_app_message_commands()
-        self.assertEqual(bulk_cmd['command'], 'start')
 
         # Force processing of messages
         yield self._amqp.kick_delivery()
-
-        # Assert that the messages are in the window managers' flight window
-        window_id = self.app.get_window_id(conversation.key, batch_id)
-        self.assertEqual(
-            (yield self.app.window_manager.count_waiting(window_id)), 2)
 
         # Go past the monitoring interval to ensure the window is
         # being worked through for delivery
@@ -89,60 +84,19 @@ class TestBulkMessageApplication(AppWorkerTestCase):
         msgs.sort(key=lambda msg: msg['to_addr'])
         [msg1, msg2] = msgs
 
-        # Create an ack and a nack for the messages
-        ack = self.mkmsg_ack(user_message_id=msg1['message_id'],
-            sent_message_id=msg1['message_id'])
-        nack = self.mkmsg_nack(user_message_id=msg2['message_id'],
-            nack_reason='unknown')
-
-        yield self.dispatch_event(ack)
-        yield self.dispatch_event(nack)
-
-        # Assert that the window's now empty because acks have been received
-        self.assertEqual(
-            (yield self.app.window_manager.count_in_flight(window_id)), 0)
-        self.assertEqual(
-            (yield self.app.window_manager.count_waiting(window_id)), 0)
-
         # check that the right to_addr & from_addr are set and that the content
         # of the message equals conversation.message
+        [contact1, contact2] = sorted(
+            (yield conversation.get_opted_in_contacts()),
+            key=lambda contact: contact.msisdn)
         self.assertEqual(msg1['to_addr'], contact1.msisdn)
         self.assertEqual(msg2['to_addr'], contact2.msisdn)
 
-        # check tags and user accounts
-        for msg in msgs:
-            tag = msg['helper_metadata']['tag']['tag']
-            user_account_key = msg['helper_metadata']['go']['user_account']
-            self.assertEqual(tag, ["pool", "tag1"])
-            self.assertEqual(user_account_key, self.user_account.key)
-
-        batch_status = yield self.vumi_api.mdb.batch_status(batch_id)
-        self.assertEqual(batch_status['sent'], 2)
-        dbmsgs = yield self.vumi_api.mdb.batch_messages(batch_id)
-        dbmsgs.sort(key=lambda msg: msg['to_addr'])
-        [dbmsg1, dbmsg2] = dbmsgs
-        self.assertEqual(dbmsg1, msg1)
-        self.assertEqual(dbmsg2, msg2)
-
     @inlineCallbacks
     def test_start_with_deduplication(self):
-        user_api = self.user_api
-        group = yield user_api.contact_store.new_group(u'test group')
-
         # Create two contacts with the same to_addr, they should be deduped
-
-        contact1 = yield user_api.contact_store.new_contact(
-            name=u'First', surname=u'Contact', msisdn=u'27831234567',
-            groups=[group])
-        contact2 = yield user_api.contact_store.new_contact(
-            name=u'Second', surname=u'Contact', msisdn=u'27831234567',
-            groups=[group])
-        conversation = yield user_api.new_conversation(
-            u'bulk_message', u'Subject', u'Message', delivery_tag_pool=u"pool",
-            delivery_class=u'sms')
-        conversation.add_group(group)
-        yield conversation.save()
-        conversation = user_api.wrap_conversation(conversation)
+        conversation = yield self.setup_conversation(from_addr=u'27831234567',
+            contact_count=2)
 
         # Provide the dedupe option to the conversation
         yield self.start_conversation(conversation, dedupe=True)
@@ -161,40 +115,42 @@ class TestBulkMessageApplication(AppWorkerTestCase):
 
         # check that the right to_addr & from_addr are set and that the content
         # of the message equals conversation.message
+        [contact1, contact2] = yield conversation.get_opted_in_contacts()
         self.assertEqual(msg['to_addr'], contact1.msisdn)
         self.assertEqual(msg['to_addr'], contact2.msisdn)
 
     @inlineCallbacks
     def test_consume_ack(self):
-        yield self.store_outbound(message_id='123')
-        ack_event = yield self.publish_event(user_message_id='123',
-                                             event_type='ack',
-                                             sent_message_id='xyz')
-        [event] = yield self.vumi_api.mdb.message_events('123')
-        self.assertEqual(event, ack_event)
+        conversation = yield self.setup_conversation()
+        yield self.start_conversation(conversation)
+        batch_id = conversation.get_latest_batch_key()
+        window_id = self.app.get_window_id(conversation.key, batch_id)
+        yield self._amqp.kick_delivery()
+        self.clock.advance(self.app.monitor_interval + 1)
 
-    @inlineCallbacks
-    def test_consume_delivery_report(self):
-        yield self.store_outbound(message_id='123')
-        dr_event = yield self.publish_event(user_message_id='123',
-                                            event_type='delivery_report',
-                                            delivery_status='delivered')
-        [event] = yield self.vumi_api.mdb.message_events('123')
-        self.assertEqual(event, dr_event)
+        [msg1, msg2] = yield self.get_dispatched_messages()
+        yield self.message_store.add_outbound_message(
+            TransportUserMessage(**msg1.payload), batch_id=batch_id)
+        yield self.message_store.add_outbound_message(
+            TransportUserMessage(**msg2.payload), batch_id=batch_id)
 
-    @inlineCallbacks
-    def test_consume_user_message(self):
-        msg = self.mkmsg_in()
-        yield self.dispatch(msg)
-        dbmsg = yield self.vumi_api.mdb.get_inbound_message(msg['message_id'])
-        self.assertEqual(dbmsg, msg)
+        # We should have two in flight
+        self.assertEqual(
+            (yield self.app.window_manager.count_in_flight(window_id)), 2)
 
-    @inlineCallbacks
-    def test_close_session(self):
-        msg = self.mkmsg_in(session_event=TransportUserMessage.SESSION_CLOSE)
-        yield self.dispatch(msg)
-        dbmsg = yield self.vumi_api.mdb.get_inbound_message(msg['message_id'])
-        self.assertEqual(dbmsg, msg)
+        # Create an ack and a nack for the messages
+        ack = self.mkmsg_ack(user_message_id=msg1['message_id'],
+            sent_message_id=msg1['message_id'])
+        yield self.dispatch_event(ack)
+        nack = self.mkmsg_nack(user_message_id=msg2['message_id'],
+            nack_reason='unknown')
+        yield self.dispatch_event(nack)
+
+        yield self._amqp.kick_delivery()
+
+        # We should have zero in flight
+        self.assertEqual(
+            (yield self.app.window_manager.count_in_flight(window_id)), 0)
 
     @inlineCallbacks
     def test_send_message_command(self):
