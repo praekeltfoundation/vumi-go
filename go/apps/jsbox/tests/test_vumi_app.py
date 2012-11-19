709b44543c18c213acc1ed58632dbd31af1836d7
Simon Cross <hodgestar@gmail.com>
2012-11-19 15:09:12 +0200

    Fix-up test so that it passes.

--- go/apps/jsbox/tests/test_vumi_app.py
+++ go/apps/jsbox/tests/test_vumi_app.py
@@ -1,30 +1,57 @@
 # -*- coding: utf-8 -*-
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 from go.vumitools.tests.utils import AppWorkerTestCase
 
 from go.apps.jsbox.vumi_app import JsBoxApplication
 
 
-class JsBoxApplication(AppWorkerTestCase):
+class JsBoxApplicationTestCase(AppWorkerTestCase):
 
     use_riak = True
     application_class = JsBoxApplication
 
     @inlineCallbacks
     def setUp(self):
-        yield super(JsBoxApplication, self).setUp()
-        self.config = self.mk_config({
-            'metrics_prefix': 'jsbox_metrics',
-            })
+        yield super(JsBoxApplicationTestCase, self).setUp()
+        self.config = self.mk_config({})
         self.app = yield self.get_application(self.config)
+
+        # Steal app's vumi_api
+        self.vumi_api = self.app.vumi_api  # YOINK!
+        self.message_store = self.vumi_api.mdb
+
+        # Create a test user account
+        self.user_account = yield self.mk_user(self.vumi_api, u'testuser')
+        self.user_api = self.vumi_api.get_user_api(self.user_account.key)
+
+        yield self.user_api.api.declare_tags([("pool", "tag1"),
+                                              ("pool", "tag2")])
+        yield self.user_api.api.set_pool_metadata("pool", {
+            "transport_type": "sphex",
+            })
+
+    @inlineCallbacks
+    def setup_conversation(self, contact_count=2,
+                            from_addr=u'+27831234567{0}'):
+        user_api = self.user_api
+        group = yield user_api.contact_store.new_group(u'test group')
+
+        for i in range(contact_count):
+            yield user_api.contact_store.new_contact(
+                name=u'First', surname=u'Surname %s' % (i,),
+                msisdn=from_addr.format(i), groups=[group])
+
+        conversation = yield self.create_conversation(
+            delivery_tag_pool=u'pool', delivery_class=u'sms')
+        conversation.add_group(group)
+        yield conversation.save()
+        returnValue(conversation)
 
     @inlineCallbacks
     def test_start(self):
-        yield self.dispatch_command("start", command_data={
-                    "batch_id": "batch-id",
-                    "conversation_type": "jsbox",
-                    "conversation_key": "key",
-                    "is_client_initiated": False,
-                    "msg_options": {}
-                    })
+        conversation = yield self.setup_conversation()
+        yield self.start_conversation(conversation)
+
+        # Force processing of messages
+        yield self._amqp.kick_delivery()
