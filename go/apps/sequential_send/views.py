4022a807b2a88f07e97b84366a6113c35035719d
Jeremy Thurgood <jeremy@praekelt.com>
2012-11-21 12:13:45 +0200

    New schedule config for sequential send app.

--- go/apps/sequential_send/views.py
+++ go/apps/sequential_send/views.py
@@ -10,7 +10,7 @@ from go.conversation.forms import VumiModelForm
 
 class ScheduleForm(BootstrapForm):
     recurring = forms.CharField()
-    days = forms.CharField()
+    days = forms.CharField(required=False)
     time = forms.CharField()
 
 
