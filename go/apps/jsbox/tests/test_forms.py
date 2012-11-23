f0299dc4fa27339d37f40fb9a201189b96dd0a48
Simon Cross <hodgestar@gmail.com>
2012-11-23 19:01:07 +0200

    Implement really basic javascript fetching from a URL and test.

--- go/apps/jsbox/tests/test_forms.py
+++ go/apps/jsbox/tests/test_forms.py
@@ -1,3 +1,5 @@
+import mock
+
 from django.test import TestCase
 
 from go.apps.jsbox.forms import JsboxForm
@@ -13,4 +15,21 @@ class JsboxFormTestCase(TestCase):
         self.assertEqual(metadata, {
             'javascript': 'x = 1;',
             'source_url': '',
+        })
+
+    @mock.patch('requests.get')
+    def test_update_from_source(self, requests_get):
+        requests_get.return_value = "custom javascript"
+        source_url = 'http://www.example.com/'
+        form = JsboxForm(data={
+            'javascript': '',
+            'source_url': source_url,
+            'update_from_source': '1',
+        })
+        self.assertTrue(form.is_valid())
+        metadata = form.to_metadata()
+        requests_get.assert_called_once_with(source_url)
+        self.assertEqual(metadata, {
+            'javascript': requests_get.return_value,
+            'source_url': source_url,
         })
