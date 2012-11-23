c32208bb9179b9aaf00560c4ca73eba896176381
Simon Cross <hodgestar@gmail.com>
2012-11-23 03:03:37 +0200

    Add form fields for updating source from a URL.

--- go/apps/jsbox/views.py
+++ go/apps/jsbox/views.py
@@ -12,6 +12,20 @@ class JavascriptField(forms.CharField):
 
 class JsboxForm(BootstrapForm):
     javascript = JavascriptField()
+    source_url = forms.CharField(required=False)
+    update_from_source = forms.BooleanField(required=False)
+
+    def _load_from_url(self, url):
+        # TODO: implement
+        return "TODO: source from url (%s)" % url
+
+    def to_metadata(self):
+        metadata = self.cleaned_data.copy()
+        if metadata['update_from_source']:
+            metadata['javascript'] = self._load_from_url(
+                metadata['source_url'])
+        del metadata['update_from_source']
+        return metadata
 
 
 class JsboxConversationViews(ConversationViews):
