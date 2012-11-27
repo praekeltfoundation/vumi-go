ebd65152970c35496317b7942bf1f34c6d1c6804
Simon Cross <hodgestar@gmail.com>
2012-11-27 13:43:13 +0200

    Add GOOGLE_ANALYTICS_UA to settings.py so that people configuring things know its there.

--- go/settings.py
+++ go/settings.py
@@ -269,6 +269,9 @@ VUMI_INSTALLED_APPS = {
 VXPOLLS_REDIS_CONFIG = {}
 VXPOLLS_PREFIX = 'vumigo'
 
+# Set this to enable Google Analytics
+GOOGLE_ANALYTICS_UA = None
+
 try:
     from production_settings import *
 except ImportError:
