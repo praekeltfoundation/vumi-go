acfcea6f68e313f9fe21442e9a928e86d99f701b
Simon Cross <hodgestar@gmail.com>
2012-12-06 16:57:31 +0200

    Clean-up metadata construction.

--- go/apps/jsbox/tests/test_forms.py
+++ go/apps/jsbox/tests/test_forms.py
@@ -2,11 +2,11 @@ import mock
 
 from django.test import TestCase
 
-from go.apps.jsbox.forms import JsboxForm
+from go.apps.jsbox.forms import JsboxForm, JsboxAppConfigForm
 
 
 class JsboxFormTestCase(TestCase):
-    def test_to_metdata(self):
+    def test_to_metadata(self):
         form = JsboxForm(data={
             'javascript': 'x = 1;',
         })
@@ -58,4 +58,20 @@ class JsboxFormTestCase(TestCase):
         self.assertEqual(metadata, {
             'javascript': '',
             'source_url': source_url,
+        })
+
+
+class JsboxAppConfigFormTestCase(TestCase):
+
+    def test_to_metadata(self):
+        form = JsboxAppConfigForm(data={
+            'key': 'foo',
+            'value': 'bar',
+        })
+        self.assertTrue(form.is_valid())
+        metadata = form.to_metadata()
+        self.assertEqual(metadata, {
+            'key': 'foo',
+            'value': 'bar',
+            'source_url': '',
         })
