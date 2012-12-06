0907737e4381615a96aa3e355c319780d7853d2f
Simon Cross <hodgestar@gmail.com>
2012-12-06 16:57:31 +0200

    Clean-up metadata construction.

--- go/apps/jsbox/forms.py
+++ go/apps/jsbox/forms.py
@@ -7,6 +7,20 @@ from django.forms.formsets import BaseFormSet
 from bootstrap.forms import BootstrapForm
 
 
+def possibly_load_from_url(url, default_value, update):
+    """Possibly load a value from a URL.
+
+    Returns the body of the response from the URL if update is True
+    and the URL is non-empty and a request to the URL returns
+    successfully. Otherwise returns the default value.
+    """
+    if update and url:
+        response = requests.get(url)
+        if response.ok:
+            return response.text
+    return default_value
+
+
 class JavascriptField(forms.CharField):
     widget = widgets.Textarea
 
@@ -16,19 +30,20 @@ class JsboxForm(BootstrapForm):
     source_url = forms.URLField(required=False)
     update_from_source = forms.BooleanField(required=False)
 
-    def _update_from_source(self, url):
-        response = requests.get(url)
-        if response.ok:
-            return response.text
+    @staticmethod
+    def initial_from_metadata(metadata):
+        return metadata
 
     def to_metadata(self):
-        metadata = self.cleaned_data.copy()
-        if metadata['update_from_source']:
-            source = self._update_from_source(metadata['source_url'])
-            if source is not None:
-                metadata['javascript'] = source
-        del metadata['update_from_source']
-        return metadata
+        javascript = possibly_load_from_url(
+            self.cleaned_data['source_url'],
+            self.cleaned_data['javascript'],
+            self.cleaned_data['update_from_source'],
+        )
+        return {
+            'javascript': javascript,
+            'source_url': self.cleaned_data['source_url'],
+        }
 
 
 class ConfigValueField(forms.CharField):
@@ -41,19 +56,21 @@ class JsboxAppConfigForm(BootstrapForm):
     source_url = forms.URLField(required=False)
     update_from_source = forms.BooleanField(required=False)
 
-    def _update_from_source(self, url):
-        response = requests.get(url)
-        if response.ok:
-            return response.text
+    @staticmethod
+    def initial_from_metadata(metadata):
+        return metadata
 
     def to_metadata(self):
-        metadata = self.cleaned_data.copy()
-        if metadata['update_from_source']:
-            source = self._update_from_source(metadata['source_url'])
-            if source is not None:
-                metadata['value'] = source
-        del metadata['update_from_source']
-        return metadata
+        value = possibly_load_from_url(
+            self.cleaned_data['source_url'],
+            self.cleaned_data['value'],
+            self.cleaned_data['update_from_source'],
+        )
+        return {
+            'key': self.cleaned_data['key'],
+            'value': value,
+            'source_url': self.cleaned_data['source_url'],
+        }
 
 
 class JsboxAppConfigFormset(BaseFormSet):
@@ -71,14 +88,15 @@ class JsboxAppConfigFormset(BaseFormSet):
             submeta = form.to_metadata()
             metadata[submeta['key']] = submeta
             del submeta['key']
-            del submeta['DELETE']  # remove formset deletion marker
         return metadata
 
-    @staticmethod
-    def initial_from_metadata(metadata):
+    @classmethod
+    def initial_from_metadata(cls, metadata):
         initials = []
         for key in sorted(metadata):
             submeta = metadata[key].copy()
             submeta['key'] = key
+            if hasattr(cls.form, 'initial_from_metadata'):
+                submeta = getattr(cls.form, 'initial_from_metadata')(submeta)
             initials.append(submeta)
         return initials
