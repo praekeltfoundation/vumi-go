e68fcf7f0420686f323b9861700b855317cc4bf7
Simon de Haan <simon@praekeltfoundation.org>
2012-12-12 00:57:24 +0200

    partial cherry pick of b4578e0

--- go/base/views.py
+++ go/base/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+import urllib
+
+from django.conf import settings
+from django.shortcuts import render, Http404, redirect
+from django.contrib.auth.views import logout
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+
+from vumi.persist.redis_manager import RedisManager
+
+from go.base.token_manager import TokenManager
 
 
 def todo(request):  # pragma: no cover
     return render(request, 'base/todo.html', {
     })
+
+
+def token(request, token):
+    redis = RedisManager.from_config(settings.VUMI_API_CONFIG['redis_manager'])
+    tm = TokenManager(redis.sub_manager('token_manager'))
+    token_data = tm.get(token)
+    if not token_data:
+        raise Http404
+    user_id = int(token_data['user_id'])
+    redirect_to = token_data['redirect_to']
+
+    # If we're authorized and we're the same user_id then redirect to
+    # where we need to be
+    if not user_id or request.user.id == user_id:
+        return redirect(redirect_to)
+
+    # If we got here then we need authentication and the user's either not
+    # logged in or is logged in with a wrong account.
+    if request.user.is_authenticated():
+        logout(request)
+        messages.info(request, 'Wrong account for this token.')
+    return redirect('%s?%s' % (reverse('auth_login'), urllib.urlencode({
+        'next': reverse('token', kwargs={'token': token}),
+        })))
