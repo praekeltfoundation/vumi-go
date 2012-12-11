c16460eb2e907a575b36b7aa4b144cb3f186186a
Simon de Haan <simon@praekeltfoundation.org>
2012-12-12 01:02:36 +0200

    token manager with verification using user & system (internal tokens)

--- go/base/tests/test_token_manager.py
+++ go/base/tests/test_token_manager.py
@@ -21,14 +21,16 @@ class TokenManagerTestCase(DjangoGoApplicationTestCase):
         token = self.tm.generate('/some/path/', lifetime=10)
         token_data = self.tm.get(token)
         self.assertEqual(len(token), 6)
-        self.assertEqual(token_data, {
-            'user_id': '',
-            'redirect_to': '/some/path/',
-            })
+        self.assertEqual(token_data['user_id'], '')
+        self.assertEqual(token_data['redirect_to'], '/some/path/')
+        self.assertTrue(token_data['system_token'])
 
     def test_token_provided(self):
-        token = self.tm.generate('/some/path/', lifetime=10, token='token!')
-        self.assertEqual(token, 'token!')
+        token = self.tm.generate('/some/path/', lifetime=10,
+                                    token=('to', 'ken'))
+        self.assertEqual(token, 'to')
+        token_data = self.tm.get(token)
+        self.assertEqual(token_data['system_token'], 'ken')
 
     def test_unknown_token(self):
         self.assertEqual(self.tm.get('foo'), {})
@@ -45,9 +47,12 @@ class TokenManagerTestCase(DjangoGoApplicationTestCase):
     def test_token_with_login(self):
         token = self.tm.generate('/path/', user_id=self.user.pk)
         token_url = reverse('token', kwargs={'token': token})
+        token_data = self.tm.get(token)
         self.client.login(username='username', password='password')
         response = self.client.get(token_url)
-        self.assertTrue(response['Location'].endswith('/path/'))
+        self.assertTrue(
+            response['Location'].endswith('/path/?token=%s%s%s' % (
+                len(token), token, token_data['system_token'])))
 
     def test_token_with_invalid_login(self):
         token = self.tm.generate('/path/', user_id=-1)
@@ -64,3 +69,18 @@ class TokenManagerTestCase(DjangoGoApplicationTestCase):
         token_url = reverse('token', kwargs={'token': token})
         response = self.client.get(token_url)
         self.assertTrue(response.status_code, 404)
+
+    def test_token_verify(self):
+        token = self.tm.generate('/foo/', token=('to', 'ken'))
+        self.assertEqual(self.tm.get(token, verify='bar'), {})
+        self.assertEqual(self.tm.get(token, verify='ken'), {
+            'user_id': '',
+            'system_token': 'ken',
+            'redirect_to': '/foo/',
+            })
+
+    def test_token_delete(self):
+        token = self.tm.generate('/foo/')
+        self.assertTrue(self.tm.get(token))
+        self.assertTrue(self.tm.delete(token))
+        self.assertFalse(self.tm.get(token))
--- go/base/token_manager.py
+++ go/base/token_manager.py
@@ -11,16 +11,21 @@ class TokenManager(object):
     def __init__(self, redis):
         self.redis = redis
 
-    def generate_token(self, length=6):
+    def generate_token(self, user_token_size=6):
         """
         Generate a token that doesn't exist yet but is also short enough
         to not take up too much space in an SMS.
+
+        :param int user_token_size:
+            How big the token sent to the user should be. Defaults to 6.
         """
         token_taken = True
         while token_taken:
-            token = uuid4().hex[0:length]
-            token_taken = self.redis.exists(token)
-        return token
+            full_token = uuid4().hex
+            user_token = full_token[0:user_token_size]
+            system_token = full_token[user_token_size:]
+            token_taken = self.redis.exists(user_token)
+        return (user_token, system_token)
 
     def generate(self, redirect_to, user_id=None, lifetime=None, token=None):
         """
@@ -31,22 +36,52 @@ class TokenManager(object):
             How long should the token be valid for in seconds.
         :param int user_id:
             Django user id, if specified this URL is only valid for this user.
-        :param str token:
+        :param tuple token:
             The token to use, if not specified then a unique one will be
-            automatically generated.
+            automatically generated. The token is a tuple in the format
+            (user_token, system_token). The user token is submitted in the SMS
+            the system_token is used internally to ensure that only users
+            arriving at the `redirect_to` URL via the token url gain access.
         """
         lifetime = lifetime or self.DEFAULT_LIFETIME
-        token = token or self.generate_token()
-        self.redis.hmset(token, {
+        user_token, system_token = token or self.generate_token()
+        self.redis.hmset(user_token, {
             'redirect_to': redirect_to,
             'user_id': user_id or '',
+            'system_token': system_token,
             })
-        self.redis.expire(token, lifetime)
-        return token
+        self.redis.expire(user_token, lifetime)
+        return user_token
 
-    def get(self, token):
+    def get(self, token, verify=None):
         """
         Retrieve the data for the given token, it it doesn't exist it'll
         return an empty dictionary.
+
+        :param str verify:
+            Provide the system_token if matching needs to occur. If there
+            is no match then an empty dictionary will be returned.
         """
-        return self.redis.hgetall(token)
+        token_data = self.redis.hgetall(token)
+        if verify is not None and verify != token_data['system_token']:
+            return {}
+        return token_data
+
+    def verify_get(self, full_token):
+        """
+        Retrieve the data for a full token as supplied to the `redirect_to`
+        URL.
+        """
+        user_token_length, _, token = full_token.partition('-')
+        user_token = token[0:int(user_token_length)]
+        system_token = token[int(user_token_length):]
+        return self.get(user_token, verify=system_token)
+
+    def delete(self, token):
+        """
+        Remove a token
+
+        :param str token:
+            The token to expire.
+        """
+        return self.redis.delete(token)
