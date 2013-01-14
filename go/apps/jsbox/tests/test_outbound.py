d66bbdb3fa9ba644c7bc9fce4a6f942dca4f554f
Simon Cross <hodgestar@gmail.com>
2013-01-14 15:57:26 +0200

    Implement Go outbound message resource that replaces the send_to command with a send_to_tag command that sends to active Go endpoints the user has access to.

--- go/apps/jsbox/tests/test_outbound.py
+++ go/apps/jsbox/tests/test_outbound.py
@@ -3,6 +3,7 @@
 import mock
 
 from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks
 
 from vumi.application.sandbox import SandboxCommand
 from vumi.tests.utils import LogCatcher
@@ -14,13 +15,74 @@ class TestGoOutboundResource(TestCase):
 
     def setUp(self):
         self.conversation = mock.Mock()
+        self.user_api = mock.Mock()
         self.app_worker = mock.Mock()
         self.dummy_api = object()
         self.resource = GoOutboundResource("test", self.app_worker, {})
         self.app_worker.conversation_for_api = mock.Mock(
             return_value=self.conversation)
+        self.app_worker.user_api_for_api = mock.Mock(
+            return_value=self.user_api)
+        self.user_api.list_endpoints = mock.Mock(
+            return_value=set([('pool1', '1234'), ('pool2', '1234')]))
+        self.user_api.msg_options = mock.Mock(
+            return_value={'opt1': 'bar'})
 
     def check_reply(self, reply, cmd, success):
         self.assertEqual(reply['reply'], True)
         self.assertEqual(reply['cmd_id'], cmd['cmd_id'])
         self.assertEqual(reply['success'], success)
+
+    def check_sent(self, to_addr, content, **msg_options):
+        self.app_worker.send_to.assert_called_once_with(
+            to_addr, content, **msg_options)
+
+    def check_not_sent(self):
+        self.assertFalse(self.app_worker.send_to.called)
+
+    @inlineCallbacks
+    def check_fails(self, reason, handler=None, **cmd_args):
+        if handler is None:
+            handler = self.resource.handle_send_to_tag
+        cmd = SandboxCommand(**cmd_args)
+        reply = yield handler(self.dummy_api, cmd)
+        self.check_reply(reply, cmd, False)
+        self.check_not_sent()
+        self.assertEqual(reply['reason'], reason)
+
+    def test_send_to_fails(self):
+        return self.check_fails("Generic sending not supported in Vumi Go",
+                                handler=self.resource.handle_send_to)
+
+    @inlineCallbacks
+    def test_send_to_tag(self):
+        cmd = SandboxCommand(tagpool='pool1', tag='1234', to_addr='6789',
+                             content='bar')
+        with LogCatcher() as lc:
+            reply = yield self.resource.handle_send_to_tag(self.dummy_api, cmd)
+            self.assertEqual(lc.messages(), [
+                "Sending outbound message to '6789' via tag ('pool1', '1234'),"
+                " content: 'bar'"])
+        self.check_reply(reply, cmd, True)
+        self.check_sent('6789', 'bar', **{'opt1': 'bar'})
+
+    def test_send_to_tag_unacquired(self):
+        return self.check_fails("Tag ('foo', '12345') not held by account",
+                                tagpool='foo', tag='12345', to_addr='6789',
+                                content='bar')
+
+    def test_send_to_tag_missing_tagpool(self):
+        return self.check_fails("Tag, content or to_addr not specified",
+                                tag='12345', to_addr='6789', content='bar')
+
+    def test_send_to_tag_missing_tag(self):
+        return self.check_fails("Tag, content or to_addr not specified",
+                                tagpool='foo', to_addr='6789', content='bar')
+
+    def test_send_to_tag_missing_to_addr(self):
+        return self.check_fails("Tag, content or to_addr not specified",
+                                tagpool='foo', tag='12345', content='bar')
+
+    def test_send_to_tag_missing_content(self):
+        return self.check_fails("Tag, content or to_addr not specified",
+                                tagpool='foo', tag='12345', to_addr='6789')
