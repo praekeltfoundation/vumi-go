125460db6809dd79bc964d84b06f93bd8a4226b8
Simon Cross <hodgestar@gmail.com>
2013-01-14 15:57:26 +0200

    Implement Go outbound message resource that replaces the send_to command with a send_to_tag command that sends to active Go endpoints the user has access to.

--- go/apps/jsbox/outbound.py
+++ go/apps/jsbox/outbound.py
@@ -3,6 +3,8 @@
 
 """Outbound message resource for JS Box sandboxes"""
 
+from twisted.internet.defer import inlineCallbacks, returnValue
+
 from vumi.application.sandbox import OutboundResource
 from vumi import log
 
@@ -10,15 +12,29 @@ from vumi import log
 class GoOutboundResource(OutboundResource):
     """Resource that provides outbound message support for Go."""
 
+    @inlineCallbacks
     def handle_send_to_tag(self, api, command):
-        tagpool = command.get('tagpool')
-        tag = command.get('tag')
-        content = command['content']
-        to_addr = command['to_addr']
-        # TODO: check tagpool / tag is owned by an active conversation
-        # TODO: fetch tagpool metadata for tagpool
-        msg_options = {}
-        self.app_worker.send_to(to_addr, content, **msg_options)
+        tag = (command.get('tagpool'), command.get('tag'))
+        content = command.get('content')
+        to_addr = command.get('to_addr')
+        if (tag[0] is None or tag[1] is None or content is None or
+                to_addr is None):
+            returnValue(self.reply(
+                command, success=False,
+                reason="Tag, content or to_addr not specified"))
+        log.info("Sending outbound message to %r via tag %r, content: %r" %
+                 (to_addr, tag, content))
+
+        user_api = self.app_worker.user_api_for_api(api)
+        tags = yield user_api.list_endpoints()
+        if tag not in tags:
+            returnValue(self.reply(command, success=False,
+                                   reason="Tag %r not held by account"
+                                   % (tag,)))
+        msg_options = yield user_api.msg_options(tag)
+        yield self.app_worker.send_to(to_addr, content, **msg_options)
+
+        returnValue(self.reply(command, success=True))
 
     def handle_send_to(self, api, command):
         # Generic sending is not supported in Vumi Go sandboxes
