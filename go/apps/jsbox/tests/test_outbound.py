8c0a84de4f99b879ca33174dfd1ec8eae61af1c4
Simon Cross <hodgestar@gmail.com>
2013-01-15 10:40:11 +0200

    Change check_* methods in sandbox outbound resource tests to assert_*.

--- go/apps/jsbox/tests/test_outbound.py
+++ go/apps/jsbox/tests/test_outbound.py
@@ -28,31 +28,31 @@ class TestGoOutboundResource(TestCase):
         self.user_api.msg_options = mock.Mock(
             return_value={'opt1': 'bar'})
 
-    def check_reply(self, reply, cmd, success):
+    def assert_reply(self, reply, cmd, success):
         self.assertEqual(reply['reply'], True)
         self.assertEqual(reply['cmd_id'], cmd['cmd_id'])
         self.assertEqual(reply['success'], success)
 
-    def check_sent(self, to_addr, content, **msg_options):
+    def assert_sent(self, to_addr, content, **msg_options):
         self.app_worker.send_to.assert_called_once_with(
             to_addr, content, **msg_options)
 
-    def check_not_sent(self):
+    def assert_not_sent(self):
         self.assertFalse(self.app_worker.send_to.called)
 
     @inlineCallbacks
-    def check_fails(self, reason, handler=None, **cmd_args):
+    def assert_fails(self, reason, handler=None, **cmd_args):
         if handler is None:
             handler = self.resource.handle_send_to_tag
         cmd = SandboxCommand(**cmd_args)
         reply = yield handler(self.dummy_api, cmd)
-        self.check_reply(reply, cmd, False)
-        self.check_not_sent()
+        self.assert_reply(reply, cmd, False)
+        self.assert_not_sent()
         self.assertEqual(reply['reason'], reason)
 
     def test_send_to_fails(self):
-        return self.check_fails("Generic sending not supported in Vumi Go",
-                                handler=self.resource.handle_send_to)
+        return self.assert_fails("Generic sending not supported in Vumi Go",
+                                 handler=self.resource.handle_send_to)
 
     @inlineCallbacks
     def test_send_to_tag(self):
@@ -63,26 +63,26 @@ class TestGoOutboundResource(TestCase):
             self.assertEqual(lc.messages(), [
                 "Sending outbound message to '6789' via tag ('pool1', '1234'),"
                 " content: 'bar'"])
-        self.check_reply(reply, cmd, True)
-        self.check_sent('6789', 'bar', **{'opt1': 'bar'})
+        self.assert_reply(reply, cmd, True)
+        self.assert_sent('6789', 'bar', **{'opt1': 'bar'})
 
     def test_send_to_tag_unacquired(self):
-        return self.check_fails("Tag ('foo', '12345') not held by account",
-                                tagpool='foo', tag='12345', to_addr='6789',
-                                content='bar')
+        return self.assert_fails("Tag ('foo', '12345') not held by account",
+                                 tagpool='foo', tag='12345', to_addr='6789',
+                                 content='bar')
 
     def test_send_to_tag_missing_tagpool(self):
-        return self.check_fails("Tag, content or to_addr not specified",
-                                tag='12345', to_addr='6789', content='bar')
+        return self.assert_fails("Tag, content or to_addr not specified",
+                                 tag='12345', to_addr='6789', content='bar')
 
     def test_send_to_tag_missing_tag(self):
-        return self.check_fails("Tag, content or to_addr not specified",
-                                tagpool='foo', to_addr='6789', content='bar')
+        return self.assert_fails("Tag, content or to_addr not specified",
+                                 tagpool='foo', to_addr='6789', content='bar')
 
     def test_send_to_tag_missing_to_addr(self):
-        return self.check_fails("Tag, content or to_addr not specified",
-                                tagpool='foo', tag='12345', content='bar')
+        return self.assert_fails("Tag, content or to_addr not specified",
+                                 tagpool='foo', tag='12345', content='bar')
 
     def test_send_to_tag_missing_content(self):
-        return self.check_fails("Tag, content or to_addr not specified",
-                                tagpool='foo', tag='12345', to_addr='6789')
+        return self.assert_fails("Tag, content or to_addr not specified",
+                                 tagpool='foo', tag='12345', to_addr='6789')
