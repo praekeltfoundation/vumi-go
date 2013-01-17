6e6de26dc5d0906d844ad508296133a2fdccddcb
Simon de Haan <simon@praekeltfoundation.org>
2013-01-17 21:53:47 +0200

    streaming client & http basic auth with tests

--- go/apps/http_api/client.py
+++ go/apps/http_api/client.py
@@ -2,7 +2,7 @@ import json
 
 from twisted.internet.defer import Deferred
 from twisted.internet import reactor
-from twisted.web.client import Agent
+from twisted.web.client import Agent, ResponseDone
 from twisted.web import http
 from twisted.protocols import basic
 from twisted.python.failure import DefaultException
@@ -20,15 +20,18 @@ class VumiMessageReceiver(basic.LineReceiver):
         self.message_class = message_class
         self.callback = callback
         self.errback = errback
-        self.response = None
-        self.deferred = Deferred()
+        self._response = None
+        self._wait_for_response = Deferred()
+
+    def get_response(self):
+        return self._wait_for_response
 
     def handle_response(self, response):
-        self.response = response
-        if self.response.code == http.NO_CONTENT:
-            self.deferred.callback(self.response)
+        self._response = response
+        if self._response.code == http.NO_CONTENT:
+            self._wait_for_response.callback(self._response)
         else:
-            response.deliverBody(self)
+            self._response.deliverBody(self)
 
     def lineReceived(self, line):
         d = Deferred()
@@ -42,9 +45,15 @@ class VumiMessageReceiver(basic.LineReceiver):
             d.errback(e)
 
     def connectionLost(self, reason):
-        d = Deferred()
-        d.addErrback(self.errback)
-        d.errback(DefaultException(reason.getErrorMessage()))
+        # check if all content was received ok
+        if (reason.check(ResponseDone, http.PotentialDataLoss)
+            and self._response is not None
+            and not self._wait_for_response.called):
+            self._wait_for_response.callback(self._response)
+        else:
+            d = Deferred()
+            d.addErrback(self.errback)
+            d.errback(DefaultException(reason.getErrorMessage()))
 
     def disconnect(self):
         if self.transport._producer is not None:
@@ -58,12 +67,13 @@ class StreamingClient(object):
         self.agent = Agent(reactor)
 
     def stream(self, message_class, callback, errback, url, headers=None):
-        d = self.agent.request('GET', url, headers)
-        d.addCallback(self.handle_response, message_class, callback, errback)
-        d.addErrback(log.err)
-        return d
-
-    def handle_response(self, response, message_class, callback, errback):
         receiver = VumiMessageReceiver(message_class, callback, errback)
-        receiver.handle_response(response)
+
+        d = self.agent.request('GET', url, headers)
+        d.addCallback(self.handle_response, receiver)
+        d.addErrback(log.err)
+
         return receiver
+
+    def handle_response(self, response, receiver):
+        receiver.handle_response(response)
