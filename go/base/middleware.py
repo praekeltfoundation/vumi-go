36897052d230c8bcd92df1e067ed9a80e3d140f0
Simon de Haan <simon@praekeltfoundation.org>
2013-02-12 10:39:47 +0200

    ResponseTimeMiddleware for Django

--- go/base/middleware.py
+++ go/base/middleware.py
@@ -1,4 +1,14 @@
+import time
+import logging
+
+from django.core.urlresolvers import resolve, Resolver404
+
 from go.base.utils import vumi_api_for_user
+from go.base.amqp import connection
+
+from vumi.blinkenlights.metrics import AVG, MetricMessage
+
+logger = logging.getLogger(__name__)
 
 
 class VumiUserApiMiddleware(object):
@@ -6,3 +16,33 @@ class VumiUserApiMiddleware(object):
         user = getattr(request, 'user', None)
         if user is not None and user.is_authenticated():
             request.user_api = vumi_api_for_user(request.user)
+
+
+class ResponseTimeMiddleware(object):
+    """
+    Middleware for generating metrics on page response times.
+
+    """
+    def process_request(self, request):
+        request.start_time = time.time()
+
+    def process_response(self, request, response):
+        try:
+            func = resolve(request.path)[0]
+            metric_name = '%s.%s' % (func.__module__, func.__name__)
+            response_time = time.time() - request.start_time
+            self.publish_metric(metric_name, response_time)
+        except (Resolver404, AttributeError), e:
+            logger.exception(e)
+        if response:
+            return response
+
+    def publish_metric(self, metric_name, value, timestamp=None,
+                        aggregators=None):
+        aggregators = aggregators or [AVG]
+        timestamp = timestamp or time.time()
+        metric_msg = MetricMessage()
+        metric_msg.append((metric_name,
+            tuple(sorted(agg.name for agg in aggregators)),
+            [(timestamp, value)]))
+        connection.publish_metric(metric_msg)
