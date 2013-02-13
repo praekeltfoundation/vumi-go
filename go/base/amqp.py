04843e9b0ed8a430085a3fe5d42b58805563e252
Simon de Haan <simon@praekeltfoundation.org>
2013-02-13 15:21:11 +0200

    differentiating between publish_metric() and publish_metric_message()

--- go/base/amqp.py
+++ go/base/amqp.py
@@ -1,4 +1,8 @@
+import time
+
 from kombu import Connection, Exchange
+
+from vumi.blinkenlights.metrics import MetricMessage
 
 
 class AmqpConnection(object):
@@ -23,13 +27,21 @@ class AmqpConnection(object):
         self.producer.publish(message, exchange=exchange,
             routing_key=routing_key)
 
-    def publish_command(self, command):
+    def publish_command_message(self, command):
         return self.publish(command.to_json, exchange=self.default_exchange,
             routing_key='vumi.api')
 
-    def publish_metric(self, metric):
+    def publish_metric_message(self, metric):
         return self.publish(metric.to_json(), exchange=self.metrics_exchange,
             routing_key='vumi.metrics')
+
+    def publish_metric(self, metric_name, aggregators, value, timestamp=None):
+        timestamp = timestamp or time.time()
+        metric_msg = MetricMessage()
+        metric_msg.append((metric_name,
+            tuple(sorted(agg.name for agg in aggregators)),
+            [(timestamp, value)]))
+        return self.publish_metric_message(metric_msg)
 
 connection = AmqpConnection()
 
