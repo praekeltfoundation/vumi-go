3a02b1a80cd4894264e24f996299a370624c829a
Simon de Haan <simon@praekeltfoundation.org>
2013-03-01 10:17:50 +0200

    Revert "adding sentry logging to Celery tasks"
    This reverts commit 96f537e22c8645b23e4db702f582e58c3f84ca31.

--- go/settings.py
+++ go/settings.py
@@ -2,11 +2,7 @@
 # Django settings for go project.
 import os
 import djcelery
-import logging
 
-from celery.signals import task_failure
-
-from sentry.client.handlers import SentryHandler
 
 djcelery.setup_loader()
 
@@ -306,29 +302,6 @@ CELERYBEAT_SCHEDULE = {
         'args': ('daily',)
     },
 }
-
-logger = logging.getLogger('task')
-logger.addHandler(SentryHandler())
-
-
-def process_failure_signal(exception, traceback, sender, task_id,
-                           signal, args, kwargs, einfo, **kw):
-    exc_info = (type(exception), exception, traceback)
-    logger.error(
-        'Celery job exception: %s(%s)' % (exception.__class__.__name__,
-                                            exception),
-        exc_info=exc_info,
-        extra={
-            'data': {
-                'task_id': task_id,
-                'sender': sender,
-                'args': args,
-                'kwargs': kwargs,
-            }
-        }
-    )
-
-task_failure.connect(process_failure_signal)
 
 try:
     from production_settings import *
