308f7f5376210c5283921063aa1244472bd69ad1
Simon de Haan <simon@praekeltfoundation.org>
2013-03-26 12:38:07 +0200

    configs for Wikipedia setup & scripts for generating necessary transport & dispatchar YAML config & supervisord .conf files

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./go-admin.sh syncdb --migrate --noinput && \
+./go-admin.sh go_setup_env --config-file=./setup_env/config.yaml \
+	--tagpool-file=./setup_env/tagpools.yaml \
+	--account-file=./setup_env/accounts.yaml \
+	--conversation-file=./setup_env/conversations.yaml \
+	--transport-file=./setup_env/transports.yaml
