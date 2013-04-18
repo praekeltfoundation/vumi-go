942ee9a706b6efc4f28272914a78faf35e8dc7f2
Simon de Haan <simon@praekeltfoundation.org>
2013-04-18 12:14:48 +0200

    Revert "scaffolding for post commit hooks"
    This reverts commit 26373e26868fb60ec4bf25580b849bcb024a44c7.

--- go/apps/jsbox/urls.py
+++ go/apps/jsbox/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
-from go.apps.jsbox.views import (
-    JsboxConversationViews, cross_domain_xhr, post_commit)
+from go.apps.jsbox.views import JsboxConversationViews, cross_domain_xhr
 
 urlpatterns = JsboxConversationViews().get_urlpatterns()
 
 urlpatterns += patterns('',
     url(r'^cross-domain-xhr/$', cross_domain_xhr),
-    url(r'^(?P<account_key>\w+)/(?P<conversation_key>\w+)/post-commit/$',
-        post_commit, name='post_commit'),
 )
