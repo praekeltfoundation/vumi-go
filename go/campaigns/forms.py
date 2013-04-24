161db84fc6e9e936c2fb56163a441d3fdd333f43
Peter Pistorius <peter.pistorius@gmail.com>
2013-04-24 13:22:59 +0100

    placeholders for campaign plumbs.

--- go/campaigns/forms.py
+++ go/campaigns/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-class CampaignForm(forms.Form):
+class CampaignGeneralForm(forms.Form):
 
     TYPE_CHOICES = (
             ('', 'Select campaign type'),
@@ -12,3 +12,31 @@ class CampaignForm(forms.Form):
     name = forms.CharField(label="Campaign name", max_length=100)
     type = forms.ChoiceField(label="Which kind of campaign would you like?", 
                              widget=forms.Select(), choices=TYPE_CHOICES)
+
+class CampaignConfigurationForm(forms.Form):
+
+    COUNTRY_CHOICES = (
+            ('*', 'All Countries'),
+            ('.ke', 'Kenya'),
+            ('.za', 'South Africa'),
+        )
+
+    CHANNEL_CHOICES = (
+            ('ussd', 'USSD'),
+            ('gtalk', 'Google Talk'),
+        )
+
+    # more than likely a many to many field, or something similair in the riak
+    # world. Whom I kidding, this is probably just a modelform?
+    countries = forms.MultipleChoiceField(label="Destinations", 
+                                          widget=forms.Select(), 
+                                          choices=COUNTRY_CHOICES)
+    
+    channels = forms.MultipleChoiceField(label="Channels", 
+                                         widget=forms.Select(), 
+                                         choices=CHANNEL_CHOICES)
+
+    keyword = forms.CharField(label="Keyword", max_length=100)
+
+
+    
