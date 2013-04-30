7edfb68aa574095740b4819167bcffc38f9ffd4a
Simon Cross <hodgestar@gmail.com>
2013-04-30 10:34:29 +0200

    Fix up routing table mangling to track conversation tag changes.

--- utils/mangle_routing_table_sequential_send.sh
+++ utils/mangle_routing_table_sequential_send.sh
@@ -7,12 +7,11 @@ if [ $# -ne 3 ]; then
 fi
 
 email_address="$1"
-conv="CONVERSATION:jsbox:$2"
+conv="CONVERSATION:sequential_send:$2"
 tag="TRANSPORT_TAG:$3"
 
 . $(dirname $0)/mangle_routing_table_utils.sh
 
-assert_routing_exists "$conv"
 assert_routing_exists "$tag"
 
-mrt --add $conv $tag $tag default
+mrt --add $conv default $tag default
