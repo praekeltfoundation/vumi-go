6940301d01556a6890905cb7c604fd614f56f061
Simon de Haan <simon@praekeltfoundation.org>
2013-05-02 16:18:58 +0200

    pep8 fixes

--- go/contacts/tasks.py
+++ go/contacts/tasks.py
@@ -126,7 +126,7 @@ def export_group_contacts(account_key, group_key, include_extra):
 
 @task(ignore_result=True)
 def import_contacts_file(account_key, group_key, file_name, file_path,
-                            fields, has_header):
+                         fields, has_header):
     api = VumiUserApi.from_config_sync(account_key, settings.VUMI_API_CONFIG)
     contact_store = api.contact_store
     group = contact_store.get_group(group_key)
@@ -140,8 +140,7 @@ def import_contacts_file(account_key, group_key, file_name, file_path,
     try:
         extension, parser = ContactFileParser.get_parser(file_name)
 
-        contact_dictionaries = parser.parse_file(file_path, fields,
-            has_header)
+        contact_dictionaries = parser.parse_file(file_path, fields, has_header)
         for counter, contact_dictionary in enumerate(contact_dictionaries):
 
             # Make sure we set this group they're being uploaded in to
@@ -150,7 +149,8 @@ def import_contacts_file(account_key, group_key, file_name, file_path,
             contact = contact_store.new_contact(**contact_dictionary)
             written_contacts.append(contact)
 
-        send_mail('Contact import completed successfully.',
+        send_mail(
+            'Contact import completed successfully.',
             render_to_string('contacts/import_completed_mail.txt', {
                 'count': counter,
                 'group': group,
@@ -166,7 +166,8 @@ def import_contacts_file(account_key, group_key, file_name, file_path,
 
         exc_type, exc_value, exc_traceback = sys.exc_info()
 
-        send_mail('Something went wrong while importing the contacts.',
+        send_mail(
+            'Something went wrong while importing the contacts.',
             render_to_string('contacts/import_failed_mail.txt', {
                 'user': user_profile.user,
                 'group_key': group_key,
