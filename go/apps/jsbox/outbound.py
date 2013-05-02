632aedb9867f370e90564a954fc1c35c677a6bbf
Simon Cross <hodgestar@gmail.com>
2013-05-02 16:40:22 +0200

    Fix-up outbound resource and add tests for new methods.

--- go/apps/jsbox/outbound.py
+++ go/apps/jsbox/outbound.py
@@ -3,23 +3,42 @@
 
 """Outbound message resource for JS Box sandboxes"""
 
-from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.defer import inlineCallbacks, returnValue, succeed
 
-from vumi.application.sandbox import OutboundResource
+from vumi.application.sandbox import SandboxResource
 from vumi import log
 
 
-class GoOutboundResource(OutboundResource):
+class GoOutboundResource(SandboxResource):
     """Resource that provides outbound message support for Go."""
 
     def _handle_reply(self, api, command, reply_func):
+        if 'content' not in command:
+            return succeed(self.reply(
+                command, success=False,
+                reason=u"'content' must be given in replies."))
+        if 'in_reply_to' not in command:
+            return succeed(self.reply(
+                command, success=False,
+                reason=u"'in_reply_to' must be given in replies."))
+        orig_msg = api.get_inbound_message(command['in_reply_to'])
+        if orig_msg is None:
+            return succeed(self.reply(
+                command, success=False, reason=u"Could not find original"
+                " message with id: %r" % command['in_reply_to']))
+
         content = command['content']
         continue_session = command.get('continue_session', True)
-        orig_msg = api.get_inbound_message(command['in_reply_to'])
         conv = self.app_worker.conversation_for_api(api)
-        helper_metadata = conv.set_go_helper_metadata()
-        return reply_func(orig_msg, content, continue_session=continue_session,
-                          helper_metadata=helper_metadata)
+        helper_metadata = conv.set_go_helper_metadata(
+            orig_msg['helper_metadata'])
+
+        d = reply_func(orig_msg, content, continue_session=continue_session,
+                       helper_metadata=helper_metadata)
+        d.addCallback(lambda r: self.reply(command, success=True))
+        d.addErrback(lambda f: self.reply(command, success=False,
+                                          reason=unicode(f.getErrorMessage())))
+        return d
 
     def handle_reply_to(self, api, command):
         return self._handle_reply(api, command, self.app_worker.reply_to)
@@ -53,8 +72,3 @@ class GoOutboundResource(OutboundResource):
             to_addr, content, endpoint=endpoint, **msg_options)
 
         returnValue(self.reply(command, success=True))
-
-    def handle_send_to(self, api, command):
-        # Generic sending is not supported in Vumi Go sandboxes
-        return self.reply(command, success=False,
-                          reason="Generic sending not supported in Vumi Go")
