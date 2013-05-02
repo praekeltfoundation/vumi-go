3af4fb627ff18d44fda6bb14f575c01d4dd64eca
Simon Cross <hodgestar@gmail.com>
2013-05-03 01:43:42 +0200

    Add docstrings.

--- go/apps/jsbox/outbound.py
+++ go/apps/jsbox/outbound.py
@@ -10,7 +10,11 @@ from vumi import log
 
 
 class GoOutboundResource(SandboxResource):
-    """Resource that provides outbound message support for Go."""
+    """Resource that provides outbound message support for Go.
+
+    Includes support for replying, replying to groups and sending
+    messages via given tags.
+    """
 
     def _handle_reply(self, api, command, reply_func):
         if 'content' not in command:
@@ -41,13 +45,82 @@ class GoOutboundResource(SandboxResource):
         return d
 
     def handle_reply_to(self, api, command):
+        """
+        Sends a reply to the individual who sent a received message.
+
+        Command fields:
+            - ``content``: The body of the reply message.
+            - ``in_reply_to``: The ``message id`` of the message being replied
+            to.
+            - ``continue_session``: Whether to continue the session (if any).
+            Defaults to ``true``.
+
+        Reply fields:
+            - ``success``: ``true`` if the operation was successful, otherwise
+            ``false``.
+
+        Example:
+        .. code-block:: javascript
+            api.request(
+                'outbound.reply_to',
+                {content: 'Welcome!',
+                 in_reply_to: '06233d4eede945a3803bf9f3b78069ec'},
+                function(reply) { api.log_info('Reply sent: ' +
+                                               reply.success); });
+        """
         return self._handle_reply(api, command, self.app_worker.reply_to)
 
     def handle_reply_to_group(self, api, command):
+        """
+        Sends a reply to the group from which a received message was sent.
+
+        Command fields:
+            - ``content``: The body of the reply message.
+            - ``in_reply_to``: The ``message id`` of the message being replied
+            to.
+            - ``continue_session``: Whether to continue the session (if any).
+            Defaults to ``true``.
+
+        Reply fields:
+            - ``success``: ``true`` if the operation was successful, otherwise
+            ``false``.
+
+        Example:
+        .. code-block:: javascript
+            api.request(
+                'outbound.reply_to_group',
+                {content: 'Welcome!',
+                 in_reply_to: '06233d4eede945a3803bf9f3b78069ec'},
+                function(reply) { api.log_info('Reply to group sent: ' +
+                                               reply.success); });
+        """
         return self._handle_reply(api, command, self.app_worker.reply_to_group)
 
     @inlineCallbacks
     def handle_send_to_tag(self, api, command):
+        """
+        Sends a message to a specified tag.
+
+        Command fields:
+            - ``content``: The body of the reply message.
+            - ``to_addr``: The address of the recipient (e.g. an MSISDN).
+            - ``tagpool``: The name of the tagpool to send the message via.
+            - ``tag``: The name of the tag (within the tagpool) to send the
+            message from. Your Go user account must have the tag acquired.
+
+        Reply fields:
+            - ``success``: ``true`` if the operation was successful, otherwise
+            ``false``.
+
+        Example:
+        .. code-block:: javascript
+            api.request(
+                'outbound.send_to_tag',
+                {content: 'Welcome!', to_addr: '+27831234567',
+                 tagpool: 'vumi_long', tag: 'default10001'},
+                function(reply) { api.log_info('Message sent: ' +
+                                               reply.success); });
+        """
         tag = (command.get('tagpool'), command.get('tag'))
         content = command.get('content')
         to_addr = command.get('to_addr')
