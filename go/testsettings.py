7e8bc5a009005e81ab6a6b60f74ad475b37d3b14
Simon de Haan <simon@praekeltfoundation.org>
2013-05-06 16:06:21 +0200

    django-pipeline settings for running tests

--- go/testsettings.py
+++ go/testsettings.py
@@ -7,7 +7,7 @@ VUMI_API_CONFIG['riak_manager'] = {'bucket_prefix': 'test.'}
 VUMI_API_CONFIG['redis_manager'] = {
     'key_prefix': 'test',
     'FAKE_REDIS': 'sure',
-    }
+}
 
 
 if os.environ.get('VUMIGO_FAST_TESTS'):
@@ -19,3 +19,5 @@ if os.environ.get('VUMIGO_FAST_TESTS'):
     }
 
 NOSE_ARGS = ['-evumitools', '-evumi_app', '-ehandlers', '-m^test']
+
+STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
