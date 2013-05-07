6be01f665707e859d6fcbcaaf2c0ec15fdc138d9
justinvdm <justinvdm@lavabit.com>
2013-05-07 14:25:35 +0200

    Fix account tests

--- go/testsettings.py
+++ go/testsettings.py
@@ -1,7 +1,6 @@
 import os
 from settings import *
 
-
 # This needs to point at the test riak buckets.
 VUMI_API_CONFIG['riak_manager'] = {'bucket_prefix': 'test.'}
 VUMI_API_CONFIG['redis_manager'] = {
@@ -19,3 +18,6 @@ if os.environ.get('VUMIGO_FAST_TESTS'):
     }
 
 NOSE_ARGS = ['-evumitools', '-evumi_app', '-ehandlers', '-m^test']
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
