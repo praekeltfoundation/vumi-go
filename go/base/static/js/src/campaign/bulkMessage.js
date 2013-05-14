d398e55a9f9a1f72c023846127b25720f5bcac67
Peter Pistorius <peter.pistorius@gmail.com>
2013-05-14 14:33:19 +0100

    attached the backbone view to the form template.

--- go/base/static/js/src/campaign/bulkMessage.js
+++ go/base/static/js/src/campaign/bulkMessage.js
@@ -4,7 +4,25 @@
 
 (function(exports) {
 
-    
+    var TextareaView = Backbone.View.extend({
+        tagName: 'p',
+        SMS_TOTAL_CHARS: 160,
+
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.$textarea = this.options.$textarea;
+            this.$textarea.on('keyup', this.render);
+        },
+        render: function() {
+            var totalChars = this.$textarea.val().length;
+            var totalSMS = Math.ceil(totalChars / this.SMS_TOTAL_CHARS)
+            this.$el.html(totalChars + ' characters used<br>' 
+                          + totalSMS + ' smses');
+            return this;
+        },
+    });
+
+    exports.TextareaView = TextareaView;
 
 
 })(go.campaign.bulkMessage = {});
