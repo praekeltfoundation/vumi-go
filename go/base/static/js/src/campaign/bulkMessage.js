f4b61d778dc0b7f76f2d4cc8662b3e9769bd573b
Peter Pistorius <peter.pistorius@gmail.com>
2013-05-14 11:43:36 +0100

    properly comment bulk message screen.

--- go/base/static/js/src/campaign/bulkMessage.js
+++ go/base/static/js/src/campaign/bulkMessage.js
@@ -1,6 +1,6 @@
-// go.campaign.interactive
+// go.campaign.bulkMessage
 // ==============================
-// Models, Views and other stuff for the interactive message screen
+// Models, Views and other stuff for the bulk message compisition screen
 
 (function(exports) {
 
