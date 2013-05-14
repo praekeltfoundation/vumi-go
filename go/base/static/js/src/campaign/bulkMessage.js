71f597e77611e45c11be76a583b0cecb413caf44
Peter Pistorius <peter.pistorius@gmail.com>
2013-05-14 11:42:25 +0100

    intial javascript and template files.

---
+++ go/base/static/js/src/campaign/bulkMessage.js
@@ -0,0 +1,8 @@
+// go.campaign.interactive
+// ==============================
+// Models, Views and other stuff for the interactive message screen
+
+(function(exports) {
+
+
+})(go.campaign.bulkMessage = {});
