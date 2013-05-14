7cf0ccee1d638be78899b777d330cbecfb52114f
justinvdm <justinvdm@lavabit.com>
2013-05-14 23:15:04 +0200

    Add utils that will be needed for the plumb endpoint component

--- go/base/static/js/src/utils.js
+++ go/base/static/js/src/utils.js
@@ -10,5 +10,30 @@
   // Backbone has an internal `extend()` function which it assigns to its
   // structures. We need this function, so we arbitrarily choose
   // `Backbone.Model`, since it has the `extend()` function we are looking for.
-  exports.Extendable.extend = Backbone.Model.extend;
+  var extend = Backbone.Model.extend;
+
+  exports.Extendable.extend = function() {
+    Array.prototype.unshift.call(arguments, {parent: this.prototype});
+    return extend.call(this, _.extend.apply(this, arguments));
+  };
+
+  // Class-like object on which events can be bound and emitted
+  exports.Eventable = exports.Extendable.extend(Backbone.Events, {
+    events: {},
+    constructor: function() { this.bindEvents(); },
+    bindEvents: function() {
+      // bind events
+      var self = this,
+          events = this.events;
+          
+      for (var e in events) { this.on(e, this[events[e]].bind(this)); }
+    }
+  });
+
+  // Pop a value from a collection
+  exports.pop = function(collection, key) {
+    var value = collection[key];
+    delete collection[key];
+    return value;
+  };
 })(go.utils = {});
--- go/base/static/js/test/tests/utils.test.js
+++ go/base/static/js/test/tests/utils.test.js
@@ -1,7 +1,7 @@
 describe("go.utils", function() {
-  var Extendable = go.utils.Extendable;
-
   describe(".Extendable", function() {
+    var Extendable = go.utils.Extendable;
+
     it("should set up the prototype chain correctly", function() {
       var Parent = Extendable.extend(),
           Child = Parent.extend();
@@ -27,6 +27,78 @@ describe("go.utils", function() {
       Child = Parent.extend();
 
       assert.equal(new Child('foo').name, 'foo');
+    });
+
+    it("should provide a reference to parent prototype", function() {
+      var Parent, Child;
+      Parent = Extendable.extend({
+        constructor: function (name) { this.name = name; }
+      });
+      Child = Parent.extend({
+        constructor: function (name) { this.parent.constructor(name); }
+      });
+
+      assert.equal(new Child('foo').name, 'foo');
+    });
+  });
+
+  describe(".Eventable", function() {
+    var Eventable = go.utils.Eventable;
+
+    it("should call callbacks when their events are emitted", function(done) {
+      var Thing,
+          thing,
+          aCalled = false,
+          bCalled = false,
+          maybeDone = function() { aCalled && bCalled && done(); };
+      
+      Thing = Eventable.extend({
+        events: {'event-a': 'a', 'event-b': 'b'},
+        a: function() {
+          aCalled = true;
+          maybeDone();
+        },
+        b: function() {
+          bCalled = true;
+          maybeDone();
+        }
+      });
+
+      thing = new Thing();
+      thing.trigger('event-a');
+      thing.trigger('event-b');
+    });
+
+    it("should bind callbacks to the Eventable instance", function(done) {
+      var Thing,
+          thing;
+      
+      Thing = Eventable.extend({
+        events: {'event': 'callback'},
+        callback: function() {
+          assert.equal(this, thing);
+          done();
+        }
+      });
+
+      thing = new Thing();
+      thing.trigger('event');
+    });
+  });
+
+  describe(".pop", function() {
+    var pop = go.utils.pop;
+
+    it("should pop a property off an object", function() {
+      var obj = {a: 'one', b: 'two'};
+      assert.equal(pop(obj, 'a'), 'one');
+      assert.deepEqual(obj, {b: 'two'});
+    });
+
+    it("should handle non-existent properties without breaking", function() {
+      var obj = {a: 'one'};
+      assert.equal(pop({a: 'one'}, 'b'), undefined);
+      assert.deepEqual(obj, {a: 'one'});
     });
   });
 });
