e7f8dc0790ff4e1f661966eb76ae7a364ee876df
Jeremy Thurgood <firxen@gmail.com>
2013-05-17 16:02:52 +0200

    Eradicate last vestiges of start_timestamp and end_date.

--- go/apps/multi_surveys/views.py
+++ go/apps/multi_surveys/views.py
@@ -78,14 +78,6 @@ def new(request):
             if tag_info[2]:
                 conversation_data['delivery_tag'] = tag_info[2]
 
-            # start_date = form.cleaned_data['start_date'] or datetime.utcnow()
-            # start_time = (form.cleaned_data['start_time'] or
-            #                 datetime.utcnow().time())
-            # conversation_data['start_timestamp'] = datetime(
-            #     start_date.year, start_date.month, start_date.day,
-            #     start_time.hour, start_time.minute, start_time.second,
-            #     start_time.microsecond)
-
             conversation = request.user_api.new_conversation(
                 u'multi_survey', **conversation_data)
             messages.add_message(request, messages.INFO,
@@ -94,11 +86,7 @@ def new(request):
                 kwargs={'conversation_key': conversation.key}))
 
     else:
-        form = ConversationForm(request.user_api, initial={
-            'start_date': datetime.utcnow().date(),
-            'start_time': datetime.utcnow().time().replace(second=0,
-                                                            microsecond=0),
-        })
+        form = ConversationForm(request.user_api)
     return render(request, 'multi_surveys/new.html', {
         'form': form,
     })
