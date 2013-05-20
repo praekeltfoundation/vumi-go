a542a1aad2c93bce3993d742533653ddc40612b8
Peter Pistorius <peter.pistorius@gmail.com>
2013-05-20 14:18:50 +0100

    stub out views.

--- go/campaigns/urls.py
+++ go/campaigns/urls.py
@@ -4,6 +4,8 @@ from go.campaigns import views
 urlpatterns = patterns('',
     url(r'^details/$', views.details, name='details'),
     url(r'^details/(?P<campaign_key>\w+)/$', views.details, name='details'),
+    # TODO: message is probably not a good name for these views, considering
+    # that you get incoming messages as well.
     url(r'^message/(?P<campaign_key>\w+)/$', views.message, name='message'),
     url(r'^message/(?P<campaign_key>\w+)/bulk/$', views.message_bulk,
         name='message_bulk'),
@@ -11,4 +13,8 @@ urlpatterns = patterns('',
         views.message_conversation, name='message_conversation'),
     url(r'^contacts/(?P<campaign_key>\w+)/', views.contacts, name='contacts'),
     url(r'^preview/(?P<campaign_key>\w+)/$', views.preview, name='preview'),
+    url(r'^incoming/(?P<campaign_key>\w+)/$', 'go.base.views.todo',
+        name='incoming_list'),
+    url(r'^incoming/(?P<campaign_key>\w+)/(?P<msisdn>\d+)/$', 
+        'go.base.views.todo', name='incoming_detail'),
 )
