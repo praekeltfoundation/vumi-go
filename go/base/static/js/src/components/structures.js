9a9f881c43cf81eb35a097e11d674f0e3ecebc14
justinvdm <justinvdm@lavabit.com>
2013-05-22 21:48:15 +0200

    Add Lookup structure

--- go/base/static/js/src/components/structures.js
+++ go/base/static/js/src/components/structures.js
@@ -15,4 +15,45 @@
     // `Backbone.Model`, since it has the function we are looking for.
     return Backbone.Model.extend.call(this, merge.apply(this, arguments));
   };
+
+  // A class-like object onto which events can be bound and emitted
+  exports.Eventable = exports.Extendable.extend(Backbone.Events);
+
+  // A structure that stores key-value pairs, provides helpful operations for
+  // accessing the data, and emits events when items are added or removed.
+  // Similar to a Backbone Collection, except the contents are key-value pairs
+  // instead of models.
+  //
+  // Events emitted:
+  //   - 'add' (key, value) - Emitted when an item is added
+  //   - 'remove' (key, value) - Emitted when an item is removed
+  exports.Lookup = exports.Eventable.extend({
+    // Arguments:
+    //   - items: the initial objects to be added
+    constructor: function(items) {
+      this._items = {};
+      for (var k in items) { this.add(k, items[k]); }
+    },
+
+    keys: function() { return _.keys(this._items); },
+    values: function() { return _.values(this._items); },
+    items: function() { return _.clone(this._items); },
+
+    get: function(key) { return this._items[key] || null; },
+
+    add: function(key, value) {
+      this._items[key] = value;
+      this.trigger('add', key, value);
+      return this;
+    },
+
+    remove: function(key) {
+      var value = this._items[key];
+      if (value) {
+        delete this._items[key];
+        this.trigger('remove', key, value);
+      }
+      return value;
+    }
+  });
 })(go.components.structures = {});
--- go/base/static/js/test/tests/components/structures.test.js
+++ go/base/static/js/test/tests/components/structures.test.js
@@ -1,4 +1,6 @@
 describe("go.components.structures", function() {
+  var structures = go.components.structures;
+
   describe(".Extendable", function() {
     var Extendable = go.components.structures.Extendable;
 
@@ -37,6 +39,82 @@ describe("go.components.structures", function() {
 
       assert.equal(thing.a, 'one');
       assert.equal(thing.b, 'two');
+    });
+  });
+
+  describe(".Lookup", function() {
+    var Lookup = structures.Lookup,
+        lookup;
+
+    beforeEach(function() {
+      lookup = new Lookup({a: 1, b: 2, c: 3});
+    });
+
+    describe(".keys", function() {
+      it("should get the lookup's item's keys", function() {
+        assert.deepEqual(lookup.keys(), ['a', 'b', 'c']);
+      });
+    });
+
+    describe(".values", function() {
+      it("should get the lookup's item's values", function() {
+        assert.deepEqual(lookup.values(), [1, 2, 3]);
+      });
+    });
+
+    describe(".items", function() {
+      it("should get a shallow copy of the lookup's items", function() {
+        var items = lookup.items();
+
+        items.a = 'one';
+        assert.deepEqual(items, {a: 'one', b: 2, c: 3});
+
+        assert.deepEqual(lookup.items(), {a: 1, b: 2, c: 3});
+      });
+    });
+
+    describe(".get", function() {
+      it("should get a value by its key", function() {
+        assert.equal(lookup.get('a'), 1);
+      });
+
+      it("return null if the key does not exist", function() {
+        assert.equal(lookup.get('d'), null);
+      });
+    });
+
+    describe(".add", function() {
+      it("should add the item to the lookup", function() {
+        assert.equal(lookup.add('d', 4), lookup);
+        assert.deepEqual(lookup.items(), {a: 1, b: 2, c: 3, d: 4});
+      });
+
+      it("should emit an 'add' event", function(done) {
+        lookup.on('add', function(key, value) {
+          assert.equal(key, 'd');
+          assert.equal(value, 4);
+          done();
+        });
+
+        lookup.add('d', 4);
+      });
+    });
+
+    describe(".remove", function() {
+      it("should remove an item from the lookup", function() {
+        assert.equal(lookup.remove('c'), 3);
+        assert.deepEqual(lookup.items(), {a: 1, b: 2});
+      });
+
+      it("should emit a 'remove' event", function(done) {
+        lookup.on('remove', function(key, value) {
+          assert.equal(key, 'c');
+          assert.equal(value, 3);
+          done();
+        });
+
+        lookup.remove('c');
+      });
     });
   });
 });
