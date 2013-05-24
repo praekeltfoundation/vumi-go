a49d43008030296e916ea3a51a169aedfbaf02c2
Simon Cross <hodgestar@gmail.com>
2013-05-24 13:58:02 +0200

    Start hooking up JS log view.

--- go/apps/jsbox/urls.py
+++ go/apps/jsbox/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
-from go.apps.jsbox.views import JsboxConversationViews, cross_domain_xhr
+from go.apps.jsbox.views import (
+    JsboxConversationViews, cross_domain_xhr, jsbox_logs)
 
 urlpatterns = JsboxConversationViews().get_urlpatterns()
 
 urlpatterns += patterns('',
     url(r'^cross-domain-xhr/$', cross_domain_xhr, name="cross_domain_xhr"),
+    url(r'^jslogs/$', jsbox_logs, name="jsbox_logs"),
 )
--- go/apps/jsbox/views.py
+++ go/apps/jsbox/views.py
@@ -3,10 +3,13 @@ from urlparse import urlparse, urlunparse
 
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
 from go.conversation.base import ConversationViews
 from go.apps.jsbox.forms import JsboxForm, JsboxAppConfigFormset
+from go.apps.jsbox.log import LogManager
+from go.utils import conversation_or_404
 
 
 class JsboxConversationViews(ConversationViews):
@@ -41,3 +44,17 @@ def cross_domain_xhr(request):
 
     r = requests.get(url, auth=auth)
     return HttpResponse(r.text, status=r.status_code)
+
+
+@login_required
+def jsbox_logs(request, conversation_key):
+    campaign_key = request.user_api.user_account_key
+    conversation = conversation_or_404(request.user_api, conversation_key)
+    # TODO: find a better way to get a suitable redis instance
+    redis = request.user_api.redis.sub_manager("jslogs")
+    log_manager = LogManager(redis)
+    logs = log_manager.get_logs(campaign_key, conversation_key)
+    return render_to_response("", {
+        "conversation": conversation,
+        "jsbox_logs": reversed(logs),
+    })
