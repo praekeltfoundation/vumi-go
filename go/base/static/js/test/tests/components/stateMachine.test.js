5241ab80ba02a867ff9ed8c39c9ad54fae8cf27e
justinvdm <justinvdm@lavabit.com>
2013-06-11 20:01:47 +0200

    Add test for ConnectionModel id assignment

---
+++ go/base/static/js/test/tests/components/stateMachine.test.js
@@ -0,0 +1,21 @@
+describe("go.components.stateMachine", function() {
+  var stateMachine = go.components.stateMachine;
+
+  describe(".ConnectionModel", function() {
+    var EndpointModel = stateMachine.EndpointModel,
+        ConnectionModel = stateMachine.ConnectionModel;
+
+    var ToyConnectionModel = ConnectionModel.extend({
+      idAttribute: 'uuid'
+    });
+
+    it("should assign a unique id from its source and target", function() {
+      var endpoint = new ToyConnectionModel({
+        source: new EndpointModel({id: 'a'}),
+        target: new EndpointModel({id: 'b'})
+      });
+
+      assert.equal(endpoint.id, 'a-b');
+    });
+  });
+});
