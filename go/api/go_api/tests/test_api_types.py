fbd3776bb83d472c3e8517a9348c26add3177c64
Simon Cross <hodgestar@gmail.com>
2013-06-11 18:12:34 +0200

    Add tests for api_types.

--- go/api/go_api/tests/test_api_types.py
+++ go/api/go_api/tests/test_api_types.py
@@ -5,43 +5,113 @@ from twisted.trial.unittest import TestCase
 from vumi.rpc import RpcCheckError
 
 from go.api.go_api.api_types import (
-    ConversationType, CampaignType)
+    CampaignType, EndpointType, ConversationType, ChannelType,
+    RoutingBlockType, RoutingEntryType, RoutingType)
 
 
-class ConversationTypeTestCase(TestCase):
+class BaseTypeTest(TestCase):
+    DEFAULTS = {}
 
-    def _conv_dict(self, without=(), **kw):
-        conv_dict = kw.copy()
-        conv_dict.setdefault('key', u'conv-1')
-        conv_dict.setdefault('name', u'Conversation One')
-        conv_dict.setdefault('description', u'A Dummy Conversation')
-        conv_dict.setdefault('conversation_type', u'jsbox')
+    def mk_dict(self, without=(), **kw):
+        value = self.DEFAULTS.copy()
+        value.update(kw)
         for key in without:
-            del conv_dict[key]
-        return conv_dict
+            del value[key]
+        return value
 
-    def test_check(self):
-        conv_type = ConversationType()
-        conv_type.check('name', self._conv_dict())
-        for key in ['key', 'name', 'description', 'conversation_type']:
+    def basic_checks(self, check_type):
+        check_type.check('name', self.mk_dict())
+        for key in self.DEFAULTS:
             self.assertRaises(
-                RpcCheckError, conv_type.check, 'name',
-                self._conv_dict(without=(key,)))
+                RpcCheckError, check_type.check, 'name',
+                self.mk_dict(without=(key,)))
 
 
-class CampaignTypeTestCase(TestCase):
-    def _campaign_dict(self, without=(), **kw):
-        campaign_dict = kw.copy()
-        campaign_dict.setdefault('key', u'campaign-1')
-        campaign_dict.setdefault('name', u'Campaign One')
-        for key in without:
-            del campaign_dict[key]
-        return campaign_dict
+class CampaignTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'key': u'campaign-1',
+        u'name': u'Campaign One',
+    }
 
-    def test_check(self):
-        campaign_type = CampaignType()
-        campaign_type.check('name', self._campaign_dict())
-        for key in ['key', 'name']:
-            self.assertRaises(
-                RpcCheckError, campaign_type.check, 'name',
-                self._campaign_dict(without=(key,)))
+    def test_basic_checks(self):
+        self.basic_checks(CampaignType())
+
+
+class EndpointTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'uuid': u'endpoint-1',
+        u'name': u'Endpoint 1',
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(EndpointType())
+
+
+class ConversationTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'uuid': u'conv-1',
+        u'type': u'jsbox',
+        u'name': u'Conversation One',
+        u'description': u'A Dummy Conversation',
+        u'endpoints': [
+            {u'uuid': u'endpoint-uuid-1', u'name': u'default'}
+        ],
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(ConversationType())
+
+
+class ChannelTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'uuid': u'channel-1',
+        u'tag': [u'apposit_sms', u'*121#'],
+        u'name': u'*121#',
+        u'description': u'Apposit Sms: *121#',
+        u'endpoints': [
+            {u'uuid': u'endpoint-uuid-1', u'name': u'default'}
+        ],
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(ChannelType())
+
+
+class RoutingBlockTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'uuid': u'routing-block-uuid-1',
+        u'type': u'keyword',
+        u'name': u'keyword-routing-block',
+        u'description': u'Keyword',
+        u'channel_endpoints': [
+            {u'uuid': u'endpoint-uuid-2', u'name': u'default'}
+        ],
+        u'conversation_endpoints': [
+            {u'uuid': u'endpoint-uuid-3', u'name': u'default'}
+        ],
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(RoutingBlockType())
+
+
+class RoutingEntryTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'source': {u'uuid': u'endpoint-uuid-1'},
+        u'target': {u'uuid': u'endpoint-uuid-2'},
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(RoutingEntryType())
+
+
+class RoutingTypeTestCase(BaseTypeTest):
+    DEFAULTS = {
+        u'channels': [ChannelTypeTestCase.DEFAULTS],
+        u'routing_blocks': [RoutingBlockTypeTestCase.DEFAULTS],
+        u'conversations': [ConversationTypeTestCase.DEFAULTS],
+        u'routing_entries': [RoutingEntryTypeTestCase.DEFAULTS],
+    }
+
+    def test_basic_checks(self):
+        self.basic_checks(RoutingType())
