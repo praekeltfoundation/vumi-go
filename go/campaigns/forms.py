0f58c0ef328c3ee4924b308e276df728853bc4db
Peter Pistorius <peter.pistorius@gmail.com>
2013-06-13 12:01:24 +0100

    updating new forms.;

--- go/campaigns/forms.py
+++ go/campaigns/forms.py
@@ -1,16 +1,26 @@
 from django import forms
+from django.forms.widgets import RadioSelect
 
 
 class CampaignGeneralForm(forms.Form):
 
-    TYPE_CHOICES = (
+    KIND_CHOICES = (
         ('bulk_message', 'Bulk Message'),
         ('survey', 'Dialogue'),
     )
+    CHANNEL_CHOICES = (
+        ('new', 'Setup a new channel'),
+        ('existing', 'Use a keyword to route messages over an existing \
+            channel'),
+    )
 
-    name = forms.CharField(label="Campaign name", max_length=100)
-    type = forms.ChoiceField(label="Which kind of campaign would you like?",
-                             widget=forms.Select(), choices=TYPE_CHOICES)
+    name = forms.CharField(label="Conversation name", max_length=100)
+    kind = forms.ChoiceField(
+        label="Which kind of conversation would you like?", 
+        choices=KIND_CHOICES
+    )
+    channel = forms.ChoiceField(label="Channels", choices=CHANNEL_CHOICES,
+                                required=False, widget=RadioSelect)
 
 
 class CampaignConfigurationForm(forms.Form):
