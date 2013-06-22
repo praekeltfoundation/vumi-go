d73c60433b8002196723ae3f67d88b5b09ccce3f
justinvdm <justinvdm@lavabit.com>
2013-06-22 13:07:03 +0200

    Test helpers for asserting a model's attributes

--- go/base/static/js/test/tests/testHelpers/testHelpers.js
+++ go/base/static/js/test/tests/testHelpers/testHelpers.js
@@ -2,6 +2,8 @@
 // ==============
 
 (function(exports) {
+  var assertFails = function(fn) { assert.throws(fn, chai.AssertionError); };
+
   var oneElExists = function(selector) {
     return $(selector).length === 1;
   };
@@ -10,8 +12,25 @@
     return $(selector).length === 0;
   };
 
+  var attrsOfModel = function(obj) {
+    var attrs = obj instanceof Backbone.Model
+      ? obj.toJSON()
+      : obj;
+
+    // Backbone.rpc adds an extra `_rpcId` attribute which isn't part of
+    // our model attributes. We need to exclude it for equality testing.
+    return _(attrs).omit('_rpcId');
+  };
+
+  var assertModelAttrs = function(model, attrs) {
+    assert.deepEqual(attrsOfModel(model), attrs);
+  };
+
   _.extend(exports, {
+    assertFails: assertFails,
     oneElExists: oneElExists,
-    noElExists: noElExists
+    noElExists: noElExists,
+    attrsOfModel: attrsOfModel,
+    assertModelAttrs: assertModelAttrs
   });
 })(go.testHelpers = {});
--- go/base/static/js/test/tests/testHelpers/testHelpers.test.js
+++ go/base/static/js/test/tests/testHelpers/testHelpers.test.js
@@ -1,5 +1,6 @@
 describe("go.testHelpers", function() {
-  var testHelpers = go.testHelpers;
+  var testHelpers = go.testHelpers,
+      assertFails = testHelpers.assertFails;
 
   beforeEach(function() {
     $('body').append([
@@ -9,6 +10,8 @@ describe("go.testHelpers", function() {
         "<div id='c' class='different-thing'></div>",
       "</div>"
     ].join(''));
+
+    Backbone.Relational.store.reset();
   });
 
   afterEach(function() {
@@ -33,6 +36,44 @@ describe("go.testHelpers", function() {
     it("should determine whether no element exists", function() {
       assert(noElExists('.kjhfsdfsdf'));
       assert(!noElExists('#a'));
+    });
+  });
+
+  describe(".attrsOfModel", function() {
+    var attrsOfModel = testHelpers.attrsOfModel;
+
+    var models = go.components.models,
+        Model = models.Model;
+
+    it("should return the model's attrs if a model was given", function() {
+      assert.deepEqual(
+        attrsOfModel(new Model({uuid: 'ackbar', a: 'red', b: 'blue'})),
+        {uuid: 'ackbar', a: 'red', b: 'blue'});
+    });
+
+    it("should return the model's attrs an object was given", function() {
+      assert.deepEqual(
+        attrsOfModel({uuid: 'ackbar', a: 'red', b: 'blue', _rpcId: 132123}),
+        {uuid: 'ackbar', a: 'red', b: 'blue'});
+    });
+  });
+
+  describe(".assertModelAttrs", function() {
+    var assertModelAttrs = testHelpers.assertModelAttrs;
+
+    var models = go.components.models,
+        Model = models.Model;
+
+    it("should determine whether a model has only the given attrs", function() {
+      assertModelAttrs(
+        new Model({uuid: 'ackbar', a: 'red', b: 'blue'}),
+        {uuid: 'ackbar', a: 'red', b: 'blue'});
+
+      assertFails(function() {
+        assertModelAttrs(
+          new Model({uuid: 'anakin', a: 'red', b: 'blue'}),
+          {uuid: 'ackbar', a: 'blue', b: 'green'});
+      });
     });
   });
 });
