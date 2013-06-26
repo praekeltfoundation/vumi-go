dc5316641603df55e7bf23437f4eb808edabb1ee
justinvdm <justinvdm@lavabit.com>
2013-06-26 15:21:15 +0200

    Change Lookup to respect ordering when returning keys or iterating through items

--- go/base/static/js/src/components/structures.js
+++ go/base/static/js/src/components/structures.js
@@ -47,29 +47,45 @@
 
     constructor: function(items) {
       this._items = {};
-      this._values = [];
+      this._itemList = [];
 
       for (var k in (items || {})) {
         this.add(k, items[k], {silent: true, sort: false});
       }
 
       if (this.ordered) {
-        this.sorter = this.determineSorter(this.comparator);
+        this._initSorting();
         this.sort();
       }
     },
 
-    determineSorter: function(comparator) {
-      return _.isString(comparator) || comparator.length === 1
-        ? _.sortBy
-        : nativeSort;
+    _initSorting: function() {
+      if (_.isString(this.comparator)) {
+        this._sorter = _.sortBy;
+        this._comparator = this._stringComparator;
+      } else if (this.comparator.length === 1) {
+        this._sorter = _.sortBy;
+        this._comparator = this._iteratorComparator;
+      } else {
+        this._sorter = nativeSort;
+        this._comparator = this._nativeComparator;
+      }
+    },
+    _iteratorComparator: function(item) {
+      return this.comparator(item.value);
+    },
+    _stringComparator: function(item) {
+      return item.value[this.comparator];
+    },
+    _nativeComparator: function(item1, item2) {
+      return this.comparator(item1.value, item2.value);
     },
 
-    size: function() { return _.size(this._values); },
+    size: function() { return this._itemList.length; },
 
-    keys: function() { return _.keys(this._items); },
+    keys: function() { return _(this._itemList).pluck('key'); },
 
-    values: function() { return this._values.slice(); },
+    values: function() { return _(this._itemList).pluck('value'); },
 
     items: function() { return _.clone(this._items); },
 
@@ -77,24 +93,37 @@
 
     map: function(fn, that) { return this.values().map(fn, that); },
 
-    where: function(props) { return _.where(this._values, props); },
+    where: function(props) { return _.where(this.values(), props); },
+
+    callAt: function(i, fn, that) {
+      var item = this._itemList[i];
+      fn.call(that, item.key, item.value);
+    },
 
     eachItem: function(fn, that) {
-      var items = this._items;
-      for (var k in items) { fn.call(that, k, items[k]); }
+      var i = -1,
+          n = this.size();
+
+      while (++i < n) { this.callAt(i, fn, that); }
     },
 
     has: function(k) { return _.has(this._items, k); },
 
     get: function(key) { return this._items[key]; },
 
-    at: function(i) { return this._values[i]; },
+    at: function(i) { return this._itemList[i].value; },
+
+    keyAt: function(i) { return this._itemList[i].key; },
+
+    indexOf: function(v) { return _(this.values()).indexOf(v); },
+
+    indexOfKey: function(k) { return _(this.keys()).indexOf(k); },
 
     add: function(key, value, options) {
       options = _(options || {}).defaults(this.addDefaults);
 
       this._items[key] = value;
-      this._values.push(value);
+      this._itemList.push({key: key, value: value});
 
       if (!options.silent) { this.trigger('add', key, value); }
       if (options.sort) { this.sort(); }
@@ -108,7 +137,7 @@
       var value = this._items[key];
       if (value) {
         delete this._items[key];
-        this._values.splice(_(this._values).indexOf(value), 1);
+        this._itemList.splice(this.indexOf(value), 1);
 
         if (!options.silent) { this.trigger('remove', key, value); }
       }
@@ -117,7 +146,7 @@
 
     sort: function() {
       if (this.ordered) {
-        this._values = this.sorter(this._values, this.comparator, this);
+        this._itemList = this._sorter(this._itemList, this._comparator, this);
       }
       return this;
     }
