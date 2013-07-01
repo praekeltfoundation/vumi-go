d35f6496bc9b169f700c55eba0a299ecb80a5c73
Jeremy Thurgood <jeremy@praekelt.com>
2013-07-01 19:20:08 +0200

    Start of wizard view tests.

---
+++ go/wizard/tests.py
@@ -0,0 +1,70 @@
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+
+from go.apps.tests.base import DjangoGoApplicationTestCase
+
+
+class WizardViewsTestCase(DjangoGoApplicationTestCase):
+    # Most of the functionality of this view lives in JS, so we just test that
+    # we're correctly injecting initial state into the template.
+
+    def setUp(self):
+        super(WizardViewsTestCase, self).setUp()
+        self.setup_riak_fixtures()
+        self.client = Client()
+        self.client.login(username=self.user.username, password='password')
+
+    def test_get_create_view(self):
+        response = self.client.get(reverse('wizard:create'))
+        # Check that we have a few conversation types in the response
+        self.assertContains(response, 'bulk_message')
+        self.assertContains(response, 'subscription')
+        # Check that we have a tagpool/tag in the response
+        self.assertContains(response, 'longcode:')
+
+    def test_post_create_view_valid(self):
+        convs = len(self.user_api.active_conversations())
+        tags = len(self.user_api.list_endpoints())
+        response = self.client.post(reverse('wizard:create'), {
+            'conversation_type': 'bulk_message',
+            'name': 'My Conversation',
+            'country': 'International',
+            'channel': 'longcode:',
+        })
+        self.assertEqual(convs + 1, len(self.user_api.active_conversations()))
+        self.assertEqual(tags + 1, len(self.user_api.list_endpoints()))
+        conv = self.get_latest_conversation()
+        self.assertRedirects(
+            response, reverse('conversations:conversation', kwargs={
+                'conversation_key': conv.key, 'path_suffix': '',
+            }))
+
+    def test_post_create_view_invalid_conversation_type(self):
+        response = self.client.post(reverse('wizard:create'), {
+            'conversation_type': 'foo',
+            'name': 'My Conversation',
+            'country': 'International',
+            'channel': 'longcode:',
+        })
+        # We just assert that we don't redirect here.
+        self.assertNotEqual(response.status_code, 302)
+
+    def test_post_create_view_invalid_country(self):
+        response = self.client.post(reverse('wizard:create'), {
+            'conversation_type': 'bulk_message',
+            'name': 'My Conversation',
+            'country': 'Nowhere',
+            'channel': 'longcode:',
+        })
+        # We just assert that we don't redirect here.
+        self.assertNotEqual(response.status_code, 302)
+
+    def test_post_create_view_invalid_channel(self):
+        response = self.client.post(reverse('wizard:create'), {
+            'conversation_type': 'bulk_message',
+            'name': 'My Conversation',
+            'country': 'International',
+            'channel': 'badpool:',
+        })
+        # We just assert that we don't redirect here.
+        self.assertNotEqual(response.status_code, 302)
