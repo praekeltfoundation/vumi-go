3fa8caf2a6236ad94b4b53cc1dd8563f2f662482
Jeremy Thurgood <jeremy@praekelt.com>
2013-07-03 10:41:46 +0200

    Basic channel dashboard page.

--- go/channel/templatetags/channel_tags.py
+++ go/channel/templatetags/channel_tags.py
@@ -1,3 +1,5 @@
+import urllib
+
 from django import template
 
 from go.channel.views import get_channel_view_definition
@@ -10,4 +12,5 @@ register = template.Library()
 def channel_screen(channel, view_name='show'):
     # TODO: Update this when we have proper channels.
     view_def = get_channel_view_definition(channel)
-    return view_def.get_view_url(view_name, channel_key=channel.key)
+    return view_def.get_view_url(
+        view_name, channel_key=urllib.quote(channel.key))
