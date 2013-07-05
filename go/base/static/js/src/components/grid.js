edbafe6bde2c14be3da4e81c033991c5d21642bc
justinvdm <justinvdm@lavabit.com>
2013-07-05 15:18:35 +0200

    GridView attempt 2

--- go/base/static/js/src/components/grid.js
+++ go/base/static/js/src/components/grid.js
@@ -3,126 +3,14 @@
 // Components for self-maintaining grids of items.
 
 (function(exports) {
-  var utils = go.utils,
-      idOfView = utils.idOfView;
+  var Lookup = go.components.structures.Lookup;
 
-  var views = go.components.views,
-      UniqueView = views.UniqueView;
+  var GridItems = Lookup.extend({
+    ordered: true,
+    comparator: function($el) { return $el.data('grid:index'); },
 
-  var structures = go.components.structures,
-      ViewCollection = structures.ViewCollection;
-
-  var RowItemView = UniqueView.extend({
-    span: 3,
-
-    className: 'item',
-
-    initialize: function(options) {
-      this.item = options.item;
-
-      this.span = this.spanOfEl(this.item.$el) || options.span || this.span;
-      this.$el.addClass('span' + this.span);
-      if (options.extraClass) { this.$el.addClass(options.extraClass); }
-
-      var itemId = idOfView(this.item);
-      this.uuid = 'item:' + itemId;
-      this.$el.data('item-id', itemId);
-    },
-
-    _spanRe: /span[0-9]/g,
-
-    spanOfEl: function($el) {
-      var classes = ($el.attr('class') || '').match(this._spanRe);
-
-      if (classes && classes.length === 1) {
-        var span = parseInt(classes[0].slice(4), 10);
-        if (!isNaN(span)) { return span; }
-      }
-
-      return null;
-    },
-
-    render: function() {
-      this.$el.append(this.item.$el);
-      this.item.render();
-      return this;
-    }
-  });
-
-  var RowView = Backbone.View.extend({
-    className: 'row',
-
-    initialize: function(options) {
-      options = options || {};
-      if (options.extraClass) { this.$el.addClass(options.extraClass); }
-
-      this.spanSum = 0;
-      this.items = [];
-    },
-
-    add: function(item) {
-      this.items.push(item);
-      this.spanSum += item.span;
-      return this;
-    },
-
-    render: function() {
-      this.items.forEach(function(item) {
-        this.$el.append(item.$el);
-        item.render();
-      }, this);
-
-      return this;
-    }
-  });
-
-  var RowCollection = ViewCollection.extend({
-    addDefaults: _({
-      render: false,
-      addModel: false
-    }).defaults(ViewCollection.prototype.addDefaults),
-
-    type: RowView,
-    rowItemType: RowItemView,
-
-    maxSpan: 12,
-
-    initialize: function(options) {
-      this.rowClass = options.rowClass;
-      this.itemClass = options.itemClass;
-      this.itemSpan = options.itemSpan;
-
-      this.rowItemType = options.rowItemType || this.rowItemType;
-      this._ensureArray(options.items).forEach(this.addItem, this);
-    },
-
-    _ensureArray: function(obj) {
-      return obj instanceof ViewCollection
-        ? obj.values()
-        : obj;
-    },
-
-    viewOptions: function() {
-      return {
-        id: 'row' + this.size(),
-        extraClass: this.rowClass
-      };
-    },
-
-    addItem: function(item) {
-      var row = this.last() || this.add();
-
-      var rowItem = new this.rowItemType({
-        item: item,
-        span: this.itemSpan,
-        extraClass: this.itemClass
-      });
-
-      if (row.spanSum + rowItem.span > this.maxSpan) { row = this.add(); }
-
-      row.add(rowItem);
-      return this;
-    }
+    arrangeable: true,
+    arranger: function($el, i) { $el.data('grid:index', i); }
   });
 
   // A self-maintaining grid of items.
@@ -130,104 +18,98 @@
   // Options:
   //   - items: A collection of views to be maintained in the grid
   var GridView = Backbone.View.extend({
-    className: 'grid container',
-
-    rowType: RowView,
-    rowItemType: RowItemView,
-    rowCollectionType: RowCollection,
+    rowClassName: 'row',
 
     sortableOptions: {},
 
     initialize: function(options) {
-      this.items = this._ensureViewCollection(options.items);
-      this.sortableOptions = options.sortableOptions || this.sortableOptions;
+      this.items = new GridItems();
+      this.width = options.width || this.$el.outerWidth();
 
-      if (options.gridClass) { this.$el.addClass(options.gridClass); }
-
-      this.rowClass = options.rowClass;
-      this.itemClass = options.itemClass;
-      this.itemSpan = options.itemSpan;
-
-      this.rowType = options.rowType || this.rowType;
-      this.rowItemType = options.rowItemType || this.rowItemType;
-      this.rowCollectionType = options.rowCollectionType
-                            || this.rowCollectionType;
-
-      this.items.on('add', this.render, this);
-      this.items.on('remove', this.render, this);
-
-      this.resetRows();
+      if (options.rowClassName) { this.rowClassName = options.rowClassName; }
+      if (options.sortable) { this.sortableOptions = options.sortable; }
     },
 
-    _ensureViewCollection: function(obj) {
-      return obj instanceof ViewCollection
-        ? obj
-        : new ViewCollection({
-          orderable: true,
-          arrangeable: true,
-          views: obj
-        });
+    $rows: function() { return this.$('.' + this.rowClassName); },
+
+    _ensureEl: function(viewOrEl) {
+      return viewOrEl instanceof Backbone.View
+        ? view.$el
+        : $(viewOrEl);
     },
 
-    resetRows: function() {
-      this.rows = new this.rowCollectionType({
-        items: this.items,
-        type: this.rowType,
-        rowItemType: this.rowItemType,
-        rowClass: this.rowClass,
-        itemSpan: this.itemSpan,
-        itemClass: this.itemClass
+    add: function(key, item) {
+      item = this._ensureEl(item);
+
+      item.data({
+        'grid:key': key,
+        'grid:index': this.items.size()
       });
+
+      this.items.add(key, item);
+    },
+
+    clear: function() {
+      this.items = new GridItems();
+    },
+
+    _reorder: function() {
+      var newOrder = this.$rows()
+        .children()
+        .map(function() { return $(this).data('grid:key'); })
+        .get();
+
+      this.items.rearrange(newOrder);
       return this;
-    },
-
-    itemOrder: function() {
-      return this.$('.row').map(function() {
-        return $(this)
-          .find('.item')
-          .map(function() { return $(this).data('item-id'); }).get();
-      }).get();
-    },
-
-    // NOTE: The grid relies on the items collection to rearrange its items
-    // correctly. The grid shouldn't know (or be responsible for) how item and
-    // model ordering happens. If this isn't handled correctly, the ui ordering
-    // will be lost.
-    reorder: function() {
-      this.items.rearrange(this.itemOrder());
-      this.render();
     },
 
     _sortableOptions: function() {
       var opts = _(this).result('sortableOptions');
 
       return _({}).extend(opts, {
-        connectWith: '.row',
+        connectWith: '.' + this.rowClassName,
         stop: function(e, ui) {
-          this.reorder();
+          this._reorder();
+          this.render();
           if (opts.stop) { opts.stop(e, ui); }
         }.bind(this)
       });
     },
 
+    _resetRows: function() {
+      this.$rows().children().detach();
+      this.$rows().remove();
+      this._newRow();
+      return this;
+    },
+
+    _newRow: function() {
+      this._$lastRow = $('<div>').addClass(this.rowClassName);
+      this._remainingWidth = this.width;
+      this.$el.append(this._$lastRow);
+      return this;
+    },
+
+    _append: function(item) {
+      var itemWidth = item.outerWidth();
+
+      if (this._remainingWidth < itemWidth) { this._newRow(); }
+      this._remainingWidth -= itemWidth;
+
+      this._$lastRow.append(item);
+      return this;
+    },
+
     render: function() {
-      this.rows.each(function(r) { r.$el.detach(); });
-      this.resetRows();
-
-      this.rows.each(function(r) { this.$el.append(r.$el); }, this);
-      this.rows.render();
-
-      this.$('.row').sortable(this._sortableOptions());
-
-      this.trigger('render');
+      this._resetRows();
+      this.items.each(this._append, this);
+      this.$rows().sortable(this._sortableOptions());
       return this;
     }
   });
 
   _.extend(exports, {
-    RowView: RowView,
-    RowItemView: RowItemView,
-    RowCollection: RowCollection,
+    GridItems: GridItems,
     GridView: GridView
   });
 })(go.components.grid = {});
