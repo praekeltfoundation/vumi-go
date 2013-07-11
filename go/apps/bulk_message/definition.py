fb0354a22ac3be04729d929540504e374c192a6c
Simon Cross <hodgestar@gmail.com>
2013-07-11 07:54:32 +0200

    Disable bulk send action when a bulk send conversation has no suitable channels attached.

--- go/apps/bulk_message/definition.py
+++ go/apps/bulk_message/definition.py
@@ -11,6 +11,14 @@ class BulkSendAction(ConversationAction):
     needs_group = True
     needs_running = True
 
+    def check_disabled(self):
+        channels = self._conv.get_channels()
+        for channel in channels:
+            if channel.supports_generic_sends():
+                return None
+        return ("This action needs channels capable of sending"
+                " messages attached to this conversation.")
+
     def perform_action(self, action_data):
         return self.send_command(
             'bulk_send', batch_id=self._conv.get_latest_batch_key(),
