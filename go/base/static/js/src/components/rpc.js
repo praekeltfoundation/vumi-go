ab89817699a82e7fe4c334cb3e6a9c645a6bbe32
justinvdm <justinvdm@lavabit.com>
2013-07-19 06:59:59 +0200

    Basic rpc sync method (to replace Backbone.rpc)

---
+++ go/base/static/js/src/components/rpc.js
@@ -0,0 +1,42 @@
+// go.components.rpc
+// =================
+// Components for rpc-ifying Backbone.
+
+(function(exports) {
+  var rpcData = function(method, model) {
+    var spec = _(model).result('methods')[method];
+
+    return JSON.stringify({
+      id: uuid.v4(),
+      jsonrpc: '2.0',
+      method: spec.method,
+      params: spec.params.map(function(p) {
+        return p === 'self'
+          ? model
+          : model.get(p);
+      })
+    });
+  };
+
+  var ajaxOptions = function(method, model, options) {
+    return {
+      contentType: 'application/json; charset=utf-8',
+      type: 'POST',
+      dataType: 'json',
+      data: rpcData(method, model)
+    };
+  };
+
+  var sync = function(method, model, options) {
+    options = _(options || {}).extend(ajaxOptions(method, model, options));
+
+    var success = options.success;
+    if (success) { options.success = function(resp) { success(resp.result); }; }
+
+    return Backbone.sync(method, model, options);
+  };
+
+  _.extend(exports, {
+    sync: sync
+  });
+})(go.components.rpc = {});
---
+++ go/base/static/js/test/tests/components/rpc.test.js
@@ -0,0 +1,60 @@
+describe("go.components.rpc", function() {
+  var rpc = go.components.rpc;
+
+  var testHelpers = go.testHelpers,
+      assertModelAttrs = testHelpers.assertModelAttrs,
+      fakeServer = testHelpers.rpc.fakeServer;
+
+  describe.only(".sync", function() {
+    var ToyModel = Backbone.Model.extend({
+      url: '/test',
+      methods: {
+        read: {method: 'r', params: ['foo', 'bar']}
+      },
+
+      sync: function() { return rpc.sync.apply(this, arguments); }
+    });
+
+    var server,
+        model;
+
+    beforeEach(function() {
+      server = fakeServer('/test');
+
+      model = new ToyModel({
+        foo: 'lerp',
+        bar: 'larp'
+      });
+    });
+
+    afterEach(function() {
+      server.restore();
+    });
+
+    it("should make a request using the corresponding model method and params",
+    function() {
+      rpc.sync('read', model);
+      server.assertRequest('r', ['lerp', 'larp']);
+    });
+
+    it("should allow the model instance to be used as a param", function() {
+      var Model = Backbone.Model.extend({
+        url: '/test',
+        methods: {
+          read: {method: 'r', params: ['self']}
+        }
+      });
+
+      rpc.sync('read', new Model({foo: 'lerp', bar: 'larp'}));
+      server.assertRequest('r', [{foo: 'lerp', bar: 'larp'}]);
+    });
+
+    describe("fetching", function() {
+      it("should update the model using on the rpc response", function() {
+        model.fetch();
+        server.respondWith({foo: 'lerp', bar: 'larp'});
+        assertModelAttrs(model, {foo: 'lerp', bar: 'larp'});
+      });
+    });
+  });
+});
