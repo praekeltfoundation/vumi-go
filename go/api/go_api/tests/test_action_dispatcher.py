22b87959099056e8189f40805bb8320c2cfbfb57
Simon Cross <hodgestar@gmail.com>
2013-08-01 16:29:37 +0200

    Add tests for unknown_action and dispatching to action handlers that generate exceptions.

--- go/api/go_api/tests/test_action_dispatcher.py
+++ go/api/go_api/tests/test_action_dispatcher.py
@@ -1,6 +1,10 @@
 """Tests for go.api.go_api.action_dispatcher."""
 
+from mock import Mock
+
 from twisted.trial.unittest import TestCase
+
+from vumi.tests.utils import LogCatcher
 
 from go.api.go_api.action_dispatcher import (
     ActionDispatcher, ActionError, ConversationActionDispatcher,
@@ -8,8 +12,28 @@ from go.api.go_api.action_dispatcher import (
 
 
 class ActionDispatcherTestCase(TestCase):
+
     def test_dispatcher_type_name(self):
         self.assertEqual(ActionDispatcher.dispatcher_type_name, None)
+
+    def test_unknown_action(self):
+        dispatcher = ActionDispatcher(Mock())
+        obj = Mock(key="abc")
+        self.assertRaises(ActionError, dispatcher.unknown_action,
+                          obj, foo="bar")
+
+    def test_dispatch_action_which_errors(self):
+        dispatcher = ActionDispatcher(Mock())
+        obj = Mock(key="abc")
+        with LogCatcher() as lc:
+            self.assertRaises(ActionError, dispatcher.dispatch_action,
+                              obj, "weird_action", {"foo": "bar"})
+            [err] = lc.errors
+            self.assertEqual(err["why"],
+                             "Action 'weird_action' on None %r (key: 'abc')"
+                             " with params {'foo': 'bar'} failed." % obj)
+        [err] = self.flushLoggedErrors(ActionError)
+        self.assertEqual(err.value.faultString, "Unknown action.")
 
 
 class ConversationAcitonDispatcherTestCase(TestCase):
