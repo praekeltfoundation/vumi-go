9ccb373eb65a7e928de2d33157c6435fb5c1e3f0
Simon Cross <hodgestar@gmail.com>
2013-08-02 17:10:33 +0200

    Tests for GoApiSubHandler.

--- go/api/go_api/tests/test_utils.py
+++ go/api/go_api/tests/test_utils.py
@@ -1,8 +1,10 @@
 """Tests for go.api.go_api.utils."""
 
+from mock import Mock
+
 from twisted.trial.unittest import TestCase
 
-from go.api.go_api.utils import GoApiError
+from go.api.go_api.utils import GoApiError, GoApiSubHandler
 
 
 class GoApiErrorTestCase(TestCase):
@@ -15,3 +17,28 @@ class GoApiErrorTestCase(TestCase):
         err = GoApiError("Other", fault_code=314)
         self.assertEqual(err.faultString, "Other")
         self.assertEqual(err.faultCode, 314)
+
+
+class GoApiSubHandlerTestCase(TestCase):
+    def setUp(self):
+        self.account = Mock(key=u"account-1")
+        self.user_api = Mock()
+        self.vumi_api = Mock(
+            get_user_api=Mock(return_value=self.user_api),
+        )
+
+    def test_create(self):
+        sub = GoApiSubHandler(self.account.key, self.vumi_api)
+        self.assertEqual(sub.user_account_key, self.account.key)
+        self.assertEqual(sub.vumi_api, self.vumi_api)
+
+    def test_get_user_api(self):
+        sub = GoApiSubHandler(self.account.key, self.vumi_api)
+        user_api = sub.get_user_api(self.account.key)
+        self.assertTrue(
+            self.vumi_api.get_user_api.called_once_with(self.account.key))
+        self.assertEqual(user_api, self.user_api)
+
+    def test_get_user_api_with_invalid_campaign_key(self):
+        sub = GoApiSubHandler(self.account.key, self.vumi_api)
+        self.assertRaises(GoApiError, sub.get_user_api, u"foo")
