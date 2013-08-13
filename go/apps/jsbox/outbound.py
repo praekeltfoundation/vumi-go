94a26a57793857f7cad2c8d815e8cd3b1f1ade59
Simon Cross <hodgestar@gmail.com>
2013-08-13 17:29:40 +0200

    Use endpoints to determine whether sending is allowed.

--- go/apps/jsbox/outbound.py
+++ go/apps/jsbox/outbound.py
@@ -142,12 +142,13 @@ class GoOutboundResource(SandboxResource):
         log.info("Sending outbound message to %r via tag %r, content: %r" %
                  (to_addr, tag, content))
 
-        user_api = self.app_worker.user_api_for_api(api)
-        tags = yield user_api.list_endpoints()
+        conv = self.app_worker.conversation_for_api(api)
+        tags = [tuple(endpoint.split(":", 1))
+                for endpoint in conv.extra_endpoints]
         if tag not in tags:
             returnValue(self._mkfail(
                 command, reason="Tag %r not held by account" % (tag,)))
-        conv = self.app_worker.conversation_for_api(api)
+
         msg_options = {}
         self.app_worker.add_conv_to_msg_options(conv, msg_options)
         endpoint = ':'.join(tag)
