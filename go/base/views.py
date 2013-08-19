4cf5a175d0d3b6eabf58f0d48f81c61a4b1595ed
justinvdm <justinvdm.0.1.0@gmail.com>
2013-08-19 16:20:56 +0200

    Revert "Revert "Move cross domain xhr view out of jsbox app's view definition and into the base views (using /cross-domain-xhr/ instead of /apps/jsbox/cross-domain-xhr/, which doesn't work)""
    This reverts commit 58618b41fcf212f21ab41f44a2e2663e14c484ef.

--- go/base/views.py
+++ go/base/views.py
@@ -1,6 +1,42 @@
+import requests
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
+from urlparse import urlparse, urlunparse
 
 
 def todo(request):  # pragma: no cover
     return render(request, 'base/todo.html', {
     })
+
+
+@csrf_exempt
+@login_required
+def cross_domain_xhr(request):
+    url = request.POST.get('url', None)
+
+    parse_result = urlparse(url)
+    if parse_result.username:
+        auth = (parse_result.username, parse_result.password)
+        url = urlunparse(
+            (parse_result.scheme,
+             ('%s:%s' % (parse_result.hostname, parse_result.port)
+              if parse_result.port
+              else parse_result.hostname),
+             parse_result.path,
+             parse_result.params,
+             parse_result.query,
+             parse_result.fragment))
+    else:
+        auth = None
+        url = url
+
+    response = requests.get(url, auth=auth)
+
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+        content_type=response.headers['content-type'])
