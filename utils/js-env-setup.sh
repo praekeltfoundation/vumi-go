b9b2b094c13a7084ba780416bbfb2c06892db2fa
justinvdm <justinvdm.0.1.0@gmail.com>
2013-09-09 16:59:34 +0200

    Change js-env-setup.sh to locate globals.npm relative the script's location

--- utils/js-env-setup.sh
+++ utils/js-env-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-npm install --prefix=$VIRTUAL_ENV -g `cat globals.npm | tr '\n' ' '`
+PROJECT_ROOT="`dirname $0`/../"
+npm install --prefix=$VIRTUAL_ENV -g `cat $PROJECT_ROOT/globals.npm | tr '\n' ' '`
 npm install
