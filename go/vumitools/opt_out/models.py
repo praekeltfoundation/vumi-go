981d0e26b0d2f364d60c6b0051a2a4be5b85d975
Simon Cross <hodgestar@gmail.com>
2013-10-02 15:42:09 +0200

    Fix opt_outs_for_addresses so that it now handles empty addresses lists.

--- go/vumitools/opt_out/models.py
+++ go/vumitools/opt_out/models.py
@@ -45,8 +45,12 @@ class OptOutStore(PerAccountStore):
     def list_opt_outs(self):
         return self.list_keys(self.opt_outs)
 
+    @Manager.calls_manager
     def opt_outs_for_addresses(self, addr_type, addresses):
+        if not addresses:
+            returnValue([])
         keys = ["%s:%s" % (addr_type, address) for address in addresses]
         mr = self.manager.mr_from_keys(self.opt_outs, keys)
         mr.filter_not_found()
-        return mr.get_keys()
+        opt_out_keys = yield mr.get_keys()
+        returnValue(opt_out_keys)
