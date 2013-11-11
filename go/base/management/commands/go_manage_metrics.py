23f62bc5238c6a6d6eed7c02b1387f540d053a27
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-11 15:48:44 +0200

    Change go_manage_metrics command to support listing which accounts have metric collection disabled

--- go/base/management/commands/go_manage_metrics.py
+++ go/base/management/commands/go_manage_metrics.py
@@ -2,17 +2,26 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 
-from go.base.utils import vumi_api_for_user
-from go.base.command_utils import get_user_by_email
+from go.base.utils import vumi_api, vumi_api_for_user
+from go.base.command_utils import (
+    get_user_by_email, get_user_by_account_key, user_details_as_string)
 
 
 class Command(BaseCommand):
+    encoding = 'utf-8'
+
     help = "Disable or re-enable metric collection for a Vumi Go user"
 
-    LOCAL_OPTIONS = [
+    LOCAL_OPTIONS = dict((opt.dest, opt) for opt in [
         make_option('--email-address',
             dest='email-address',
             help='Email address for the Vumi Go user'),
+        make_option('--list',
+            dest='list',
+            action='store_true',
+            default=False,
+            help='List the Vumi Go user accounts that currently have '
+                 'metric collection disabled'),
         make_option('--enable',
             dest='enable',
             action='store_true',
@@ -23,41 +32,66 @@ class Command(BaseCommand):
             action='store_true',
             default=False,
             help='Disable metric collection for the Vumi Go user'),
-    ]
-    option_list = BaseCommand.option_list + tuple(LOCAL_OPTIONS)
+    ])
+
+    option_list = BaseCommand.option_list + tuple(LOCAL_OPTIONS.values())
 
     def handle(self, *args, **options):
-        options = options.copy()
-        for opt in self.LOCAL_OPTIONS:
-            if options.get(opt.dest) is None:
-                value = raw_input("%s: " % (opt.help,))
-                if value:
-                    options[opt.dest] = value
-                else:
-                    raise CommandError('Please provide %s:' % (opt.dest,))
+        action = self.get_action(options)
 
-        enable = options['enable']
-        disable = options['disable']
+        if action == 'list':
+            self.list_users()
+        else:
+            email_addr = (
+                options.get('email-address') or
+                self.ask_for_option('email-address'))
 
-        if (enable and disable) or not (enable or disable):
+            if action == 'enable':
+                self.enable_metrics(email_addr)
+            elif action == 'disable':
+                self.disable_metrics(email_addr)
+
+    def ask_for_option(self, name):
+        opt = self.LOCAL_OPTIONS[name]
+        value = raw_input("%s: " % (opt.help,))
+        if value:
+            return value
+        else:
+            raise CommandError('Please provide %s:' % (opt.dest,))
+
+    def get_action(self, options):
+        actions = [
+            name for name in ['enable', 'disable', 'list']
+            if options[name]]
+
+        if len(actions) != 1:
             raise CommandError(
-                'Please specify either --enable or --disable.')
+                'Please specify either --list, --enable or --disable.')
 
-        self.handle_validated(*args, **options)
+        return actions[0]
 
-    def handle_validated(self, *args, **options):
-        email_address = options['email-address']
-        enable = options['enable']
-        disable = options['disable']
-
-        user = get_user_by_email(email_address)
+    def enable_metrics(self, email_addr):
+        user = get_user_by_email(email_addr)
         user_api = vumi_api_for_user(user)
         user_account_key = user_api.user_account_key
-
-        # TODO use riak instead of riak for this command
         redis = user_api.api.redis
+        redis.srem('disabled_metrics_accounts', user_account_key)
 
-        if enable:
-            redis.srem('disabled_metrics_accounts', user_account_key)
-        elif disable:
-            redis.sadd('disabled_metrics_accounts', user_account_key)
+    def disable_metrics(self, email_addr):
+        user = get_user_by_email(email_addr)
+        user_api = vumi_api_for_user(user)
+        user_account_key = user_api.user_account_key
+        redis = user_api.api.redis
+        redis.sadd('disabled_metrics_accounts', user_account_key)
+
+    def list_users(self):
+        api = vumi_api()
+        acc_keys = api.redis.smembers('disabled_metrics_accounts')
+
+        if not acc_keys:
+            self.stderr.write('No accounts have metric collection disabled.\n')
+        else:
+            for i, acc_key in enumerate(acc_keys):
+                user = get_user_by_account_key(acc_key)
+                output = u"%s. %s\n" % (i, user_details_as_string(user))
+                self.stdout.write(output.encode(self.encoding))
--- go/base/tests/test_go_manage_metrics.py
+++ go/base/tests/test_go_manage_metrics.py
@@ -1,3 +1,5 @@
+from StringIO import StringIO
+
 from go.base.utils import vumi_api_for_user
 from go.base.tests.utils import VumiGoDjangoTestCase
 from go.base.management.commands import go_manage_metrics
@@ -13,9 +15,24 @@ class GoManageApplicationCommandTestCase(VumiGoDjangoTestCase):
         self.user_account_key = user_api.user_account_key
         self.redis = user_api.api.redis
 
-    def set_metrics(self, user, disabled):
+        self.stdout = StringIO()
+        self.stderr = StringIO()
+
+    def run_command(self, **kw):
         command = go_manage_metrics.Command()
-        command.handle_validated(**{
+        command.stdout = self.stdout
+        command.stderr = self.stderr
+
+        opts = {
+            'list': False,
+            'enable': False,
+            'disable': False,
+        }
+        opts.update(kw)
+        command.handle(**opts)
+
+    def set_metrics(self, user, disabled):
+        self.run_command(**{
             'email-address': user.email,
             'enable': not disabled,
             'disable': disabled,
@@ -23,11 +40,13 @@ class GoManageApplicationCommandTestCase(VumiGoDjangoTestCase):
 
     def test_enable_metrics(self):
         self.set_metrics(self.user, disabled=True)
+
         self.assertEqual(
             set([self.user_account_key]),
             self.redis.smembers('disabled_metrics_accounts'))
 
         self.set_metrics(self.user, disabled=False)
+
         self.assertEqual(
             set(),
             self.redis.smembers('disabled_metrics_accounts'))
@@ -54,13 +73,18 @@ class GoManageApplicationCommandTestCase(VumiGoDjangoTestCase):
             set(),
             self.redis.smembers('disabled_metrics_accounts'))
 
-    def test_disable_metrics_when_disabled(self):
+    def test_listing(self):
         self.set_metrics(self.user, disabled=True)
-        self.assertEqual(
-            set([self.user_account_key]),
-            self.redis.smembers('disabled_metrics_accounts'))
+        self.run_command(list=True)
 
-        self.set_metrics(self.user, disabled=True)
         self.assertEqual(
-            set([self.user_account_key]),
-            self.redis.smembers('disabled_metrics_accounts'))
+            self.stdout.getvalue(),
+            '0. Test User <user@domain.com> [%s]\n' % (
+                self.user.get_profile().user_account))
+
+    def test_listing_for_no_accounts_disabled(self):
+        self.run_command(list=True)
+
+        self.assertEqual(
+            self.stderr.getvalue(),
+            'No accounts have metrics disabled.\n')
