cbd2032047bb0d7a4b4c0c4eea4bd59698eb9c89
Simon de Haan <simon@praekeltfoundation.org>
2013-11-12 15:50:26 +0200

    better tests (thanks @justinvdm)

--- go/apps/jsbox/tests/test_message_store.py
+++ go/apps/jsbox/tests/test_message_store.py
@@ -39,6 +39,20 @@ class MessageStoreResourceTestCase(ResourceTestCaseBase, GoAppWorkerTestMixin):
 
         self.conversation = yield self.create_conversation(started=True)
 
+        # store inbound
+        yield self.message_store.add_inbound_message(
+            self.msg_helper.make_inbound('hello'),
+            batch_id=self.conversation.batch.key)
+
+        # store outbound
+        outbound_msg = self.msg_helper.make_outbound('hello')
+        yield self.message_store.add_outbound_message(
+            outbound_msg, batch_id=self.conversation.batch.key)
+
+        # ack outbound
+        event = self.msg_helper.make_ack(outbound_msg)
+        yield self.message_store.add_event(event)
+
         # monkey patch for testing!
         self.app_worker.conversation_for_api = lambda *a: self.conversation
 
@@ -57,42 +71,50 @@ class MessageStoreResourceTestCase(ResourceTestCaseBase, GoAppWorkerTestMixin):
     def test_handle_progress_status(self):
         reply = yield self.dispatch_command('progress_status')
         self.assertTrue(reply['success'])
-        self.assertTrue(isinstance(reply['progress_status'], dict))
+        self.assertEqual(reply['progress_status'], {
+            'ack': 1,
+            'delivery_report': 0,
+            'delivery_report_delivered': 0,
+            'delivery_report_failed': 0,
+            'delivery_report_pending': 0,
+            'nack': 0,
+            'sent': 1,
+        })
 
     @inlineCallbacks
     def test_handle_count_replies(self):
         reply = yield self.dispatch_command('count_replies')
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['count'], 0)
+        self.assertEqual(reply['count'], 1)
 
     @inlineCallbacks
     def test_handle_count_sent_messages(self):
         reply = yield self.dispatch_command('count_sent_messages')
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['count'], 0)
+        self.assertEqual(reply['count'], 1)
 
     @inlineCallbacks
     def test_handle_count_inbound_uniques(self):
         reply = yield self.dispatch_command('count_inbound_uniques')
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['count'], 0)
+        self.assertEqual(reply['count'], 1)
 
     @inlineCallbacks
     def test_handle_count_outbound_uniques(self):
         reply = yield self.dispatch_command('count_outbound_uniques')
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['count'], 0)
+        self.assertEqual(reply['count'], 1)
 
     @inlineCallbacks
     def test_handle_inbound_throughput(self):
         reply = yield self.dispatch_command('inbound_throughput',
-                                            sample_time=1)
+                                            sample_time=60)
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['throughput'], 0)
+        self.assertEqual(reply['throughput'], 1)
 
     @inlineCallbacks
     def test_handle_outbound_throughput(self):
         reply = yield self.dispatch_command('outbound_throughput',
-                                            sample_time=1)
+                                            sample_time=60)
         self.assertTrue(reply['success'])
-        self.assertEqual(reply['throughput'], 0)
+        self.assertEqual(reply['throughput'], 1)
