b106db6a3b807e73fa5d5f4b35430e5d1033bd94
Jeremy Thurgood <firxen@gmail.com>
2013-11-19 09:59:27 +0200

    Fix middleware test assertion.

--- go/vumitools/tests/test_middleware.py
+++ go/vumitools/tests/test_middleware.py
@@ -313,7 +313,8 @@ class MetricsMiddlewareTestCase(MiddlewareTestCase):
         yield mw.handle_inbound(msg1, 'dummy_endpoint')
         key = mw.key('dummy_endpoint', msg1['message_id'])
         ttl = yield mw.redis.ttl(key)
-        self.assertTrue(0 < ttl < 10)
+        self.assertTrue(
+            0 < ttl <= 10, "Expected 0 < ttl <= 10, found: %r" % (ttl,))
 
 
 class ConversationStoringMiddlewareTestCase(MiddlewareTestCase):
