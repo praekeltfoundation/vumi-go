cc4e083931e350e7f31b9d1aea5420ff948043ea
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-19 19:02:49 +0200

    Add FakeResponse and FakeServer test utils

---
+++ go/base/tests/test_testutils.py
@@ -0,0 +1,61 @@
+import requests
+
+from go.vumitools.tests.utils import GoTestCase
+from go.base.tests.utils import FakeResponse, FakeServer
+
+
+class TestFakeResponse(GoTestCase):
+    def test_json_content(self):
+        resp = FakeResponse(data={'foo': 'bar'})
+        self.assertEqual(resp.json, {'foo': 'bar'})
+
+    def test_plain_content(self):
+        resp = FakeResponse(content='foo')
+        self.assertEqual(resp.content, 'foo')
+
+    def test_error_raising(self):
+        resp = FakeResponse(code=500)
+        self.assertRaises(requests.exceptions.HTTPError, resp.raise_for_status)
+
+
+class TestFakeServer(GoTestCase):
+    def setUp(self):
+        super(TestFakeServer, self).setUp()
+        self.server = FakeServer()
+
+    def tearDown(self):
+        super(TestFakeServer, self).tearDown()
+        self.server.tear_down()
+
+    def test_request_catching(self):
+        requests.request('get', 'http://some.place')
+        requests.get('http://some.place')
+        requests.post('http://some.place')
+        requests.put('http://some.place')
+        requests.head('http://some.place')
+        requests.patch('http://some.place')
+        requests.options('http://some.place')
+        requests.delete('http://some.place')
+
+        self.assertEqual(self.server.get_requests(), [
+            {'method': 'get', 'url': 'http://some.place'},
+            {'method': 'get', 'url': 'http://some.place'},
+            {'method': 'post', 'url': 'http://some.place'},
+            {'method': 'put', 'url': 'http://some.place'},
+            {'method': 'head', 'url': 'http://some.place'},
+            {'method': 'patch', 'url': 'http://some.place'},
+            {'method': 'options', 'url': 'http://some.place'},
+            {'method': 'delete', 'url': 'http://some.place'}])
+
+    def test_request_response_setting(self):
+        resp = FakeResponse()
+        self.server.set_response(resp)
+
+        self.assertEqual(resp, requests.request('get', 'http://some.place'))
+        self.assertEqual(resp, requests.get('http://some.place'))
+        self.assertEqual(resp, requests.post('http://some.place'))
+        self.assertEqual(resp, requests.put('http://some.place'))
+        self.assertEqual(resp, requests.head('http://some.place'))
+        self.assertEqual(resp, requests.patch('http://some.place'))
+        self.assertEqual(resp, requests.options('http://some.place'))
+        self.assertEqual(resp, requests.delete('http://some.place'))
