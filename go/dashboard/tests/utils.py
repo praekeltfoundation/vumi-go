62c573fadad1b0268353c2dc21c35ac5b645052a
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-22 14:34:30 +0200

    Add raw_request method for FakeDiamondashApiClient to correspond to the recently added DiamondashApiClient.raw_request()

--- go/dashboard/tests/utils.py
+++ go/dashboard/tests/utils.py
@@ -6,10 +6,14 @@ from go.dashboard import DiamondashApiError, DiamondashApiClient
 class FakeDiamondashApiClient(DiamondashApiClient):
     def __init__(self):
         self.requests = []
-        self.response = None
+        self._response = None
 
-    def get_requests(self):
-        return self.requests
+    @property
+    def response(self):
+        if isinstance(self._response, Exception):
+            raise self._response
+
+        return self._response
 
     def set_error_response(self, code, message):
         data = json.dumps({
@@ -17,10 +21,13 @@ class FakeDiamondashApiClient(DiamondashApiClient):
             'message': message
         })
 
-        self.response = DiamondashApiError("(%s) %s" % (code, data))
+        self._response = DiamondashApiError("(%s) %s" % (code, data))
 
     def set_response(self, response):
-        self.response = response
+        self._response = response
+
+    def get_requests(self):
+        return self.requests
 
     def request(self, method, url, data):
         self.requests.append({
@@ -29,7 +36,13 @@ class FakeDiamondashApiClient(DiamondashApiClient):
             'data': data,
         })
 
-        if isinstance(self.response, Exception):
-            raise self.response
+        return self.response
+
+    def raw_request(self, method, url, content=""):
+        self.requests.append({
+            'method': method,
+            'url': url,
+            'content': content,
+        })
 
         return self.response
