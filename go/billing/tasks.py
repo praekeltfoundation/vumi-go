346fc5b07679fb899a2dc474fc9550c994b19e48
Andrei Todorov <andrei@teamgeek.co.za>
2013-11-28 12:30:00 +0200

    Monthly Statement task spawns sub-tasks for each account

--- go/billing/tasks.py
+++ go/billing/tasks.py
@@ -11,39 +11,52 @@ from go.billing.models import Account, Transaction, Statement, LineItem
 
 
 @task(ignore_result=True)
+def generate_monthly_statement(account_id, from_date, to_date):
+    """Generate a new *Monthly* ``Statement`` for the given ``account_id``
+       between the given ``from_date`` and ``to_date``.
+    """
+    account = Account.objects.get(id=account_id)
+    transaction_list = Transaction.objects\
+        .filter(account_number=account.account_number,
+                created__gte=from_date,
+                created__lt=(to_date + relativedelta(days=1)))\
+        .values('tag_pool_name', 'tag_name', 'message_direction')\
+        .annotate(total_cost=Sum('credit_amount'))
+
+    statement = Statement(
+        account=account,
+        title=settings.MONTHLY_STATEMENT_TITLE,
+        type=Statement.TYPE_MONTHLY,
+        from_date=from_date,
+        to_date=to_date)
+
+    statement.save()
+
+    line_item_list = []
+    for transaction in transaction_list:
+        line_item_list.append(LineItem(
+            statement=statement,
+            tag_pool_name=transaction.get('tag_pool_name', ''),
+            tag_name=transaction.get('tag_name', ''),
+            message_direction=transaction.get('message_direction', ''),
+            total_cost=transaction.get('total_cost', 0)))
+
+    statement.lineitem_set.bulk_create(line_item_list)
+
+
+@task(ignore_result=True)
 def generate_monthly_account_statements():
+    """Spawn sub-tasks to generate a *Monthly* ``Statement`` for accounts
+       without a *Monthly* statement.
+    """
     today = date.today()
     last_month = today - relativedelta(months=1)
     from_date = date(last_month.year, last_month.month, 1)
     to_date = date(today.year, today.month, 1) - relativedelta(days=1)
     account_list = Account.objects.exclude(
+        statement__type=Statement.TYPE_MONTHLY,
         statement__from_date=from_date,
         statement__to_date=to_date)
 
     for account in account_list:
-        transaction_list = Transaction.objects\
-            .filter(account_number=account.account_number,
-                    created__gte=from_date,
-                    created__lt=(to_date + relativedelta(days=1)))\
-            .values('tag_pool_name', 'tag_name', 'message_direction')\
-            .annotate(total_cost=Sum('credit_amount'))
-
-        statement = Statement(
-            account=account,
-            title=settings.MONTHLY_STATEMENT_TITLE,
-            type=Statement.TYPE_MONTHLY,
-            from_date=from_date,
-            to_date=to_date)
-
-        statement.save()
-
-        line_item_list = []
-        for transaction in transaction_list:
-            line_item_list.append(LineItem(
-                statement=statement,
-                tag_pool_name=transaction.get('tag_pool_name', ''),
-                tag_name=transaction.get('tag_name', ''),
-                message_direction=transaction.get('message_direction', ''),
-                total_cost=transaction.get('total_cost', 0)))
-
-        statement.lineitem_set.bulk_create(line_item_list)
+        generate_monthly_statement.delay(account.id, from_date, to_date)
