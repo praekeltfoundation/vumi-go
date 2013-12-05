c340d42fb8b4bcb8c49d323c60b5d703a65f1114
Jeremy Thurgood <jeremy@praekelt.com>
2013-12-05 13:46:37 +0200

    Properly filter migrations out of pep8 output. (@Hodgestar)

--- run-tests.sh
+++ run-tests.sh
@@ -13,5 +13,5 @@ echo "=== Running tests..."
 export PYTHONPATH=.
 ./run-tests-pytest.sh --cov=go
 echo "=== Checking for PEP-8 violations..."
-pep8 --repeat go | grep -v '^go/\(base\|billing\)/\(auth_\)?migrations/' | tee pep8.txt
+pep8 --repeat go | grep -v '^go/\(base\|billing\)/\(auth_\|registration_\)\?migrations/' | tee pep8.txt
 echo "=== Done."
