e83d5a216625985c9640b54e87b425d78e6c15a4
Jeremy Thurgood <jeremy@praekelt.com>
2013-12-12 18:52:32 +0200

    Nuke obsolete context processor.

--- go/base/context_processors.py
+++ go/base/context_processors.py
@@ -14,15 +14,6 @@ def standard_forms(request):
         }
 
 
-def credit(request):
-    # TODO: Fill this in with real data.
-    if request.user.is_authenticated() and hasattr(request, 'user_api'):
-        return {
-            'account_credits': 0,
-        }
-    return {}
-
-
 def google_analytics(request):
     return {
         'google_analytics_ua': getattr(settings, "GOOGLE_ANALYTICS_UA", None),
