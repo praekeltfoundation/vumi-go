d89bc8fcf6ea227341a10a1bdda1a68961e7c84a
Simon Cross <hodgestar@gmail.com>
2014-01-08 17:56:26 +0200

    Fix edit view test.

--- go/apps/rapidsms/tests/test_views.py
+++ go/apps/rapidsms/tests/test_views.py
@@ -37,18 +37,20 @@ class TestRapidSmsViews(GoDjangoTestCase):
             'rapidsms-rapidsms_auth_method': 'basic',
             'rapidsms-rapidsms_http_method': 'POST',
             'rapidsms-allowed_endpoints': 'default',
-            'rapidsms-auth_token': 'auth-token-1',
+            'auth_tokens-auth_token': 'auth-token-1',
         }, follow=True)
         self.assertRedirects(response, conv_helper.get_view_url('show'))
         reloaded_conv = conv_helper.get_conversation()
         self.assertEqual(reloaded_conv.config, {
             'rapidsms': {
                 u'allowed_endpoints': [u'default'],
-                u'api_tokens': [u'auth-token-1'],
                 u'rapidsms_auth_method': u'basic',
                 u'rapidsms_http_method': u'POST',
                 u'rapidsms_password': u'rapid_pass',
                 u'rapidsms_url': u'http://www.example.com/',
                 u'rapidsms_username': u'rapid_user',
-            }
+            },
+            'auth_tokens': {
+                u'api_tokens': [u'auth-token-1'],
+            },
         })
