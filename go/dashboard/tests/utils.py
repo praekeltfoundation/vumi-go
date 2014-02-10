6aebbdcb0b8b013b56265418d7f81a892350c770
Simon Cross <hodgestar@gmail.com>
2014-02-10 14:10:24 +0200

    Proxy diamondash errors to the client and test.

--- go/dashboard/tests/utils.py
+++ go/dashboard/tests/utils.py
@@ -21,7 +21,8 @@ class FakeDiamondashApiClient(DiamondashApiClient):
             'message': message
         })
 
-        self._response = DiamondashApiError("(%s) %s" % (code, data))
+        self._response = DiamondashApiError(
+            code, data, "(%s) %s" % (code, data))
 
     def set_raw_response(self, content="", code=200):
         self._response = {
