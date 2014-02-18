b562e38733fde50beb7446bf304b0f5fbc95a9fc
Simon Cross <hodgestar@gmail.com>
2014-02-18 18:43:24 +0200

    Add test for reconcile all active conversations in all accounts.

--- go/base/tests/test_go_manage_message_cache.py
+++ go/base/tests/test_go_manage_message_cache.py
@@ -21,7 +21,7 @@ class TestGoManageMessageCache(GoCommandTestCase):
             expected_output, 'reconcile',
             email_address=self.user_email, conversation_key=conv.key)
 
-    def test_reconcile_active_conversations(self):
+    def test_reconcile_active_conversations_in_account(self):
         conv1 = self.user_helper.create_conversation(u"http_api")
         conv2 = self.user_helper.create_conversation(u"http_api")
         batch_ids = sorted([conv1.batch.key, conv2.batch.key])
@@ -38,3 +38,23 @@ class TestGoManageMessageCache(GoCommandTestCase):
         self.assert_command_output(
             expected_output, 'reconcile',
             email_address=self.user_email, active_conversations=True)
+
+    def test_reconcile_active_conversations_in_all_accounts(self):
+        user1 = self.vumi_helper.make_django_user("user1")
+        user2 = self.vumi_helper.make_django_user("user2")
+        conv1 = user1.create_conversation(u"http_api")
+        conv2 = user2.create_conversation(u"http_api")
+        expected_output = "\n".join([
+            u'Processing account Test User <user1> [test-1-user] ...',
+            u'  Performing reconcile on'
+            u' batch %s ...' % conv1.batch.key,
+            u'done.',
+            u'Processing account Test User <user2> [test-2-user] ...',
+            u'  Performing reconcile on'
+            u' batch %s ...' % conv2.batch.key,
+            u'done.',
+            u''
+        ])
+        self.assert_command_output(
+            expected_output, 'reconcile',
+            active_conversations=True)
