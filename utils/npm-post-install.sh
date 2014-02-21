ac9305077dc79d262174b943da206df7b12843b7
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-21 09:43:48 +0200

    Ensure script doesn't exit early when making symlinks in npm post install script

--- utils/npm-post-install.sh
+++ utils/npm-post-install.sh
@@ -3,9 +3,13 @@
 BINS='grunt bower yuglify'
 PROJECT_ROOT=$(cd "`dirname $0`/.."; pwd)
 
-if [ -n $VIRTUAL_ENV ]; then
-  for bin in $BINS
-  do
-    ln -s $PROJECT_ROOT/node_modules/.bin/$bin $VIRTUAL_ENV/bin/$bin
-  done
-fi
+function make_links {
+    if [ -n $VIRTUAL_ENV ]; then
+        for bin in $BINS
+        do
+            ln -s $PROJECT_ROOT/node_modules/.bin/$bin $VIRTUAL_ENV/bin/$bin
+        done
+    fi
+}
+
+make_links || :
