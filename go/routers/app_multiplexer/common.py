7fe7f3e4360a49baf14e415848391e3d729745a4
Vincent Geddes <vincent@praekeltconsulting.com>
2014-02-25 12:23:16 +0200

    remove unused smart_truncate function

--- go/routers/app_multiplexer/common.py
+++ go/routers/app_multiplexer/common.py
@@ -11,15 +11,3 @@ def clean(content):
 def mkmenu(options, start=1, format='%s) %s'):
     items = [format % (idx, opt) for idx, opt in enumerate(options, start)]
     return '\n'.join(items)
-
-
-def smart_truncate(content, k, sfx='...'):
-    """
-    Useful for truncating text strings in the following manner:
-
-    'MyFancyTeam' => 'MyFancyT...'
-    """
-    if len(content) <= k:
-        return content
-    else:
-        return content[:k - len(sfx)] + sfx
