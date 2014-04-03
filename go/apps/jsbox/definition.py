5c3c21295bbb5975e55f98792e38c52109114dab
Simon Cross <hodgestar@gmail.com>
2014-04-03 14:16:13 +0200

    Add action definition for pushing Jsbox messages.

--- go/apps/jsbox/definition.py
+++ go/apps/jsbox/definition.py
@@ -4,6 +4,27 @@ from go.vumitools.conversation.definition import (
     ConversationDefinitionBase, ConversationAction)
 
 
+class SendJsboxAction(ConversationAction):
+    action_name = 'send_jsbox'
+    action_display_name = 'Push next message'
+
+    needs_confirmation = True
+
+    needs_group = True
+    needs_running = True
+
+    def check_disabled(self):
+        if self._conv.has_channel_supporting_generic_sends():
+            return None
+        return ("This action needs channels capable of sending"
+                " messages attached to this conversation.")
+
+    def perform_action(self, action_data):
+        return self.send_command(
+            'send_jsbox', batch_id=self._conv.batch.key,
+            delivery_class=self._conv.delivery_class)
+
+
 class ViewLogsAction(ConversationAction):
     action_name = 'view_logs'
     action_display_name = 'View Sandbox Logs'
@@ -14,7 +35,10 @@ class ConversationDefinition(ConversationDefinitionBase):
     conversation_type = 'jsbox'
     conversation_display_name = 'Javascript App'
 
-    actions = (ViewLogsAction,)
+    actions = (
+        SendJsboxAction,
+        ViewLogsAction,
+    )
 
     def configured_endpoints(self, config):
         app_config = config.get("jsbox_app_config", {})
