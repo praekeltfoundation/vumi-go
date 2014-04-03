a325d9e01477eac126433d75710737502e9f66a3
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-03 15:42:32 +0200

    Add the basics for adding different state types to the dialogue jsbox app

--- go/apps/dialogue/tests/test_vumi_app.js
+++ go/apps/dialogue/tests/test_vumi_app.js
@@ -1,38 +1,60 @@
 require('mocha-as-promised')();
+var assert = require('assert');
+
+var _ = require('lodash');
 var vumigo = require('vumigo_v02');
+var AppTester = vumigo.AppTester;
+
 var app = require('../vumi_app');
 var DialogueApp = app.DialogueApp;
-var AppTester = vumigo.AppTester;
+var dummy_polls = require('./dummy_polls');
 
 describe.only("app", function() {
     describe("DialogueApp", function() {
         var tester;
         
         beforeEach(function() {
-            tester = new AppTester(new DialogueApp());
+            tester = new AppTester(new DialogueApp())
+                .setup.config.app({name: 'dialogue_app'})
+                .setup.config({poll: dummy_polls.simple_poll});
+        });
 
-            tester.setup.config.app({
-                name: 'dialogue_app'
-            });
+        it("should throw an error if an unknown state type is encountered",
+        function() {
+            return tester
+                .setup.config({
+                    poll: _.extend({}, dummy_polls.simple_poll, {
+                        states: [{type: 'unknown'}]
+                    })
+                })
+                .run()
+                .catch(function(e) {
+                    assert(e instanceof Error);
+                    assert.equal(
+                        e.message,
+                        "Unknown dialogue state type: 'unknown'");
+                });
         });
 
         describe("when the user enters a choice state", function() {
             it("should display the state's question");
-            it("should respond to valid input");
-            it("should respond to invalid input");
+            it("should move the user to the next state on valid input");
+            it("should stay on the same state on invalid input");
             it("should store the user's answer");
-            it("should support accepting label and number based answers");
+            it("should only accept number based answers if asked");
+            it("should accept label and number based answers if asked");
         });
 
         describe("when the user enters a freetext state", function() {
             it("should display the state's question");
-            it("should respond to input");
+            it("should move the user to the next state on valid input");
             it("should store the user's answer");
         });
 
         describe("when the user enters an end state", function() {
             it("should display the state's text");
-            it("should put the user on the start state next session");
+            it("should move to the start state next session if asked");
+            it("should not move to the start state next session if asked");
         });
     });
 });
--- go/apps/dialogue/vumi_app.js
+++ go/apps/dialogue/vumi_app.js
@@ -1,11 +1,75 @@
+var Q = require('q');
+var _ = require('lodash');
 var vumigo = require('vumigo_v02');
 
 var App = vumigo.App;
+var State = vumigo.states.State;
 var InteractionMachine = vumigo.InteractionMachine;
 
 
 var DialogueApp = App.extend(function(self) {
-    App.call(self, null);
+    App.call(self, 'states:start');
+
+    self.poll_defaults = {
+        states: [],
+        repeatable: false,
+        accept_labels: true,
+        start_state: {uuid: null}
+    };
+
+    self.events = {
+        'im im:shutdown': function() {
+            return self.contacts.save(self.contact);
+        }
+    };
+
+    self.init = function() {
+        return Q
+            .all([self.get_poll(), self.im.contacts.for_user()])
+            .spread(function(poll, contact) {
+                self.poll = poll;
+                self.contact = contact;
+
+                self.poll.states.forEach(self.add_state);
+            });
+    };
+
+    self.get_poll = function() {
+        return self
+            .im.sandbox_config.get('poll', {json: true})
+            .then(function(poll) {
+                return _.defaults(poll, self.poll_defaults);
+           });
+    };
+
+    self.add_state = function(desc) {
+        var type = self.types[desc.type];
+
+        if (!type) {
+            throw new Error(
+                "Unknown dialogue state type: '" + desc.type + "'");
+        }
+
+        return self.states.add(type(desc));
+    };
+
+    self.types = {};
+
+    self.types.choice = function(desc) {
+        return new State(desc.uuid);
+    };
+
+    self.types.freetext = function(desc) {
+        return new State(desc.uuid);
+    };
+
+    self.types.end = function(desc) {
+        return new State(desc.uuid);
+    };
+
+    self.states.add('states:start', function() {
+        return self.states.create(self.poll.start_state.uuid);
+    });
 });
 
 
