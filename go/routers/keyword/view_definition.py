072bc480cbc489cd89d03405026f152934893b7e
Simon Cross <hodgestar@gmail.com>
2014-04-07 10:13:43 +0200

    Revert "Remove unnecessary and broken DELETE check."
    This reverts commit 7906153b4718f34ed31c193a8e80b171e567209c.
    
    Reverting commit accidentally commited straight to develop.

--- go/routers/keyword/view_definition.py
+++ go/routers/keyword/view_definition.py
@@ -17,7 +17,7 @@ class BaseKeywordFormSet(forms.formsets.BaseFormSet):
     def to_config(self):
         keyword_endpoint_mapping = {}
         for form in self:
-            if not form.is_valid():
+            if (not form.is_valid()) or form.cleaned_data['DELETE']:
                 continue
             keyword = form.cleaned_data['keyword']
             target_endpoint = form.cleaned_data['target_endpoint']
