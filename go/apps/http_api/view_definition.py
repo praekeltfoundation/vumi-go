e267f4994c70e3739ee6507ea9b3e62290ea3f2f
Simon de Haan <simon@praekeltfoundation.org>
2014-04-08 21:36:35 +0200

    add __all__

--- go/apps/http_api/view_definition.py
+++ go/apps/http_api/view_definition.py
@@ -1,2 +1,4 @@
 # NOTE: this uses the exact same view definition as the no-stream HTTP API
 from go.apps.http_api_nostream import ConversationViewDefinition
+
+__all__ = ['ConversationViewDefinition']
