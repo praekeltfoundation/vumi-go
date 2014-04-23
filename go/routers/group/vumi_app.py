8c88da6083189ca6813ec9c879307eb1d673cfbc
Jeremy Thurgood <jeremy@praekelt.com>
2014-04-23 17:39:15 +0200

    Clean up a bunch of unnecessary code.

--- go/routers/group/vumi_app.py
+++ go/routers/group/vumi_app.py
@@ -3,23 +3,16 @@
 from twisted.internet.defer import inlineCallbacks
 
 from vumi import log
-from vumi.config import ConfigList, ConfigDict
+from vumi.config import ConfigList
 
 from go.vumitools.app_worker import GoRouterWorker
 from go.vumitools.contact import ContactError, ContactNotFoundError
-from go.vumitools.api import VumiUserApi, VumiApi
 
 
 class GroupRouterConfig(GoRouterWorker.CONFIG_CLASS):
     rules = ConfigList(
         "List of groups and endpoint pairs",
         default=[])
-    riak_manager = ConfigDict(
-        'How to connect to Riak.',
-        default={}, static=True, required=False)
-    redis_manager = ConfigDict(
-        'How to connect to Redis.',
-        default={}, static=True, required=False)
 
 
 class GroupRouter(GoRouterWorker):
@@ -31,6 +24,8 @@ class GroupRouter(GoRouterWorker):
     worker_name = 'group_router'
 
     def endpoint_for_contact(self, config, contact):
+        if contact is None:
+            return 'default'
         contact_groups = set(contact.groups.keys())
         rules = dict([(x['group'], x['endpoint']) for x in config.rules])
         for group_key in contact_groups:
@@ -43,32 +38,16 @@ class GroupRouter(GoRouterWorker):
     def handle_inbound(self, config, msg, conn_name):
         log.msg("Handling inbound: %s" % (msg,))
 
-        user_account = config.router.user_account
-        # NOTE: we're passing in an empty dict for the config here,
-        #       we only have the vumi.config.Config object here, which
-        #       doesn't have support for the {}.get('foo') calls that
-        #       VumiApi.from_config_async does.
-        api = yield VumiUserApi.from_config_async(user_account.key, {
-            'riak_manager': config.riak_manager,
-            'redis_manager': config.redis_manager,
-        })
-        contact_store = api.contact_store
-        addr = msg['from_addr']
-        dclass = api.delivery_class_for_msg(msg)
-
         try:
-            contact = yield contact_store.contact_for_addr(
-                dclass,
-                addr,
-                create=False
-            )
+            contact = yield self.get_contact_for_message(msg, create=False)
         except ContactNotFoundError:
-            yield self.publish_inbound(msg, 'default')
+            contact = None
         except ContactError:
             log.err()
-        else:
-            endpoint = self.endpoint_for_contact(config, contact)
-            yield self.publish_inbound(msg, endpoint)
+            return
+
+        endpoint = self.endpoint_for_contact(config, contact)
+        yield self.publish_inbound(msg, endpoint)
 
     def handle_outbound(self, config, msg, conn_name):
         log.msg("Handling outbound: %s" % (msg,))
