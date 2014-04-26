e706f0196e91c7c09589098862e4e542ebdafeee
Jeremy Thurgood <jeremy@praekelt.com>
2014-04-26 18:48:58 +0200

    Log dialogue app events from Python instead of JS.

--- go/apps/dialogue/vumi_app.py
+++ go/apps/dialogue/vumi_app.py
@@ -2,8 +2,10 @@
 
 import pkg_resources
 import json
+import logging
 
 from vumi.application.sandbox import SandboxResource
+from twisted.internet.defer import inlineCallbacks
 
 from go.apps.jsbox.vumi_app import JsBoxConfig, JsBoxApplication
 
@@ -63,3 +65,17 @@ class DialogueApplication(JsBoxApplication):
     CONFIG_CLASS = DialogueConfig
 
     worker_name = 'dialogue_application'
+
+    @inlineCallbacks
+    def process_event_in_sandbox(self, event):
+        """
+        We don't want to process events in the sandbox, just log them.
+
+        We use the sandbox logging resource to pretend this happened in the
+        sandbox, however.
+        """
+        config = yield self.get_config(event)
+        api = self.create_sandbox_api(self.resources, config)
+        log_msg = "Saw %s for message %s." % (
+            event['event_type'], event['sent_message_id'])
+        yield api.log(log_msg, logging.info)
