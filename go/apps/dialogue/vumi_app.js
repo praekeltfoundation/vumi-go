dce557cfd3cdab6d597bbd4d4a2f77d99a1bcd67
Jeremy Thurgood <jeremy@praekelt.com>
2014-04-26 18:57:31 +0200

    Remove dialogue JS event handling.

--- go/apps/dialogue/vumi_app.js
+++ go/apps/dialogue/vumi_app.js
@@ -20,15 +20,6 @@ var DialogueApp = App.extend(function(self) {
         poll_metadata: {repeatable: false}
     };
 
-    self.events = {
-        'im inbound_event': function (event) {
-            var ev = event.event;
-            return event.im.log.info(
-                "Saw " + ev.event_type + " for message " +
-                ev.sent_message_id + ".");
-        }
-    };
-
     self.init = function() {
         return Q
             .all([self.get_poll(), self.im.contacts.for_user()])
