c48458091a15bc6cdc4bcdf800b82474a81db6d2
Simon Cross <hodgestar@gmail.com>
2014-05-08 20:31:04 +0200

    Add sphinxcontrib.blockdiag to the Sphinx extensions list.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,11 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinxcontrib.blockdiag',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
