1d8833b7f7f5e00b661256ba6f33ec58259cdd1e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-05-20 11:06:41 +0200

    Fix false positive dialogue edit view test caused by `yield`s

--- go/apps/dialogue/tests/test_views.py
+++ go/apps/dialogue/tests/test_views.py
@@ -107,6 +107,11 @@ class TestDialogueViews(GoDjangoTestCase):
         conv_helper = self.setup_conversation(
             with_group=False, started=False, name=u"myconv")
 
+        conversation = conv_helper.get_conversation()
+        group1 = self.app_helper.create_group(u'group1')
+        group2 = self.app_helper.create_group(u'group2')
+        conversation.add_group(group1)
+
         poll = {"foo": "bar"}
         self.mock_rpc.set_response(result={"poll": poll})
         response = self.client.get(conv_helper.get_view_url('edit'))
@@ -119,11 +124,6 @@ class TestDialogueViews(GoDjangoTestCase):
         self.assertContains(response, 'Mxit')
         self.assertContains(response, 'WeChat')
         self.assertContains(response, 'Twitter')
-
-        conversation = conv_helper.get_conversation()
-        group1 = yield self.app_helper.create_group(u'group1')
-        group2 = yield self.app_helper.create_group(u'group2')
-        conversation.add_group(group1)
 
         expected = poll.copy()
         expected.update({
