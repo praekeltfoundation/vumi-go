8e99c47abad59ae2c4f1bf5960841820692effe9
Jeremy Thurgood <jeremy@praekelt.com>
2014-05-22 14:09:47 +0200

    Better metrics service component test setup.

--- go/services/metrics/tests/test_service_component.py
+++ go/services/metrics/tests/test_service_component.py
@@ -1,5 +1,5 @@
 
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 from vumi.config import ConfigError
 from vumi.tests.helpers import VumiTestCase
@@ -16,6 +16,18 @@ class TestMetricsServiceComponent(VumiTestCase):
     def setUp(self):
         self.service_helper = yield self.add_helper(
             ServiceComponentHelper(u'metrics', is_sync=self.is_sync))
+
+    @inlineCallbacks
+    def build_component(self, config_override=None, name=u"myservice"):
+        config = {u"metrics_prefix": u"foo"}
+        if config_override is not None:
+            config.update(config_override)
+        service = yield self.service_helper.create_service_component(
+            name=name, config=config)
+        service_api = yield self.service_helper.get_service_component_api(
+            service.key)
+        component = yield service_api.get_service_component_object(service)
+        returnValue(component)
 
     def full_metric(self, metric_suffix, account_key="test-0-user"):
         return u'go.campaigns.%s.%s' % (account_key, metric_suffix)
@@ -47,14 +59,9 @@ class TestMetricsServiceComponent(VumiTestCase):
 
     @inlineCallbacks
     def test_fire_metric(self):
-        service = yield self.service_helper.create_service_component(
-            name=u"myservice", config={
-                u"metrics_prefix": u"foo",
-                u"metrics": [{u"name": u"bar"}],
-            })
-        service_api = yield self.service_helper.get_service_component_api(
-            service.key)
-        component = yield service_api.get_service_component_object(service)
+        component = yield self.build_component({u"metrics": [
+            {u"name": u"bar"},
+        ]})
         self.assertEqual(self.service_helper.get_published_metrics(), [])
         component.fire_metric("bar", 1)
         self.assertEqual(self.service_helper.get_published_metrics(), [
@@ -63,27 +70,16 @@ class TestMetricsServiceComponent(VumiTestCase):
 
     @inlineCallbacks
     def test_fire_metric_undefined(self):
-        service = yield self.service_helper.create_service_component(
-            name=u"myservice", config={u"metrics_prefix": u"foo"})
-        service_api = yield self.service_helper.get_service_component_api(
-            service.key)
-        component = yield service_api.get_service_component_object(service)
+        component = yield self.build_component({u"metrics": []})
         self.assertEqual(self.service_helper.get_published_metrics(), [])
         self.assertRaises(MissingMetricError, component.fire_metric, "bar", 1)
         self.assertEqual(self.service_helper.get_published_metrics(), [])
 
     @inlineCallbacks
     def test_fire_metric_multiple_aggregators(self):
-        service = yield self.service_helper.create_service_component(
-            name=u"myservice", config={
-                u"metrics_prefix": u"foo",
-                u"metrics": [
-                    {u"name": u"bar", u"aggregators": ['min', 'max']},
-                ],
-            })
-        service_api = yield self.service_helper.get_service_component_api(
-            service.key)
-        component = yield service_api.get_service_component_object(service)
+        component = yield self.build_component({u"metrics": [
+            {u"name": u"bar", u"aggregators": ['min', 'max']},
+        ]})
         self.assertEqual(self.service_helper.get_published_metrics(), [])
         component.fire_metric("bar", 1)
         self.assertEqual(
