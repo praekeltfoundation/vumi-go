3820eee75cb9816a8445679146b2080d4ebc3cfc
Simon Cross <hodgestar@gmail.com>
2014-06-25 11:36:31 +0200

    Better names for user helpers.

--- go/tests/test_urls.py
+++ go/tests/test_urls.py
@@ -9,34 +9,38 @@ class TestLoginAs(GoDjangoTestCase):
         self.vumi_helper = self.add_helper(DjangoVumiApiHelper())
         self.superuser_helper = self.vumi_helper.make_django_user(
             superuser=True, email='superuser@example.com')
-        self.user_helper_1 = self.vumi_helper.make_django_user(
-            email='user1@example.com')
-        self.user_helper_2 = self.vumi_helper.make_django_user(
-            email='user2@example.com')
+        self.ordinary_user_helper = self.vumi_helper.make_django_user(
+            email='ordinary_user@example.com')
+        self.target_user_helper = self.vumi_helper.make_django_user(
+            email='loginas_target@example.com')
 
         self.superuser_client = self.vumi_helper.get_client(
             username='superuser@example.com')
-        self.user_client_1 = self.vumi_helper.get_client(
-            username='user1@example.com')
+        self.ordinary_user_client = self.vumi_helper.get_client(
+            username='ordinary_user@example.com')
 
     def test_successful_login_as(self):
         """
         Superusers should be able to use login-as.
         """
-        user_2_pk = self.user_helper_2.get_django_user().pk
+        target_user_pk = self.target_user_helper.get_django_user().pk
         response = self.superuser_client.get(
-            reverse('loginas-user-login', kwargs={'user_id': str(user_2_pk)}))
+            reverse('loginas-user-login',
+                    kwargs={'user_id': str(target_user_pk)})
+        )
         self.assertRedirects(response, reverse('home'), target_status_code=302)
         self.assertEqual(response.client.session.get('_go_user_account_key'),
-                         'test-2-user')
+                         self.target_user_helper.account_key)
 
     def test_failed_login_as(self):
         """
         Ordinary users should not be able to use login-as.
         """
-        user_2_pk = self.user_helper_2.get_django_user().pk
-        response = self.user_client_1.get(
-            reverse('loginas-user-login', kwargs={'user_id': str(user_2_pk)}))
+        target_user_pk = self.target_user_helper.get_django_user().pk
+        response = self.ordinary_user_client.get(
+            reverse('loginas-user-login',
+                    kwargs={'user_id': str(target_user_pk)})
+        )
         self.assertRedirects(response, reverse('home'), target_status_code=302)
         self.assertEqual(response.client.session.get('_go_user_account_key'),
-                         'test-1-user')
+                         self.ordinary_user_helper.account_key)
