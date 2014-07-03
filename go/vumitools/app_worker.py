ab6bd18f2ee0afb1ee1f6803af6ed141bebb6d38
Simon Cross <hodgestar@gmail.com>
2014-07-03 13:46:11 +0200

    Add log for stopping routers.

--- go/vumitools/app_worker.py
+++ go/vumitools/app_worker.py
@@ -444,6 +444,8 @@ class GoRouterMixin(GoWorkerMixin):
 
     @inlineCallbacks
     def process_command_stop(self, user_account_key, router_key):
+        log.info("Stopping router '%s' for user '%s'." % (
+            router_key, user_account_key))
         router = yield self.get_router(user_account_key, router_key)
         if router is None:
             log.warning(
