7daac88916ab4d67803f2a7609d5e68e95123e6d
Simon Cross <hodgestar@gmail.com>
2014-10-28 09:15:08 +0200

    Add PDF link to billing statement admin view.

--- go/billing/tests/test_admin.py
+++ go/billing/tests/test_admin.py
@@ -1,14 +1,19 @@
 """ Test for billing admin. """
 
-from go.base.tests.helpers import GoDjangoTestCase, DjangoVumiApiHelper
-
 from django.core.urlresolvers import reverse
+
+from go.base.tests.helpers import GoDjangoTestCase, DjangoVumiApiHelper
+from go.billing.models import Account
+
+from .helpers import mk_statement
 
 
 class TestStatementAdmin(GoDjangoTestCase):
     def setUp(self):
         self.vumi_helper = self.add_helper(DjangoVumiApiHelper())
         self.user_helper = self.vumi_helper.make_django_user(superuser=True)
+        self.account = Account.objects.get(
+            user=self.user_helper.get_django_user())
 
     def test_admin_accessible(self):
         client = self.vumi_helper.get_client()
@@ -21,3 +26,12 @@ class TestStatementAdmin(GoDjangoTestCase):
         self.assertContains(response, "Statements")
         self.assertContains(response, "Tag pools")
         self.assertContains(response, "Transactions")
+
+    def test_link_to_pdf_view(self):
+        statement = mk_statement(self.account)
+        client = self.vumi_helper.get_client()
+        client.login()
+        response = client.get(reverse('admin:billing_statement_changelist'))
+        self.assertContains(response, "1 statement")
+        self.assertContains(
+            response, '<a href="/billing/statement/%s">pdf</a>' % statement.id)
