7134cab397a3dccd70ccbf7fef6846a67fb6e45c
Simon Cross <hodgestar@gmail.com>
2014-10-28 09:13:55 +0200

    Move transaction and statement creation test helpers into a separate module.

--- go/billing/tests/test_views.py
+++ go/billing/tests/test_views.py
@@ -1,13 +1,11 @@
 """ Tests for PDF billing statement generation """
 
-from datetime import date
-from dateutil.relativedelta import relativedelta
-from decimal import Decimal
-
 from django.core.urlresolvers import reverse
+
 from go.base.tests.helpers import DjangoVumiApiHelper, GoDjangoTestCase
-from go.billing import tasks
-from go.billing.models import Account, MessageCost, Transaction
+from go.billing.models import Account
+
+from .helpers import mk_statement, mk_transaction
 
 
 class TestStatementView(GoDjangoTestCase):
@@ -17,39 +15,9 @@ class TestStatementView(GoDjangoTestCase):
         self.account = Account.objects.get(
             user=self.user_helper.get_django_user())
 
-    def _mk_transaction(self, account_number, tag_pool_name='pool1',
-                        tag_name="tag1",
-                        message_direction=MessageCost.DIRECTION_INBOUND,
-                        message_cost=100, markup_percent=10.0,
-                        credit_factor=0.25, credit_amount=28,
-                        status=Transaction.STATUS_COMPLETED, **kwargs):
-        transaction = Transaction(
-            account_number=account_number,
-            tag_pool_name=tag_pool_name,
-            tag_name=tag_name,
-            message_direction=message_direction,
-            message_cost=message_cost,
-            markup_percent=Decimal(str(markup_percent)),
-            credit_factor=Decimal(str(credit_factor)),
-            credit_amount=credit_amount,
-            status=status, **kwargs)
-
-        transaction.save()
-        return transaction
-
-    def _mk_statement(self):
-        today = date.today()
-        next_month = today + relativedelta(months=1)
-        from_date = date(today.year, today.month, 1)
-        to_date = (date(next_month.year, next_month.month, 1)
-                   - relativedelta(days=1))
-
-        return tasks.generate_monthly_statement(
-            self.account.id, from_date, to_date)
-
     def test_statement_accessable(self):
-        self._mk_transaction(self.account.account_number)
-        statement = self._mk_statement()
+        mk_transaction(self.account)
+        statement = mk_statement(self.account)
         client = self.vumi_helper.get_client()
         client.login()
         response = client.get(
