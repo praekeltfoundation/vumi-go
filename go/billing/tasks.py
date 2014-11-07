ddac560be3b8f5a8d04c98d788e3d6d73f0ecb44
Simon Cross <hodgestar@gmail.com>
2014-11-07 12:49:52 +0200

    Outline of archival functions.

--- go/billing/tasks.py
+++ go/billing/tasks.py
@@ -5,9 +5,30 @@ from dateutil.relativedelta import relativedelta
 from celery.task import task, group
 
 from django.db.models import Sum
+from django.core import serializers
 
 from go.billing import settings
-from go.billing.models import Account, Transaction, Statement, LineItem
+from go.billing.models import (
+    Account, Transaction, Statement, LineItem, TransactionArchive)
+
+
+def month_range(months_ago=1, today=None):
+    """ Return the dates at the start and end of a calendar month.
+
+    :param in months_ago:
+        How many months back the range should be.
+    :param date today:
+        The date which months_ago is relative to.
+
+    :returns:
+        A tuple consisting of (from_date, to_date).
+    """
+    if today is None:
+        today = date.today()
+    last_month = today - relativedelta(months=months_ago)
+    from_date = date(last_month.year, last_month.month, 1)
+    to_date = date(today.year, today.month, 1) - relativedelta(days=1)
+    return from_date, to_date
 
 
 @task()
@@ -50,10 +71,7 @@ def generate_monthly_account_statements():
     """Spawn sub-tasks to generate a *Monthly* ``Statement`` for accounts
        without a *Monthly* statement.
     """
-    today = date.today()
-    last_month = today - relativedelta(months=1)
-    from_date = date(last_month.year, last_month.month, 1)
-    to_date = date(today.year, today.month, 1) - relativedelta(days=1)
+    from_date, to_date = month_range(months_ago=1)
     account_list = Account.objects.exclude(
         statement__type=Statement.TYPE_MONTHLY,
         statement__from_date=from_date,
@@ -65,3 +83,50 @@ def generate_monthly_account_statements():
             generate_monthly_statement.s(account.id, from_date, to_date))
 
     return group(task_list)()
+
+
+@task
+def archive_monthly_transactions(months_ago=3):
+    """Spawn sub-tasks to archive transactions from N months ago."""
+    from_date, to_date = month_range(months_ago=months_ago)
+    account_list = Account.objects.exclude(
+        transactionarchive__from_date=from_date,
+        transactionarchive__to_date=to_date)
+
+    task_list = []
+    for account in account_list:
+        task_list.append(archive_transactions.s(
+            account.id, from_date, to_date))
+
+    return group(task_list)()
+
+
+@task()
+def archive_transactions(account_id, from_date, to_date, serializer='json'):
+    account = Account.objects.get(id=account_id)
+    formatter = serializers.get(serializer)()
+    filename = (
+        u"transactions-%(account_number)s-%(from)s-to-%(to)s"
+        ".%(serializer)s" % {
+            "account_number": account.account_number,
+            "from": from_date,
+            "to": to_date,
+            "serializer": serializer,
+        })
+
+    archive = TransactionArchive.create(
+        account=account, filename=filename,
+        from_date=from_date, to_date=to_date,
+        status=TransactionArchive.PENDING)
+
+    transaction_list = Transaction.objects.filter(
+        account_number=account.account_number,
+        created__gte=from_date,
+        created__lt=(to_date + relativedelta(days=1)))
+
+    for transaction in transaction_list:
+        formatter.serialize([transaction])
+        # TODO: write to S3
+
+    archive.status = TransactionArchive.COMPLETED
+    archive.save()
