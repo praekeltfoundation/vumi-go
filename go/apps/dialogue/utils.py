f2c8aab71386e30619437dd052287af47d4b4d1e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-11-11 15:35:13 +0200

    Change dialogue app utils to build the config's endpoints correctly

--- go/apps/dialogue/utils.py
+++ go/apps/dialogue/utils.py
@@ -1,12 +1,16 @@
-def configured_endpoints(config):
-    poll = config.get("poll", {})
-
-    names = set(
+def channel_types(poll):
+    return set(
         s['channel_type']
         for s in poll.get('states', []) if s['type'] == 'send')
 
+
+def endpoint_configs(poll):
+    names = channel_types(poll)
     types = poll.get('channel_types', [])
-    return [t['label'] for t in types if t['name'] in names]
+
+    return dict(
+        (t['label'], {'delivery_class': t['name']})
+        for t in types if t['name'] in names)
 
 
 def dialogue_js_config(conv):
@@ -14,7 +18,7 @@ def dialogue_js_config(conv):
 
     config = {
         "name": "poll-%s" % conv.key,
-        "endpoints": configured_endpoints(conv.config)
+        "endpoints": endpoint_configs(poll)
     }
 
     poll_metadata = poll.get('poll_metadata', {})
@@ -24,3 +28,10 @@ def dialogue_js_config(conv):
         config['delivery_class'] = delivery_class
 
     return config
+
+
+def configured_endpoints(config):
+    poll = config.get("poll", {})
+    names = channel_types(poll)
+    types = poll.get('channel_types', [])
+    return [t['label'] for t in types if t['name'] in names]
