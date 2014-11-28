354f8570a5babbfbb30063dfa4e3c342db780f1e
Simon de Haan <simon@praekeltfoundation.org>
2014-11-28 15:20:17 +0200

    refactor to use call_command() and CommandIO (thanks @hodgestar)

--- go/base/tests/test_go_generate_export_conversations_urls.py
+++ go/base/tests/test_go_generate_export_conversations_urls.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-from StringIO import StringIO
-
-from go.base.tests.helpers import GoDjangoTestCase, DjangoVumiApiHelper
+from go.base.tests.helpers import (
+    GoDjangoTestCase, DjangoVumiApiHelper, CommandIO)
 from go.base.management.commands import go_generate_export_conversations_urls
-# from go.vumitools.tests.helpers import GoMessageHelper
+from django.core.management import call_command
+
+
+Command = go_generate_export_conversations_urls.Command
 
 
 class TestGoGenExportConvUrls(GoDjangoTestCase):
@@ -15,51 +16,58 @@ class TestGoGenExportConvUrls(GoDjangoTestCase):
         self.user_helper = self.vumi_helper.make_django_user()
         self.user_email = self.user_helper.get_django_user().email
 
-        self.command = go_generate_export_conversations_urls.Command()
-        self.command.stdout = StringIO()
-        self.command.stderr = StringIO()
+    def run_command(self, **kw):
+        cmd_io = CommandIO()
+        call_command('go_generate_export_conversations_urls',
+                     stdout=cmd_io.stdout, stderr=cmd_io.stderr, **kw)
+        return cmd_io
 
     def test_direction(self):
         conv = self.user_helper.create_conversation(u"http_api")
-        self.command.handle(email=self.user_email,
-                            base_url=self.base_url,
-                            template=self.command.DEFAULT_TEMPLATE)
-        output = self.command.stdout.getvalue()
+        cmd = self.run_command(
+            email=self.user_email,
+            base_url=self.base_url,
+            template=Command.DEFAULT_TEMPLATE)
+        output = cmd.stdout.getvalue()
         self.assertTrue('inbound' in output)
         self.assertTrue('outbound' in output)
         self.assertTrue(conv.batch.key in output)
 
     def test_running(self):
         self.user_helper.create_conversation(u"http_api", started=True)
-        self.command.handle(email=self.user_email,
-                            base_url=self.base_url,
-                            template=self.command.DEFAULT_TEMPLATE)
-        output = self.command.stdout.getvalue()
+        cmd = self.run_command(
+            email=self.user_email,
+            base_url=self.base_url,
+            template=Command.DEFAULT_TEMPLATE)
+        output = cmd.stdout.getvalue()
         self.assertTrue('running' in output)
 
     def test_stopped(self):
         conv = self.user_helper.create_conversation(u"http_api")
         conv.stop_conversation()
-        self.command.handle(email=self.user_email,
-                            base_url=self.base_url,
-                            template=self.command.DEFAULT_TEMPLATE)
-        output = self.command.stdout.getvalue()
+        cmd = self.run_command(
+            email=self.user_email,
+            base_url=self.base_url,
+            template=Command.DEFAULT_TEMPLATE)
+        output = cmd.stdout.getvalue()
         self.assertTrue('stopping' in output)
 
     def test_archived(self):
         conv = self.user_helper.create_conversation(u"http_api")
         conv.archive_conversation()
-        self.command.handle(email=self.user_email,
-                            base_url=self.base_url,
-                            template=self.command.DEFAULT_TEMPLATE)
-        output = self.command.stdout.getvalue()
+        cmd = self.run_command(
+            email=self.user_email,
+            base_url=self.base_url,
+            template=Command.DEFAULT_TEMPLATE)
+        output = cmd.stdout.getvalue()
         self.assertTrue('archived' in output)
 
     def test_template(self):
         self.user_helper.create_conversation(u"http_api")
-        self.command.handle(email=self.user_email,
-                            base_url='http://foo/bar/',
-                            template='Hello {direction} {status}\n')
-        output = self.command.stdout.getvalue()
+        cmd = self.run_command(
+            email=self.user_email,
+            base_url=self.base_url,
+            template='Hello {direction} {status}\n')
+        output = cmd.stdout.getvalue()
         self.assertEqual(
             output, 'Hello inbound stopped\nHello outbound stopped\n')
