5883ee79dca31c56fa19a7da3dae5567c6bcf247
Rudi Giesler <rudi@praekelt.com>
2014-12-15 11:36:48 +0200

    Changed Account model to contain last_topup_balance

--- go/billing/models.py
+++ go/billing/models.py
@@ -44,18 +44,11 @@ class Account(models.Model):
         max_digits=20, decimal_places=6, default=Decimal('0.0'),
         help_text=_("The current credit balance."))
 
-    alert_threshold = models.DecimalField(
-        max_digits=10, decimal_places=2, default=Decimal('0.0'),
-        help_text=_("Low-credits notification will be sent when the "
-                    "credit balance reaches the alert threshold percentage "
-                    "of the balance after the last credit purchase."))
-
-    alert_credit_balance = models.DecimalField(
+    last_topup_balance = models.DecimalField(
         max_digits=20, decimal_places=6, default=Decimal('0.0'),
-        help_text=_("Low-credits notification will be sent when the credit "
-                    "balance goes below this value. This value is updated to "
-                    "(alert_threshold * current balance) when credits are "
-                    "loaded."))
+        help_text=_("The credits balance after the last credit top up. Used "
+                    "in the calculation to determine when low-credits "
+                    "notifications should be sent."))
 
     def __unicode__(self):
         return u"{0} ({1})".format(self.account_number, self.user.email)
--- go/billing/tests/test_models.py
+++ go/billing/tests/test_models.py
@@ -35,8 +35,7 @@ class TestAccount(GoDjangoTestCase):
         self.assertEqual(acc.user, django_user)
         self.assertEqual(acc.account_number, profile.user_account)
         self.assertEqual(acc.credit_balance, decimal.Decimal('0.0'))
-        self.assertEqual(acc.alert_threshold, decimal.Decimal('0.0'))
-        self.assertEqual(acc.alert_credit_balance, decimal.Decimal('0.0'))
+        self.assertEqual(acc.last_topup_balance, decimal.Decimal('0.0'))
 
     def test_post_save_hook_not_created(self):
         django_user = self.user_helper.get_django_user()
