21fcb9883a76b59df9cd23743548cd9468d8bc4c
Jeremy Thurgood <jeremy@praekelt.com>
2015-02-03 14:01:59 +0200

    Use indexes in go_system_stats management command and bring back unique address counts.

--- go/base/management/commands/go_system_stats.py
+++ go/base/management/commands/go_system_stats.py
@@ -106,17 +106,16 @@ class Command(BaseGoCommand):
             writer.writerow(row)
 
     def _increment_msg_stats(self, conv, stats):
-        batch_key = conv.batch.key
-        mdb = conv.mdb
-        cache = mdb.cache
-        inbound_uniques = cache.count_from_addrs(batch_key)
-        outbound_uniques = cache.count_to_addrs(batch_key)
+        inbound_stats = conv.mdb.batch_inbound_stats(conv.batch.key)
+        outbound_stats = conv.mdb.batch_outbound_stats(conv.batch.key)
         stats["conversations_started"] += 1
-        stats["inbound_message_count"] += mdb.batch_inbound_count(batch_key)
-        stats["outbound_message_count"] += mdb.batch_outbound_count(batch_key)
-        stats["inbound_uniques"] += inbound_uniques
-        stats["outbound_uniques"] += outbound_uniques
-        stats["total_uniques"] += max(inbound_uniques, outbound_uniques)
+        stats["inbound_message_count"] += inbound_stats['total']
+        stats["outbound_message_count"] += outbound_stats['total']
+        stats["inbound_uniques"] += inbound_stats['unique_addresses']
+        stats["outbound_uniques"] += outbound_stats['unique_addresses']
+        stats["total_uniques"] += max(
+            inbound_stats['unique_addresses'],
+            outbound_stats['unique_addresses'])
 
     def handle_command_message_counts_by_month(self, *args, **options):
         month_stats = {}
