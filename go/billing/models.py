debd3fc59b503433beb2db40b6e1381d34876789
Rudi Giesler <rudi@praekelt.com>
2015-02-05 11:00:55 +0200

    Change parameter name for calculate_session_length_credit_cost to be less ambiguous.

--- go/billing/models.py
+++ go/billing/models.py
@@ -130,12 +130,12 @@ class MessageCost(models.Model):
 
     @classmethod
     def calculate_session_length_credit_cost(
-            cls, session_length_credits, markup_percent, context=None):
+            cls, session_length_cost, markup_percent, context=None):
         """
         Return the cost of a session length (in credits).
         """
         return cls.apply_markup_and_convert_to_credits(
-            session_length_credits, markup_percent, context=context)
+            session_length_cost, markup_percent, context=context)
 
     @classmethod
     def calculate_credit_cost(cls, message_cost, storage_cost, markup_percent,
