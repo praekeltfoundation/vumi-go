3e27932f8f13825f9976cb37714f6a20cd40a433
justinvdm <justinvdm.0.1.0@gmail.com>
2015-02-24 09:05:44 +0200

    Explicitly return zero if the unit cost is zero

--- go/billing/tasks.py
+++ go/billing/tasks.py
@@ -172,7 +172,11 @@ def get_count(transaction):
 def get_session_length_count(transaction):
     length_cost = get_session_length_cost(transaction)
     unit_cost = get_session_length_unit_cost(transaction)
-    return length_cost / (unit_cost if unit_cost != 0 else 1)
+
+    if unit_cost == 0:
+        return Decimal(0)
+    else:
+        return length_cost / unit_cost
 
 
 def get_message_unit_cost(transaction):
