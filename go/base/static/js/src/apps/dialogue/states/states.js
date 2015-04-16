59ad4ea4b9e77c395219dd43872aeec3faf58cb9
Steve Barnett <steve@naga.co.za>
2015-04-16 10:10:14 +0200

    WIP

--- go/base/static/js/src/apps/dialogue/states/states.js
+++ go/base/static/js/src/apps/dialogue/states/states.js
@@ -18,6 +18,8 @@
       ParametricEndpointView = endpoints.ParametricEndpointView,
       AligningEndpointCollection = endpoints.AligningEndpointCollection;
 
+  var maxChars = 140;
+
   var DialogueEndpointView = ParametricEndpointView.extend();
 
   var EntryEndpointView = DialogueEndpointView.extend({
@@ -123,7 +125,11 @@
     detach: function() {
       this.$el.detach();
       return this;
-    }
+    },
+
+    // maxChars: function() {
+    //   return 140;
+    // }
   });
 
   // Mode allowing the user to make changes to the dialogue state. Acts as a
