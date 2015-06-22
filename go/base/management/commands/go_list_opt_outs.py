a3cfa99fee208ee6aa8ab3fdf0c487e2066de375
Jamie Hewland <jhewland@gmail.com>
2015-06-22 10:00:18 +0000

    Make go_list_opt_outs extend BaseGoAccountCommand

--- go/base/management/commands/go_list_opt_outs.py
+++ go/base/management/commands/go_list_opt_outs.py
@@ -1,30 +1,22 @@
 from optparse import make_option
 
-from go.base.command_utils import BaseGoCommand, make_email_option
+from go.base.command_utils import BaseGoAccountCommand
 from go.vumitools.opt_out import OptOutStore
 
 
 class Command(BaseGoCommand):
     help = "List opt-outs from a particular account"
 
-    LOCAL_OPTIONS = [
-        make_email_option()
-    ]
-
-    option_list = BaseGoCommand.option_list + tuple(LOCAL_OPTIONS)
-
-    def handle(self, *args, **options):
+    def handle_no_command(self, *args, **options):
         options = options.copy()
         self.handle_validated(*args, **options)
 
     def handle_validated(self, *args, **options):
-        _, user_api = self.mk_user_api(options['email-address'])
+        self.show_opt_outs()
 
-        self.show_opt_outs(user_api, email_address)
-
-    def show_opt_outs(self, user_api, email_address):
-        opt_out_store = OptOutStore(user_api.manager,
-                                    user_api.user_account_key)
+    def show_opt_outs(self):
+        opt_out_store = OptOutStore(self.user_api.manager,
+                                    self.user_api.user_account_key)
         opt_outs = opt_out_store.list_opt_outs()
 
         print "Address Type, Address, Message ID, Timestamp"
