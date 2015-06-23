2ce255eca59cd1149ee78e99fd21f7ef8d56bf4e
Jamie Hewland <jhewland@gmail.com>
2015-06-23 09:06:33 +0000

    Revert "Make go_list_opt_outs extend BaseGoAccountCommand"
    * Must wait for tests to be written before changing
    
    This reverts commit a3cfa99fee208ee6aa8ab3fdf0c487e2066de375.

--- go/base/management/commands/go_list_opt_outs.py
+++ go/base/management/commands/go_list_opt_outs.py
@@ -1,22 +1,30 @@
 from optparse import make_option
 
-from go.base.command_utils import BaseGoAccountCommand
+from go.base.command_utils import BaseGoCommand, make_email_option
 from go.vumitools.opt_out import OptOutStore
 
 
 class Command(BaseGoCommand):
     help = "List opt-outs from a particular account"
 
-    def handle_no_command(self, *args, **options):
+    LOCAL_OPTIONS = [
+        make_email_option()
+    ]
+
+    option_list = BaseGoCommand.option_list + tuple(LOCAL_OPTIONS)
+
+    def handle(self, *args, **options):
         options = options.copy()
         self.handle_validated(*args, **options)
 
     def handle_validated(self, *args, **options):
-        self.show_opt_outs()
+        _, user_api = self.mk_user_api(options['email-address'])
 
-    def show_opt_outs(self):
-        opt_out_store = OptOutStore(self.user_api.manager,
-                                    self.user_api.user_account_key)
+        self.show_opt_outs(user_api, email_address)
+
+    def show_opt_outs(self, user_api, email_address):
+        opt_out_store = OptOutStore(user_api.manager,
+                                    user_api.user_account_key)
         opt_outs = opt_out_store.list_opt_outs()
 
         print "Address Type, Address, Message ID, Timestamp"
