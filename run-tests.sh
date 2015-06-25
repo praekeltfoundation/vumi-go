db586ddb766f6240d1c900094c310ad239eaa6c5
Jeremy Thurgood <jeremy@praekelt.com>
2015-06-25 17:50:45 +0200

    Run tests with manager closed assertions in Travis.

--- run-tests.sh
+++ run-tests.sh
@@ -12,7 +12,7 @@ export VUMI_TEST_NODE_PATH="$(which node)"
 # This is necessary so that we import test modules from the working dir instead
 # of the installed package.
 export PYTHONPATH=.
-./run-tests-pytest.sh --cov=go go/
+VUMI_TEST_ASSERT_CLOSED=true ./run-tests-pytest.sh --cov=go go/
 echo "=== Checking for PEP-8 violations..."
 pep8 --repeat go | grep -v '^go/\(base\|billing\)/\(auth_\|registration_\)\?migrations/' | tee pep8.txt
 echo "=== Done."
