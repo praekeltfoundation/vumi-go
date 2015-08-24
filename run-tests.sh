9f5934ad85248d6131ba0070d8853dbce2291270
Jeremy Thurgood <jeremy@praekelt.com>
2015-08-24 17:13:28 +0200

    Skip unnecessary coverage.

--- run-tests.sh
+++ run-tests.sh
@@ -6,13 +6,14 @@ echo "=== Nuking old .pyc files..."
 find go/ -name '*.pyc' -delete
 find go/ -name '__pycache__' -delete
 echo "=== Erasing previous coverage data..."
-coverage erase
+[ -z "$NO_COVERAGE" ] && coverage erase
 echo "=== Running tests..."
 export VUMI_TEST_NODE_PATH="$(which node)"
 # This is necessary so that we import test modules from the working dir instead
 # of the installed package.
 export PYTHONPATH=.
-VUMI_TEST_ASSERT_CLOSED=true ./run-tests-pytest.sh --cov=go go/
+[ -z "$NO_COVERAGE" ] && COVERAGE_ARGS="--cov=go"
+VUMI_TEST_ASSERT_CLOSED=true ./run-tests-pytest.sh $COVERAGE_ARGS go
 echo "=== Checking for PEP-8 violations..."
 pep8 --repeat go | grep -v '^go/\(base\|billing\)/\(auth_\|registration_\)\?migrations/' | tee pep8.txt
 echo "=== Done."
