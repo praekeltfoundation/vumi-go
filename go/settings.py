4fbf5705a4a95235712be6221d299f7c8cd01dff
Simon Cross <hodgestar@gmail.com>
2015-11-05 13:45:55 +0200

    Add scheduler to settings.

--- go/settings.py
+++ go/settings.py
@@ -170,6 +170,7 @@ INSTALLED_APPS = (
     'go.contacts',
     'go.account',
     'go.billing',
+    'go.scheduler',
 
 
     'vxpolls.djdashboard',
@@ -306,6 +307,10 @@ CELERYBEAT_SCHEDULE = {
     #     'task': 'go.billing.tasks.generate_monthly_account_statements',
     #     'schedule': crontab(day_of_month=1),
     # },
+    'poll-scheduler-tasks': {
+        'task': 'go.scheduler.tasks.poll_tasks',
+        'schedule': crontab(minute='*/5'),
+    },
 }
 
 
