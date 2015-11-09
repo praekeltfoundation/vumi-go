8a1e64cb67746ea5372e81265071e69794fed99d
Jeremy Thurgood <jeremy@praekelt.com>
2015-11-09 18:14:08 +0200

    We can give up after 100 failing tests, I think.

--- run-tests.sh
+++ run-tests.sh
@@ -13,7 +13,7 @@ export VUMI_TEST_NODE_PATH="$(which node)"
 # of the installed package.
 export PYTHONPATH=.
 [ -z "$NO_COVERAGE" ] && COVERAGE_ARGS="--cov=go"
-VUMI_TEST_ASSERT_CLOSED=true ./run-tests-pytest.sh $COVERAGE_ARGS go
+VUMI_TEST_ASSERT_CLOSED=true ./run-tests-pytest.sh $COVERAGE_ARGS go --maxfail=100
 echo "=== Checking for PEP-8 violations..."
 pep8 --repeat go | grep -v '^go/\(base\|billing\)/\(auth_\|registration_\)\?migrations/' | tee pep8.txt
 echo "=== Done."
