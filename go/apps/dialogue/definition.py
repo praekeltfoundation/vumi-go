741bb6d35cff947e2a55568a5295f15aa4fb74a4
Rudi Giesler <rudi@praekelt.com>
2016-01-20 12:07:46 +0200

    Remove performing the scheduled action from the apps Actions

--- go/apps/dialogue/definition.py
+++ go/apps/dialogue/definition.py
@@ -10,23 +10,6 @@ class SendDialogueAction(SendJsboxAction):
     action_display_verb = 'Send dialogue now'
     action_schedule_verb = 'Schedule dialogue send'
 
-    def perform_scheduled_action(self, action_data):
-        # We're importing here to avoid top level django imports in the
-        # definition.
-        # TODO: Find a better solution.
-        from go.scheduler.models import Task
-        task = Task.objects.create(
-            account_id=self._conv.user_api.user_account_key,
-            label='Dialogue Message Send',
-            task_type=Task.TYPE_CONVERSATION_ACTION,
-            task_data={
-                'conversation_key': self._conv.key,
-                'action_name': 'send_jsbox',
-                'action_kwargs': {},
-            },
-            scheduled_for=action_data['scheduled_datetime'])
-        task.save()
-
 
 class ConversationDefinition(ConversationDefinitionBase):
     conversation_type = 'dialogue'
