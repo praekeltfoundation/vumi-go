2e5a5a7d241c45d45e670db65961f0616ea8abec
Rudi Giesler <rudi@praekelt.com>
2016-01-25 17:54:50 +0200

    Add Cancel/Reactivate button

--- go/scheduler/urls.py
+++ go/scheduler/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import patterns, url
-from go.scheduler.views import SchedulerListView
+from django.contrib.auth.decorators import login_required
+from go.scheduler.views import (
+    SchedulerCreatePendingTask, SchedulerDelete, SchedulerListView)
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', SchedulerListView.as_view(), name='tasks'),
+    url(r'^$', login_required(SchedulerListView.as_view()), name='tasks'),
+    url(
+        r'^(?P<pk>\d+)/delete$', login_required(SchedulerDelete.as_view()),
+        name='delete_task'),
+    url(
+        r'^(?P<pk>\d+)/reactivate$',
+        login_required(SchedulerCreatePendingTask.as_view()),
+        name='reactivate_task'),
 )
--- go/scheduler/views.py
+++ go/scheduler/views.py
@@ -1,6 +1,13 @@
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView
+from django.views.generic.base import View
+from django.views.generic.detail import SingleObjectMixin
 
-from go.scheduler.models import Task
+from go.scheduler.models import Task, PendingTask
 
 
 class SchedulerListView(ListView):
@@ -12,3 +19,45 @@ class SchedulerListView(ListView):
         return Task.objects.filter(
             account_id=self.request.user_api.user_account_key
             ).order_by('-scheduled_for')
+
+
+class SchedulerDelete(SingleObjectMixin, View):
+    model = Task
+    url = reverse_lazy('scheduler:tasks')
+
+    @method_decorator(csrf_protect)
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+
+        if task.status != Task.STATUS_PENDING:
+            raise PermissionDenied
+        if task.account_id != self.request.user_api.user_account_key:
+            raise PermissionDenied
+
+        task.status = Task.STATUS_CANCELLED
+        task.save()
+
+        PendingTask.objects.get(task=task).delete()
+
+        return HttpResponseRedirect(self.url)
+
+
+class SchedulerCreatePendingTask(SingleObjectMixin, View):
+    model = Task
+    url = reverse_lazy('scheduler:tasks')
+
+    @method_decorator(csrf_protect)
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+
+        if task.status != Task.STATUS_CANCELLED:
+            raise PermissionDenied
+        if task.account_id != self.request.user_api.user_account_key:
+            raise PermissionDenied
+
+        PendingTask.objects.create(
+            task=task, scheduled_for=task.scheduled_for)
+        task.status = Task.STATUS_PENDING
+        task.save()
+
+        return HttpResponseRedirect(self.url)
