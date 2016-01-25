bc6c3834cd8383f7e1f9e109f0413bb6015a92bf
Rudi Giesler <rudi@praekelt.com>
2016-01-25 09:54:25 +0200

    Remove unneeded datetime from view

--- go/scheduler/views.py
+++ go/scheduler/views.py
@@ -1,4 +1,3 @@
-import datetime
 from django.views.generic import ListView
 
 from go.scheduler.models import Task
@@ -10,7 +9,6 @@ class SchedulerListView(ListView):
     template = 'scheduler/task_list.html'
 
     def get_queryset(self):
-        now = datetime.datetime.utcnow()
         return Task.objects.filter(
             account_id=self.request.user_api.user_account_key
             ).order_by('-scheduled_for')
