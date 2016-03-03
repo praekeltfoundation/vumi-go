bc5823171cb0d9a89a5b721c7ac5f6e6fbc39cf3
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-03-03 11:10:37 +0200

    retrive set contents with 'smembers' rather than 'get'

--- go/base/tasks.py
+++ go/base/tasks.py
@@ -28,7 +28,7 @@ def get_and_reset_recent_conversations(vumi_api):
     # makes use of redis atomic functions to ensure nothing is added to the set
     # before it is deleted
     redis.rename("recent_coversations", "old_recent_conversations")
-    conversation_details = redis.get("old_recent_conversations")
+    conversation_details = redis.smembers("old_recent_conversations")
     redis.delete("old_recent_conversations")
     return conversation_details
 
